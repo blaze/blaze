8cad3fe690fc4ee07ea0ea10242b9586fea08f4b
Stephen Diehl <stephen.m.diehl@gmail.com>
2012-10-12 12:18:28 -0400

    test for 3d partition case

--- ndtable/regions/tests/test_scalar.py
+++ ndtable/regions/tests/test_scalar.py
@@ -10,7 +10,10 @@ def test_vertical_stack():
     x = Chart([a,b], alpha)
     y = Chart([a,b], beta)
 
+    # -------------
     s = vstack(x,y)
+    # -------------
+
     block, coords = s[[3,1]]
     assert block is beta
     assert coords == [1,1]
@@ -41,7 +44,10 @@ def test_horizontal_stack():
     x = Chart([a,b], alpha)
     y = Chart([a,b], beta)
 
+    # -------------
     s = hstack(x,y)
+    # -------------
+
     block, coords = s[[0,0]]
     assert block is alpha
     assert coords == [0,0]
@@ -57,3 +63,22 @@ def test_horizontal_stack():
     block, coords = s[[2,4]]
     assert block is beta
     assert coords == [2,2]
+
+def test_vertical_stack_3d():
+    alpha = object()
+    beta  = object()
+
+    a = interval(0,2)
+    b = interval(0,2)
+    c = interval(0,2)
+
+    x = Chart([a,b,c], alpha)
+    y = Chart([a,b,c], beta)
+
+    # -------------
+    s = vstack(x,y)
+    # -------------
+
+    block, coords = s[[0,0,0]]
+    assert block is alpha
+    assert coords == [0,0,0]
