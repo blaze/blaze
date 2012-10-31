71a523772991fd0488cc6a36e9a8d4ab137525fe
Stephen Diehl <stephen.m.diehl@gmail.com>
2012-10-31 12:26:36 -0400

    Experiments with minivect.

---
+++ ndtable/expr/tests/test_minivect.py
@@ -0,0 +1,54 @@
+from ndtable.engine.pipeline import Pipeline
+from ndtable.expr.graph import IntNode, FloatNode
+from ndtable.expr.visitor import MroTransformer
+from ndtable.expr.nodes import flat
+from ndtable.table import NDTable
+
+#------------------------------------------------------------------------
+# Minivect
+#------------------------------------------------------------------------
+
+from ndtable.engine.mv import LazyLLVMContext, miniast, \
+    minitypes, specializers, Minivect
+
+context = LazyLLVMContext()
+builder = context.astbuilder
+
+#------------------------------------------------------------------------
+# Sample Graph
+#------------------------------------------------------------------------
+
+a = NDTable()
+b = NDTable()
+
+x = a+b
+
+#------------------------------------------------------------------------
+# Tests
+#------------------------------------------------------------------------
+
+def test_simple_expr():
+    walk = Minivect()
+
+    body = walk.visit(x)
+    variables = walk.variables
+
+    temp = builder.variable(x.cod.to_minitype(), 'temp')
+    prog = builder.assign(temp, body)
+
+    func = builder.build_function(variables, prog, 'func')
+
+    # First arg is the outer shape
+    #args = [fist_array.ctypes.shape]
+    args = [0]
+
+    for variable in variables:
+        if variable.type.is_array:
+            numpy_array = variable.value
+            data_pointer = get_blaze_pointer(numpy_array, variable.type)
+            args.append(data_pointer)
+            # Assume contigious so we don't need to specify strides
+
+    specializer = specializers.StridedCInnerContigSpecializer
+    specialized_func, (llvm_func, ctypes_func) = specialize(specializer, func)
+    ctypes_func(args)
