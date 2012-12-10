3cc1cd460229b6f3bc4e3d1815b1fe62ab89a092
Francesc Alted <francesc@continuum.io>
2012-12-10 20:14:47 +0100

    Added a mock test for persistence

--- blaze/tests/test_quickstart.py
+++ blaze/tests/test_quickstart.py
@@ -8,6 +8,20 @@ def test_simple_session():
 
     a = Array([1,2,3,4], ds)
 
+def test_simple_persistence():
+    import tempfile, shutil, os.path
+    import numpy as np
+    from blaze import Array, dshape, params
+    ds = dshape('2, 2, float64')
+    data = np.zeros(4).reshape(2,2)
+    td = tempfile.mkdtemp()
+    tmppath = os.path.join(td, 'a')
+
+    a = Array([1,2,3,4], ds, params=params(storage=tmppath))
+
+    # Remove everything under the temporary dir
+    shutil.rmtree(td)
+
 def test_custom_dshape():
     from blaze import Array, RecordDecl
     from blaze import int32, string
