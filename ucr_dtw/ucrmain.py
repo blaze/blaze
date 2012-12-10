67588abf8cff3683a40d105a4b72711746026bf2
Francesc Alted <francesc@continuum.io>
2012-12-10 18:44:30 +0100

    Added code to convert data if necessary.

--- ucr_dtw/ucrmain.py
+++ ucr_dtw/ucrmain.py
@@ -1,4 +1,18 @@
-import ucr
+import os.path
+import blaze
+from ucr_dtw import ucr
+
+
+# Convert txt file into Blaze native format
+def convert(filetxt, storage):
+    if not os.path.exists(storage):
+        blaze.Array(np.loadtxt(filetxt),
+                    params=blaze.params(storage=storage))
+
+convert("Data.txt", "Data")
+convert("Query.txt", "Query")
+convert("Query2.txt", "Query2")
 
 #ucr.ed("Data", "Query", 128)
-ucr.dtw("Data", "Query", 0.1, 128)
+#ucr.dtw("Data", "Query", 0.1, 128)
+ucr.dtw("Data", "Query2", 0.1, 128)
