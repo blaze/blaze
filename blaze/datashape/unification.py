6869d5edd706d95c8cadbd1945b29fdd3bfecd6b
Stephen Diehl <stephen.m.diehl@gmail.com>
2012-12-13 01:05:25 -0600

    Remove very old type unifier, for robust one

--- blaze/datashape/unification.py
+++ blaze/datashape/unification.py
@@ -11,77 +11,16 @@ on the types.
 """
 
 from numpy import promote_types
-from coretypes import Fixed, Range, TypeVar, Record, \
-    CType, Enum, top, dynamic
+from blaze.datashape.coretypes import TypeVar
+from blaze.expr.typeinference import infer
 
-class Incommensurable(Exception):
-    def __init__(self, space, dim):
-        self.space = space
-        self.dim   = dim
+class Incommensurable(TypeError):
+    pass
 
-    def __str__(self):
-        return "No way of unifying (%s) (%s)" % (
-            self.space, self.dim
-        )
-
-def unify(a, b):
+def unify(sig, concrete=True):
     """
     Unification of Datashapes.
     """
-    ta = type(a)
-    tb = type(b)
-
-    # --
-
-    # Unification over BlazeT has two zeros
-
-    if ta == top or tb == top:
-        return top
-
-    if ta == dynamic or tb == dynamic:
-        return top
-
-    # --
-
-    if (ta,tb) == (Fixed, Fixed):
-        if a.val == b.val:
-            return Fixed(a.val)
-        else:
-            return Enum(a.val, b.val)
-
-    # --
-
-    if (ta,tb) == (TypeVar, Fixed):
-        return TypeVar('x0')
-
-    if (ta,tb) == (Fixed, TypeVar):
-        return TypeVar('x0')
-
-    # --
-
-    if (ta,tb) == (Record, Record):
-        c = a.d.items() + b.d.items()
-        return Record(**dict(c))
-
-    # --
-
-    if (ta,tb) == (Fixed, Range):
-        return Range(min(a.val, b.lower), max(a.val, b.upper))
-
-    if (ta,tb) == (Range, Fixed):
-        return Range(min(a.lower, b.val), max(a.val, b.val))
-
-    if (ta,tb) == (Range, Range):
-        return Range(min(a.lower, b.lower), max(b.upper, b.upper))
-
-    # --
-
-    #if (ta,tb) == (Union, Union):
-        #return Union(a.parameters + b.parameters)
-
-    # --
-
-    if (ta,tb) == (CType, CType):
-        return CType.from_str(promote_types(a.name, b.name).name)
-
-    raise Incommensurable(a,b)
+    resolved = infer(sig)
+    if all(not isinstance(a, TypeVar) for a in resolved):
+        return resolved
