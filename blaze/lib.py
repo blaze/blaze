4e768601b0326f74307c77278fe3d2712248b9a2
Stephen Diehl <stephen.m.diehl@gmail.com>
2012-12-27 14:30:52 -0600

    Extend lib and metadata.

--- blaze/lib.py
+++ blaze/lib.py
@@ -1,3 +1,39 @@
+"""
+
+Anatomy of a Blaze Function Def
+-------------------------------
+
+::
+
+       ATerm Pattern Matcher
+                |                      +-- Type Signature
+                |                      |
+                v                      v
+     @lift('Mul(<term>,<term>)', '(a,a) -> a', {
+         'types'   : {'a': array_like},     <- Type Constraints
+         'metadata': {'a': aligned, local}, <- Metadata Constraint
+     }, costfn)
+     def multiply(a, b):
+         return np.multipy(a, b)
+        |                     |
+        +---------------------+
+        Function Implementation
+
+
+If we were to "read" this definition in English. It would read:
+
+> Here is a function called Mul, it matches any expression of two
+> graph nodes. In addition the two graph nodes must be of the same
+> datashapes (a) or unify/broadcast to the same type (a,a). It
+> returns a result of the same type of the operands. In addition the
+> two arguments must have metadata annotating them both as aligned
+> memory and in system local memory. If the expression in question
+> matches these criterion than I can tell you that you can perfom
+> this operation in (n*m) FLOPS where n and m are the size of your
+> input array, if you can't find a better implementaion than that use
+> this function implementation!
+
+"""
 import numpy as np
 from math import sqrt
 
@@ -14,48 +50,10 @@ from blaze.metadata import aligned
 from blaze.ts.ucr_dtw import ucr
 #from blaze.ooc import linalg
 
-# evaluating this function over a term has a cost, in the future this
-# might be things like calculations for FLOPs associated with the
-# operation. Could be a function of the shape of the term Most of the
-# costs of BLAS functions can calculated a priori
-
 zerocost = lambda term: 0
 
 #------------------------------------------------------------------------
 # Function Library
-#------------------------------------------------------------------------
-
-# Anatomy of a Blaze Function Def
-# -------------------------------
-
-#   ATerm Pattern Matcher
-#            |                      +-- Type Signature
-#            |                      |
-#            v                      v
-# @lift('Mul(<term>,<term>)', '(a,a) -> a', {
-#     'types'   : {'a': array_like},     <- Type Constraints
-#     'metadata': {'a': aligned, local}, <- Metadata Constraint
-# }, costfn)
-# def multiply(a, b):
-#     return np.multipy(a, b)
-#    |                     |
-#    +---------------------+
-#    Function Implementation
-#
-#
-# If we were to "read" this definition in English. It would read:
-#
-#  > Here is a function called Mul, it matches any expression of two
-#  > graph nodes. In addition the two graph nodes must be of the same
-#  > datashapes (a) or unify/broadcast to the same type (a,a). It
-#  > returns a result of the same type of the operands. In addition the
-#  > two arguments must have metadata annotating them both as aligned
-#  > memory and in system local memory. If the expression in question
-#  > matches these criterion than I can tell you that you can perfom
-#  > this operation in (n*m) FLOPS where n and m are the size of your
-#  > input array, if you can't find a better implementaion than that use
-#  > this function implementation!
-
 #------------------------------------------------------------------------
 
 @lift('dtw(<term>, <term>, <term>, <term>)', '(a,a,float,int) -> b', {
--- blaze/metadata.py
+++ blaze/metadata.py
@@ -1,6 +1,10 @@
 from collections import Mapping, OrderedDict
 from blaze.expr.utils import Symbol as S
 
+#------------------------------------------------------------------------
+# Symbols
+#------------------------------------------------------------------------
+
 # The set of possible facets that are specifiable in the
 # metadata. Most structures will define a subset of these. This
 # is the set over which we will do metadata transformations and
@@ -17,3 +21,22 @@ writeable    = S('writeable')
 aligned      = S('aligned')
 
 update_if_copied = S('update_if_copied')
+
+#------------------------------------------------------------------------
+# Metadata Querying
+#------------------------------------------------------------------------
+
+def has_prop(indexable, prop):
+    """
+    Check whether the indexable object has the given property in
+    its metadata.
+    """
+    return prop in indexable._metadata
+
+def all_prop(args, prop):
+    """
+    Check whether the arguments to a Fun are all manifest
+    array/table objects, needed to determine whether to construct
+    a graph node or to
+    """
+    return all(has_prop(a, prop) for a in args)
