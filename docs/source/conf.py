c86a9af82335a84689c64ba2c0794f5787414db6
Stephen Diehl <stephen.m.diehl@gmail.com>
2012-12-31 18:47:48 -0600

    Fix website.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -141,7 +141,7 @@ highlight_language = 'python'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'blazetheme'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
