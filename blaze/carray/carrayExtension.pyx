92779c372961bd56bb020766a0972c7e0ea496d7
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-01-14 11:12:19 -0600

    Restore self-contained carray, not more header.

--- blaze/carray/carrayExtension.pyx
+++ blaze/carray/carrayExtension.pyx
@@ -205,6 +205,16 @@ cdef class chunk:
   This class is meant to be used only by the `carray` class.
 
   """
+  cdef char typekind, isconstant
+  cdef public int atomsize, itemsize, blocksize
+  cdef public int nbytes, cbytes, cdbytes
+  cdef int true_count
+  cdef char *data
+  cdef object atom, constant, dobject
+
+  cdef void _getitem(self, int start, int stop, char *dest)
+  cdef compress_data(self, char *data, size_t itemsize, size_t nbytes, object cparams)
+  cdef compress_arrdata(self, ndarray array, object cparams, object _memory)
 
   property dtype:
     "The NumPy dtype for this chunk."
