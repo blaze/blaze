843d418a17e47daf102ddd98b92627c3ec98d10c
Francesc Alted <francesc@continuum.io>
2013-01-15 17:24:48 +0100

    Added persistent tests for blob datatype.

--- blaze/tests/test_vlen.py
+++ blaze/tests/test_vlen.py
@@ -3,28 +3,55 @@ Tests for the blob data type.
 """
 
 import blaze
+import tempfile, shutil, os.path
+
 
 def test_simple_blob():
     ds = blaze.dshape('x, blob')
-
     c = blaze.Array(["s1", "sss2"], ds)
 
     assert c[0] == "s1"
     assert c[1] == "sss2"
 
+def test_simple_persistent_blob():
+    td = tempfile.mkdtemp()
+    tmppath = os.path.join(td, 'c')
+
+    ds = blaze.dshape('x, blob')
+    c = blaze.Array(["s1", "sss2"], ds,
+                    params=blaze.params(storage=tmppath))
+
+    assert c[0] == "s1"
+    assert c[1] == "sss2"
+
+    # Remove everything under the temporary dir
+    shutil.rmtree(td)
+
 def test_object_blob():
     ds = blaze.dshape('x, blob')
-
     c = blaze.Array([(i, str(i*.2)) for i in range(10)], ds)
-    print "c:", `c`
 
     for v in c:
         assert v[0] == i
         assert v[1] == i*.2
 
+def test_object_persistent_blob():
+    td = tempfile.mkdtemp()
+    tmppath = os.path.join(td, 'c')
+
+    ds = blaze.dshape('x, blob')
+    c = blaze.Array([(i, str(i*.2)) for i in range(10)], ds,
+                    params=blaze.params(storage=tmppath))
+
+    for v in c:
+        assert v[0] == i
+        assert v[1] == i*.2
+
+    # Remove everything under the temporary dir
+    shutil.rmtree(td)
+
 def test_intfloat_blob():
     ds = blaze.dshape('x, blob')
-
     c = blaze.Array([(i, i*.2) for i in range(10)], ds)
 
     for v in c:
