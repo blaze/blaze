d92b7dc3acc569d8fbb6df60c0aeb728b67ab322
Mark Florisson <markflorisson88@gmail.com>
2013-01-22 21:46:21 -0600

    Add stopgap datashape type unifiers

--- blaze/stopgap.py
+++ blaze/stopgap.py
@@ -1,1 +1,33 @@
-__author__ = 'mark'
+"""
+This file contains stopgap solutions that should be nuked
+whenever someone gets around to implementing something real.
+"""
+
+from blaze import error
+from blaze.expr import graph, nodes
+from blaze.datashape import coretypes, DataShape
+
+def _get_datashape(graph_node):
+    if isinstance(graph_node, graph.App):
+        return _get_datashape(graph_node.operator)
+    elif isinstance(graph_node, nodes.Node):
+        return graph_node.datashape
+    else:
+        return coretypes.from_python_scalar(graph_node)
+
+def broadcast(*operands):
+    types = [_get_datashape(op) for op in operands if op is not None]
+    shapes = []
+    for t in types:
+        try:
+            shapes.append(coretypes.extract_dims(t))
+        except coretypes.NotNumpyCompatible:
+            pass
+
+    # TODO: broadcasting
+    datashapes = [coretypes.to_numpy(coretypes.extract_measure(ds))
+                      for ds in types]
+    type = coretypes.promote_cvals(*datashapes)
+    if not shapes:
+        return type
+    return DataShape(shapes[0] + (type,))
