c1f0a373851f61f65d64569b8b7e2af84f2fc273
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-02-15 15:09:29 -0600

    Regenerate parse tables.

--- blaze/datashape/dlex.py
+++ blaze/datashape/dlex.py
@@ -4,6 +4,6 @@ _lextokens    = {'LBRACE': 1, 'NAME': 1, 'SEMI': 1, 'EQUALS': 1, 'NUMBER': 1, 'C
 _lexreflags   = 0
 _lexliterals  = '=,():{}'
 _lexstateinfo = {'INITIAL': 'inclusive'}
-_lexstatere   = {'INITIAL': [('(?P<t_TYPE>type)|(?P<t_newline>\\n+)|(?P<t_NAME>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<t_COMMENT>\\#.*)|(?P<t_NUMBER>\\d+)|(?P<t_RBRACE>\\})|(?P<t_LBRACE>\\{)|(?P<t_COLON>:)|(?P<t_COMMA>,)|(?P<t_SEMI>;)|(?P<t_EQUALS>=)', [None, ('t_TYPE', 'TYPE'), ('t_newline', 'newline'), ('t_NAME', 'NAME'), ('t_COMMENT', 'COMMENT'), ('t_NUMBER', 'NUMBER'), (None, 'RBRACE'), (None, 'LBRACE'), (None, 'COLON'), (None, 'COMMA'), (None, 'SEMI'), (None, 'EQUALS')])]}
+_lexstatere   = {'INITIAL': [('(?P<t_TYPE>type)|(?P<t_newline>\\n+)|(?P<t_NAME>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<t_COMMENT>\\#.*)|(?P<t_NUMBER>\\d+)|(?P<t_RBRACE>\\})|(?P<t_LBRACE>\\{)|(?P<t_SEMI>;)|(?P<t_EQUALS>=)|(?P<t_COLON>:)|(?P<t_COMMA>,)', [None, ('t_TYPE', 'TYPE'), ('t_newline', 'newline'), ('t_NAME', 'NAME'), ('t_COMMENT', 'COMMENT'), ('t_NUMBER', 'NUMBER'), (None, 'RBRACE'), (None, 'LBRACE'), (None, 'SEMI'), (None, 'EQUALS'), (None, 'COLON'), (None, 'COMMA')])]}
 _lexstateignore = {'INITIAL': '[ ]'}
 _lexstateerrorf = {'INITIAL': 't_error'}
