6f35d065b6664495816abf5ff6c4b4044c4cebb9
ovillellas <oscar.villellas@continuum.io>
2013-02-28 18:41:06 +0100

    rewrote test_dtw.py. It relied on bug in #30 to work. Made it more significant as well.

--- blaze/tests/test_dtw.py
+++ blaze/tests/test_dtw.py
@@ -1,12 +1,28 @@
-from blaze import dshape, ones, zeros, open
+
+from blaze import dshape, fromiter
 from blaze.ts.ucr_dtw import ucr
+from math import sin, pi
+from nose.tools import ok_, eq_
 
 def test_dtw():
-    data  = ones(dshape('100, float32'))
-    query = ones(dshape('100, float32'))
+    # note: ucr.dtw only supports float64 atm
+    count = 100 
+    data  = fromiter((sin(2*pi*i/count) for i in xrange(count)), 'x, float64')
+    query = data[50:60]
 
-    loc, dist = ucr.dtw(data, query, 0.1, 100, verbose=False)
-
+    loc, dist = ucr.dtw(data, query, 0.1, verbose=False)
+    
     # these are stupid, mostly just to check for regressions
-    assert isinstance(loc, int) or isinstance(loc, long)
-    assert isinstance(dist, float)
+    ok_ (isinstance(loc, (int, long)))
+    ok_ (isinstance(dist, float))
+    eq_ (loc, 50)
+    ok_ (dist < 1e-10 and dist >= 0.0)
+
+
+## Local Variables:
+## mode: python
+## coding: utf-8 
+## python-indent: 4
+## tab-width: 4
+## fill-column: 66
+## End:
