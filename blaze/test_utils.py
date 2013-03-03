1f7de3d3549600d8b629e5b6fafe01ee7f120cd7
Stan Seibert <stan@mtrr.org>
2013-03-03 13:47:54 -0500

    Add blaze.test_utils.temp_dir(), a context manager that creates a temporary directory and cleans it up when exiting the block.
    Handy for writing unit tests that need to create files on disk. To
    debug tests, set environment variable BLAZE_TEST_KEEP_TEMP to 1 and
    the temporary directory paths will be printed to stderr and not be
    deleted.

--- blaze/test_utils.py
+++ blaze/test_utils.py
@@ -1,3 +1,8 @@
+import sys
+import os
+import shutil
+import tempfile
+
 class assert_raises(object):
 
     def __init__(self, exc_ty):
@@ -12,3 +17,35 @@ class assert_raises(object):
         if issubclass(type, self.exc_ty):
             return True
         raise AssertionError('Wrong exception raised: ' + repr(value))
+
+class temp_dir(object):
+    '''Context manager that creates a temporary directory and automatically
+    removes the directory and its contents at the end of the block.
+
+    To assist in debugging failing tests, if the environment variable
+    BLAZE_TEST_KEEP_TEMP is set to 1, then the context manager will not delete
+    the temporary directory.  Instead, the name of the directory will be
+    printed to stderr.
+
+    Example usage:
+
+        with temp_dir() as tmpd:
+            temp_filename = os.path.join(tmpd, 'test.txt')
+            with open(temp_filename, 'w') as f:
+                f.write('Test Output')
+    '''
+
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        self.dirname = tempfile.mkdtemp(prefix='blaze_test_')
+        return self.dirname
+
+    def __exit__(self, type, value, traceback):
+        if not bool(os.environ.get('BLAZE_TEST_KEEP_TEMP', False)):
+            shutil.rmtree(self.dirname)
+        else:
+            print >>sys.stderr, '\nKeeping temporary directory:', self.dirname
+
+        return False  # If there was an exception, reraise it.
