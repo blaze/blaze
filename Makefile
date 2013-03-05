c7cfa0767e199c3992df22a20da353f6560907c7
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-03-05 12:05:23 -0600

    Simple NumPy + Blir example

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
 .PHONY: all docs tests build clean web
 
+CC = gcc
+LPYTHON = $(shell python-config --includes)
+CFLAGS = -shared -fPIC -lpthread $(LPYTHON)
+
 all: build
+
+# stupid hack for now
+blir:
+	$(CC) $(CFLAGS) blaze/blir/datashape.c -o blaze/blir/datashape.o
+	$(CC) $(CFLAGS) blaze/blir/prelude.c blaze/blir/datashape.o -o blaze/blir/prelude.so
 
 build:
 	python setup.py build_ext --inplace
---
+++ samples/numpy_blir.py
@@ -0,0 +1,24 @@
+import numpy as np
+from blaze.blir import compile, execute
+
+source = """
+def main(x: array[int], n : int) -> void {
+    var int i;
+    var int j;
+    for i in range(n) {
+        for j in range(n) {
+            x[i,j] = i+j;
+        }
+        x[i-1,j-1] = 10;
+    }
+}
+"""
+
+N = 14
+ast, env = compile(source)
+
+arr = np.eye(N, dtype='int32')
+args = (arr, N)
+
+execute(env, args, timing=True)
+print arr
