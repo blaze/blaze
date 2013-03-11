5fbb5262338e462d34b863f511a6dd009ffb1134
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-03-11 16:12:49 -0500

    Another cgen test.

--- blaze/tests/test_cgen2.py
+++ blaze/tests/test_cgen2.py
@@ -6,7 +6,11 @@ from blaze.blir import compile, assembly, bitcode, Context, execute
 from blaze.cgen.kernels import *
 from blaze.cgen.utils import namesupply
 
-def test_cgen2():
+#------------------------------------------------------------------------
+# Code Generation ( Level 2 )
+#------------------------------------------------------------------------
+
+def test_cgen2_expadd():
     with namesupply():
 
         krn = ElementwiseKernel(
@@ -29,3 +33,27 @@ def test_cgen2():
 
         execute(ctx, args=(a,b,c), fname='kernel0', timing=False)
         assert np.allclose(c, np.exp(a + b))
+
+def test_cgen2_add():
+    with namesupply():
+
+        krn = ElementwiseKernel(
+            [
+                (IN  , VectorArg((300,), 'array[int]')),
+                (IN  , VectorArg((300,), 'array[int]')),
+                (OUT , VectorArg((300,), 'array[int]')),
+            ],
+            '_out0[i0] = _in0[i0] + _in1[i0]',
+        )
+
+        krn.verify()
+        ast, env = krn.compile()
+
+        ctx = Context(env)
+
+        a = np.array(xrange(300), dtype='int32')
+        b = np.array(xrange(300), dtype='int32')
+        c = np.empty_like(b)
+
+        execute(ctx, args=(a,b,c), fname='kernel0', timing=False)
+        assert np.allclose(c, a + b)
