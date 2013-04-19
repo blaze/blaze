c3f92d0f6cb7e79ce7a0d5d6aae0f02339af3c74
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-04-19 14:44:44 -0500

    Added blir prelude to setup.py

--- setup.py
+++ setup.py
@@ -178,6 +178,12 @@ extensions = [
         define_macros=def_macros,
    ),
 
+   Extension(
+        "blaze.blir.prelude",
+        sources = ["blaze/blir/prelude.c"],
+        depends = [],
+        include_dirs = [],
+   ),
 ]
 
 #------------------------------------------------------------------------
