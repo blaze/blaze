6baa8b81e2dc324f9d7632ebe14748a19b1ec058
Mark Wiebe <mwwiebe@gmail.com>
2013-04-26 14:29:13 -0700

    Add untested initial implementation of a numpy datadescriptor

--- blaze/__init__.py
+++ blaze/__init__.py
@@ -2,3 +2,4 @@
 # build the blaze namespace with selected functions
 
 from constructors import array, open
+from datashape import dshape
---
+++ blaze/datadescriptor/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .datadescriptor import DataDescriptor, IGetDescriptor, \
+                IDescriptorIter, IGetElement, IElementIter
+
