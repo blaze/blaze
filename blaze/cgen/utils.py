c8c7327e6599514462f51165ef922268ed27f19c
Mark Wiebe <mwwiebe@gmail.com>
2013-05-03 23:10:45 -0700

    Add collections.Counter implementation for python 2.6

--- blaze/cgen/utils.py
+++ blaze/cgen/utils.py
@@ -1,6 +1,10 @@
+import sys
 import string
-from collections import Counter
 from contextlib import contextmanager
+if sys.version_info >= (2, 7):
+    from collections import Counter
+else:
+    from counter_py26 import Counter
 
 #------------------------------------------------------------------------
 # Naming
