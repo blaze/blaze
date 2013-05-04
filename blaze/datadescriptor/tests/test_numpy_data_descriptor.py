86ba5ee35dc8fa4597f0f35d57539a03d68c6756
Mark Wiebe <mwwiebe@gmail.com>
2013-05-03 23:13:08 -0700

    assertIsInstance is only in py27 and later

--- blaze/datadescriptor/tests/test_numpy_data_descriptor.py
+++ blaze/datadescriptor/tests/test_numpy_data_descriptor.py
@@ -13,7 +13,7 @@ class TestNumPyDataDescriptor(unittest.TestCase):
         a = np.arange(6).reshape(2,3)
         npdd = NumPyDataDescriptor(a)
         # Make sure the right type is returned
-        self.assertIsInstance(npdd, DataDescriptor)
+        self.assertTrue(isinstance(npdd, DataDescriptor))
 
     def test_descriptor_iter_types(self):
         a = np.arange(6).reshape(2,3)
@@ -21,16 +21,16 @@ class TestNumPyDataDescriptor(unittest.TestCase):
 
         # Iteration should produce NumPyDataDescriptor instances
         for el in npdd:
-            self.assertIsInstance(el, NumPyDataDescriptor)
-            self.assertIsInstance(el, DataDescriptor)
+            self.assertTrue(isinstance(el, NumPyDataDescriptor))
+            self.assertTrue(isinstance(el, DataDescriptor))
 
     def test_descriptor_getitem_types(self):
         a = np.arange(6).reshape(2,3)
         npdd = NumPyDataDescriptor(a)
 
         # Indexing should produce NumPyDataDescriptor instances
-        self.assertIsInstance(npdd[0], NumPyDataDescriptor)
-        self.assertIsInstance(npdd[1,2], NumPyDataDescriptor)
+        self.assertTrue(isinstance(npdd[0], NumPyDataDescriptor))
+        self.assertTrue(isinstance(npdd[1,2], NumPyDataDescriptor))
 
     def test_element_iter_types(self):
         a = np.arange(6).reshape(2,3)
@@ -39,11 +39,11 @@ class TestNumPyDataDescriptor(unittest.TestCase):
         # Requesting element iteration should produce an
         # IElementIter object
         ei = npdd.element_iter_interface()
-        self.assertIsInstance(ei, IElementIter)
+        self.assertTrue(isinstance(ei, IElementIter))
         # Iteration over the IElementIter object should produce
         # ctypes c_void_p pointers
         for ptr in ei:
-            self.assertIsInstance(ptr, ctypes.c_void_p)
+            self.assertTrue(isinstance(ptr, ctypes.c_void_p))
 
     def test_element_getitem_types(self):
         a = np.arange(6).reshape(2,3)
@@ -52,18 +52,18 @@ class TestNumPyDataDescriptor(unittest.TestCase):
         # Requesting get_element with one index should produce an
         # IGetElement object
         ge = npdd.get_element_interface(1)
-        self.assertIsInstance(ge, IGetElement)
+        self.assertTrue(isinstance(ge, IGetElement))
         # Indexing the IGetElement object should produce
         # ctypes c_void_p pointers
-        self.assertIsInstance(ge.get((1,)), ctypes.c_void_p)
+        self.assertTrue(isinstance(ge.get((1,)), ctypes.c_void_p))
 
         # Requesting get_element with two indices should produce an
         # IGetElement object
         ge = npdd.get_element_interface(2)
-        self.assertIsInstance(ge, IGetElement)
+        self.assertTrue(isinstance(ge, IGetElement))
         # Indexing the IGetElement object should produce
         # ctypes c_void_p pointers
-        self.assertIsInstance(ge.get((1,2)), ctypes.c_void_p)
+        self.assertTrue(isinstance(ge.get((1,2)), ctypes.c_void_p))
 
 if __name__ == '__main__':
     unittest.main()
