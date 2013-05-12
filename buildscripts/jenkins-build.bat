e17292548bfa7c0951f9a1dd2020c231a8ac43c7
Mark Wiebe <mwwiebe@gmail.com>
2013-05-12 11:41:26 -0700

    Add numba to the build environment

--- buildscripts/jenkins-build.bat
+++ buildscripts/jenkins-build.bat
@@ -21,7 +21,7 @@ REM python version and containing the dependencies.
 SET PYENV_PREFIX=%WORKSPACE%\build\pyenv
 rd /q /s %PYENV_PREFIX%
 REM TODO: Add cffi to this list once it is added to anaconda windows.
-call C:\Anaconda\Scripts\conda create --yes -p %PYENV_PREFIX% python=%PYTHON_VERSION%  cython=0.19 scipy llvmpy ply
+call C:\Anaconda\Scripts\conda create --yes -p %PYENV_PREFIX% python=%PYTHON_VERSION%  cython=0.19 scipy llvmpy ply numba
 IF %ERRORLEVEL% NEQ 0 exit /b 1
 echo on
 set PYTHON_EXECUTABLE=%PYENV_PREFIX%\Python.exe
