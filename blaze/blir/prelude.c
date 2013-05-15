6451feb864c6909d07e3e41f554542ea06d91db0
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-05-15 12:06:40 -0500

    No reason for prelude to need Python.h

--- blaze/blir/prelude.c
+++ blaze/blir/prelude.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "Python.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -46,11 +44,17 @@ char* append(char* x, char* y)
 
 int strHead(char* str)
 {
+    if (str[0]=='\0') {
+        return NULL;
+    }
     return (int)(str[0]);
 }
 
 char* strTail(char* str)
 {
+    if (str[0]=='\0') {
+        return NULL;
+    }
     return str+1;
 }
 
@@ -98,29 +102,6 @@ void show_bool(int b)
 void show_array(ndarray *a) {
     printf("array(%p)", a->data);
 }
-
-#if PY_VERSION_HEX >= 0x03000000
-PyMODINIT_FUNC
-PyInit_prelude(void)
-{
-    PyObject *m;
-
-    m = PyModule_Create(NULL);
-    if (m == NULL)
-        return NULL;
-    return m;
-}
-#else
-PyMODINIT_FUNC
-initprelude(void)
-{
-    PyObject *m;
-
-    m = Py_InitModule("prelude", NULL);
-    if (m == NULL)
-        return;
-}
-#endif
 
 #ifdef __cplusplus
 }
