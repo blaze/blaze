21de6c75455ab953977dfe8a61320d78727e993e
Mark Wiebe <mwwiebe@gmail.com>
2013-05-16 17:54:42 -0700

    Add a test for the dynd data descriptor

--- blaze/datadescriptor/tests/test_numpy_data_descriptor.py
+++ blaze/datadescriptor/tests/test_numpy_data_descriptor.py
@@ -3,67 +3,75 @@ import unittest
 import numpy as np
 import blaze
 from blaze import datashape
-from blaze.datadescriptor import NumPyDataDescriptor, \
-                IDataDescriptor, IElementReader, IElementReadIter
+from blaze.datadescriptor import (NumPyDataDescriptor,
+                IDataDescriptor, IElementReader, IElementReadIter,
+                dd_as_py)
+from ...py3help import _inttypes
 import ctypes
 
 class TestNumPyDataDescriptor(unittest.TestCase):
     def test_basic_object_type(self):
         self.assertTrue(issubclass(NumPyDataDescriptor, IDataDescriptor))
         a = np.arange(6).reshape(2,3)
-        npdd = NumPyDataDescriptor(a)
+        dd = NumPyDataDescriptor(a)
         # Make sure the right type is returned
-        self.assertTrue(isinstance(npdd, IDataDescriptor))
+        self.assertTrue(isinstance(dd, IDataDescriptor))
+        self.assertEqual(dd_as_py(dd), [[0,1,2], [3,4,5]])
 
     def test_descriptor_iter_types(self):
         a = np.arange(6).reshape(2,3)
-        npdd = NumPyDataDescriptor(a)
+        dd = NumPyDataDescriptor(a)
 
         # Iteration should produce NumPyDataDescriptor instances
-        for el in npdd:
+        vals = []
+        for el in dd:
             self.assertTrue(isinstance(el, NumPyDataDescriptor))
             self.assertTrue(isinstance(el, IDataDescriptor))
+            vals.append(dd_as_py(el))
+        self.assertEqual(vals, [[0,1,2], [3,4,5]])
 
     def test_descriptor_getitem_types(self):
         a = np.arange(6).reshape(2,3)
-        npdd = NumPyDataDescriptor(a)
+        dd = NumPyDataDescriptor(a)
 
         # Indexing should produce NumPyDataDescriptor instances
-        self.assertTrue(isinstance(npdd[0], NumPyDataDescriptor))
-        self.assertTrue(isinstance(npdd[1,2], NumPyDataDescriptor))
+        self.assertTrue(isinstance(dd[0], NumPyDataDescriptor))
+        self.assertEqual(dd_as_py(dd[0]), [0,1,2])
+        self.assertTrue(isinstance(dd[1,2], NumPyDataDescriptor))
+        self.assertEqual(dd_as_py(dd[1,2]), 5)
 
     def test_element_iter_types(self):
         a = np.arange(6).reshape(2,3)
-        npdd = NumPyDataDescriptor(a)
+        dd = NumPyDataDescriptor(a)
 
         # Requesting element iteration should produce an
         # IElementReadIter object
-        ei = npdd.element_read_iter()
+        ei = dd.element_read_iter()
         self.assertTrue(isinstance(ei, IElementReadIter))
         # Iteration over the IElementReadIter object should produce
-        # ctypes c_void_p pointers
+        # raw ints which are pointers
         for ptr in ei:
-            self.assertTrue(isinstance(ptr, ctypes.c_void_p))
+            self.assertTrue(isinstance(ptr, _inttypes))
 
     def test_element_getitem_types(self):
         a = np.arange(6).reshape(2,3)
-        npdd = NumPyDataDescriptor(a)
+        dd = NumPyDataDescriptor(a)
 
         # Requesting get_element with one index should produce an
         # IElementReader object
-        ge = npdd.element_reader(1)
+        ge = dd.element_reader(1)
         self.assertTrue(isinstance(ge, IElementReader))
-        # Indexing the IElementReader object should produce
-        # ctypes c_void_p pointers
-        self.assertTrue(isinstance(ge.read_single((1,)), ctypes.c_void_p))
+        # Iteration over the IElementReadIter object should produce
+        # raw ints which are pointers
+        self.assertTrue(isinstance(ge.read_single((1,)), _inttypes))
 
         # Requesting element reader with two indices should produce an
         # IElementReader object
-        ge = npdd.element_reader(2)
+        ge = dd.element_reader(2)
         self.assertTrue(isinstance(ge, IElementReader))
-        # Indexing the IElementReader object should produce
-        # ctypes c_void_p pointers
-        self.assertTrue(isinstance(ge.read_single((1,2)), ctypes.c_void_p))
+        # Iteration over the IElementReadIter object should produce
+        # raw ints which are pointers
+        self.assertTrue(isinstance(ge.read_single((1,2)), _inttypes))
 
 if __name__ == '__main__':
     unittest.main()
