df1ed7f32e2c9a4cae1d6b12d97bdce8cce3d626
Mark Wiebe <mwwiebe@gmail.com>
2013-05-17 17:43:11 -0700

    Fix ckernel __del__ for case of errors during construction

--- blaze/ckernel/tests/test_ckernel.py
+++ blaze/ckernel/tests/test_ckernel.py
@@ -5,9 +5,6 @@ from blaze import ckernel
 class TestCKernel(unittest.TestCase):
     def test_constructor_errors(self):
         self.assertRaises(TypeError, ckernel.CKernel, "wrong", "types")
-        self.assertRaises(ValueError, ckernel.CKernel,
-                        ckernel.DynamicKernelInstanceP(),
-                        ckernel.UnarySingleOperation)
 
 if __name__ == '__main__':
     unittest.main()
