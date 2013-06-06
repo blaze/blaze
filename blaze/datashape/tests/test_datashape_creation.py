90a603d2807484f7e858e8a0f7d1753df25d0a7c
Mark Wiebe <mwwiebe@gmail.com>
2013-06-06 12:11:26 -0700

    Add test for issue 11

--- blaze/datashape/tests/test_datashape_creation.py
+++ blaze/datashape/tests/test_datashape_creation.py
@@ -9,6 +9,8 @@ class TestDatashapeCreation(unittest.TestCase):
         # Make sure it raises exceptions on a few nonsense inputs
         self.assertRaises(TypeError, blaze.dshape, None)
         self.assertRaises(TypeError, blaze.dshape, lambda x: x+1)
+        # Check issue 11
+        self.assertRaises(datashape.DatashapeSyntaxError, blaze.dshape, '1,')
 
     def test_atom_shapes(self):
         self.assertEqual(blaze.dshape('bool'), datashape.bool_)
