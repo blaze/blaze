fa543a35015b2dbd49a6b2829a130f2a8eb9fd19
Francesc Alted <francesc@continuum.io>
2013-07-22 17:16:40 -0400

    Syncing vtable branch with master

--- blaze/blz/vtable.py
+++ blaze/blz/vtable.py
@@ -14,7 +14,7 @@ btables in a single, virtual one.
 from __future__ import absolute_import
 import numpy as np
 
-from ..py3help import _inttypes, imap, xrange
+from ..py2help import _inttypes, imap, xrange
 _inttypes += (np.integer,)
 
 # BLZ utilities
