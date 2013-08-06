b28aba7c1075e5ffb24ff1bc2f38076db4ba7343
Mark Wiebe <mwwiebe@gmail.com>
2013-08-06 14:03:52 -0700

    Move some llvm-related code from blaze_kernels.py to llutil.py

--- blaze/bkernel/blaze_kernels.py
+++ blaze/bkernel/blaze_kernels.py
@@ -35,8 +35,8 @@ from ..ckernel import (ExprSingleOperation, JITKernelData,
 from ..py2help import izip, _strtypes, c_ssize_t, PY2
 from ..datashape import Fixed, TypeVar
 from ..datashape.util import to_ctypes, dshape as make_dshape
-
-int32_type = Type.int(32)
+from .llutil import (int32_type, int8_p_type, single_ckernel_func_type,
+                map_llvm_to_ctypes)
 
 arg_kinds = (SCALAR, POINTER) + array_kinds
 
@@ -46,100 +46,6 @@ for this in array_kinds:
 del this, _g
 
 _invmap = {}
-
-def map_llvm_to_ctypes(llvm_type, py_module=None, sname=None, i8p_str=False):
-    '''
-    Map an LLVM type to an equivalent ctypes type. py_module is an
-    optional module that is used for structure wrapping.  If
-    structures are found, the struct definitions will be created in
-    that module.
-    '''
-    kind = llvm_type.kind
-    if kind == lc.TYPE_INTEGER:
-        ctype = getattr(ctypes,"c_int"+str(llvm_type.width))
-
-    elif kind == lc.TYPE_DOUBLE:
-        ctype = ctypes.c_double
-
-    elif kind == lc.TYPE_FLOAT:
-        ctype = ctypes.c_float
-
-    elif kind == lc.TYPE_VOID:
-        ctype = None
-
-    elif kind == lc.TYPE_POINTER:
-        pointee = llvm_type.pointee
-        p_kind = pointee.kind
-        if p_kind == lc.TYPE_INTEGER:
-            width = pointee.width
-
-            # Special case:  char * is mapped to strings
-            if width == 8 and i8p_str:
-                ctype = ctypes.c_char_p
-            else:
-                ctype = ctypes.POINTER(map_llvm_to_ctypes(pointee, py_module, sname))
-
-        # Special case: void * mapped to c_void_p type
-        elif p_kind == lc.TYPE_VOID:
-            ctype = ctypes.c_void_p
-        else:
-            ctype = ctypes.POINTER(map_llvm_to_ctypes(pointee, py_module, sname))
-
-    elif kind == lc.TYPE_ARRAY:
-        ctype = llvm_type.count * map_llvm_to_ctypes(llvm_type.element, py_module, sname)
-    elif kind == lc.TYPE_STRUCT:
-        lookup = True
-        if llvm_type.is_literal:
-            if sname:
-                struct_name = sname
-            else:
-                struct_name = 'llvm_struct'
-                lookup = False
-        else:
-            struct_name = llvm_type.name
-            struct_name = struct_name.replace('.','_')
-        if PY2:
-            struct_name = struct_name.encode('ascii')
-
-        # If the named type is already known, return it
-        if py_module and lookup:
-            struct_type = getattr(py_module, struct_name, None)
-        else:
-            struct_type = None
-
-        if struct_type and issubclass(struct_type, ctypes.Structure):
-            return struct_type
-
-        # If there is an object with the name of the structure already
-        # present and it has the field names specified, use those names
-        # to help out
-        if hasattr(struct_type, '_fields_'):
-            names = struct_type._fields_
-        else:
-            names = [ "e"+str(n) for n in range(llvm_type.element_count) ]
-
-        # Create a class definition for the type. It is critical that this
-        # Take place before the handling of members to avoid issues with
-        # self-referential data structures
-        if py_module and lookup:
-            type_dict = { '__module__' : py_module.__name__}
-        else:
-            type_dict = {}
-        ctype = type(ctypes.Structure)(struct_name, (ctypes.Structure,),
-                                       type_dict)
-        if py_module and lookup:
-            setattr(py_module, struct_name, ctype)
-
-        # Resolve the structure fields
-        fields = [ (name, map_llvm_to_ctypes(elem, py_module))
-                   for name, elem in zip(names, llvm_type.elements) ]
-
-        # Set the fields member of the type last.  The order is critical
-        # to deal with self-referential structures.
-        setattr(ctype, '_fields_', fields)
-    else:
-        raise TypeError("Unknown type %s" % kind)
-    return ctype
 
 class BlazeElementKernel(object):
     """
@@ -325,12 +231,9 @@ class BlazeElementKernel(object):
         """Creates an UnboundCKernelFunction with the ExprSingleOperation prototype.
         """
         import ctypes
-        i8_p_type = Type.pointer(Type.int(8))
-        func_type = Type.function(void_type,
-                        [i8_p_type, Type.pointer(i8_p_type), i8_p_type])
         module = self.module.clone()
         single_ck_func_name = self.func.name +"_single_ckernel"
-        single_ck_func = Function.new(module, func_type,
+        single_ck_func = Function.new(module, single_ckernel_func_type,
                                           name=single_ck_func_name)
         block = single_ck_func.append_basic_block('entry')
         builder = lc.Builder.new(block)
@@ -342,7 +245,7 @@ class BlazeElementKernel(object):
         # adding a shape field for each array argument. First comes
         # the kernel data prefix with a spot for the 'owner' reference added.
         input_field_indices = []
-        kernel_data_fields = [Type.struct([i8_p_type]*3)]
+        kernel_data_fields = [Type.struct([int8_p_type]*3)]
         kernel_data_ctypes_fields = [('base', JITKernelData)]
         for i, (kind, a) in enumerate(izip(self.kinds, self.argtypes)):
             if isinstance(kind, tuple):
