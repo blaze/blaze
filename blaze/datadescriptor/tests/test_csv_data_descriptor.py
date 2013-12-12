fda7e827785bdb433f5ad6a884ea6adc6f6b04de
Francesc Alted <francesc@continuum.io>
2013-12-12 10:03:50 +0100

    Fixed the use of kwargs in tests

--- blaze/datadescriptor/tests/test_csv_data_descriptor.py
+++ blaze/datadescriptor/tests/test_csv_data_descriptor.py
@@ -21,7 +21,7 @@ csv_schema = "{ f0: string; f1: string; f2: int16; f3: bool }"
 class TestCSVDataDescriptor(unittest.TestCase):
     def test_basic_object_type(self):
         self.assertTrue(issubclass(CSVDataDescriptor, IDataDescriptor))
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         self.assertTrue(isinstance(dd, IDataDescriptor))
         self.assertEqual(dd_as_py(dd), [
             {u'f0': u'k1', u'f1': u'v1', u'f2': 1, u'f3': False},
@@ -29,7 +29,7 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k3', u'f1': u'v3', u'f2': 3, u'f3': False}])
 
     def test_iter(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         # This equality does not work yet
         # self.assertEqual(dd.dshape, datashape.dshape(
         #     'Var, %s' % csv_schema))
@@ -46,7 +46,7 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k3', u'f1': u'v3', u'f2': 3, u'f3': False}])
 
     def test_iterchunks(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
 
         # Iteration should produce DyNDDataDescriptor instances
         vals = []
@@ -60,7 +60,7 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k3', u'f1': u'v3', u'f2': 3, u'f3': False}])
 
     def test_iterchunks_start(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         vals = []
         for el in dd.iterchunks(blen=2, start=1):
             vals.extend(dd_as_py(el))
@@ -69,14 +69,14 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k3', u'f1': u'v3', u'f2': 3, u'f3': False}])
 
     def test_iterchunks_stop(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         vals = [dd_as_py(v) for v in dd.iterchunks(blen=1, stop=2)]
         self.assertEqual(vals, [
             {u'f0': u'k1', u'f1': u'v1', u'f2': 1, u'f3': False},
             {u'f0': u'k2', u'f1': u'v2', u'f2': 2, u'f3': True}])
 
     def test_iterchunks_start_stop(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         vals = [dd_as_py(v) for v in dd.iterchunks(blen=1, start=1, stop=2)]
         self.assertEqual(vals, [
             {u'f0': u'k2', u'f1': u'v2', u'f2': 2, u'f3': True}])
@@ -84,14 +84,14 @@ class TestCSVDataDescriptor(unittest.TestCase):
     def test_append(self):
         # Get a private stream so as to not mess the original one
         stream = io.StringIO(csv_buf)
-        dd = CSVDataDescriptor(stream, csv_schema)
+        dd = CSVDataDescriptor(stream, schema=csv_schema)
         dd.append(["k4", "v4", 4, True])
         vals = [dd_as_py(v) for v in dd.iterchunks(blen=1, start=3)]
         self.assertEqual(vals, [
             {u'f0': u'k4', u'f1': u'v4', u'f2': 4, u'f3': True}])
 
     def test_getitem_start(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         el = dd[0]
         self.assertTrue(isinstance(el, DyNDDataDescriptor))
         vals = dd_as_py(el)
@@ -99,7 +99,7 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k1', u'f1': u'v1', u'f2': 1, u'f3': False}])
 
     def test_getitem_stop(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         el = dd[:1]
         self.assertTrue(isinstance(el, DyNDDataDescriptor))
         vals = dd_as_py(el)
@@ -107,7 +107,7 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k1', u'f1': u'v1', u'f2': 1, u'f3': False}])
 
     def test_getitem_step(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         el = dd[::2]
         self.assertTrue(isinstance(el, DyNDDataDescriptor))
         vals = dd_as_py(el)
@@ -116,7 +116,7 @@ class TestCSVDataDescriptor(unittest.TestCase):
             {u'f0': u'k3', u'f1': u'v3', u'f2': 3, u'f3': False}])
 
     def test_getitem_start_step(self):
-        dd = CSVDataDescriptor(csv_file, csv_schema)
+        dd = CSVDataDescriptor(csv_file, schema=csv_schema)
         el = dd[1::2]
         self.assertTrue(isinstance(el, DyNDDataDescriptor))
         vals = dd_as_py(el)
