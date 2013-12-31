a634d7c549ed0f2f492ef70be011651411f207bc
Mark Wiebe <mwiebe@continuum.io>
2013-12-31 12:04:52 -0800

    Add file for remote data descriptor

---
+++ blaze/datadescriptor/remote_data_descriptor.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function
+
+class RemoteDataDescriptor(IDataDescriptor):
+    """
+    A Blaze data descriptor which exposes an array on another
+    server.
+    """
+
+
+
