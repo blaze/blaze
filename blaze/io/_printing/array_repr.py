fb1a5f892e684143cd254447b5e1607a4b9e2c03
Mark Wiebe <mwiebe@continuum.io>
2014-01-14 15:21:28 -0800

    Revert repr change, it looks worse for small example arrays

--- blaze/io/_printing/array_repr.py
+++ blaze/io/_printing/array_repr.py
@@ -5,18 +5,16 @@ from ...datadescriptor import RemoteDataDescriptor
 
 
 def array_repr(a):
+    pre = 'array('
+    post = ',\n' + ' '*len(pre) + "dshape='" + str(a.dshape) + "'" + ')'
+
     # TODO: create a mechanism for data descriptor to override
     #       printing.
     if isinstance(a._data, RemoteDataDescriptor):
         body = 'RemoteDataDescriptor(%r)' % a._data.url
     else:
-        body = _arrayprint.array2string(a._data, separator=', ')
-
-    pre = 'array('
-    post = ',\n' + ' '*len(pre) + "dshape='" + str(a.dshape) + "'" + ')'
-
-    # For a multi-line, start it on the next line so things align properly
-    if '\n' in body:
-        pre += '\n'
+        body = _arrayprint.array2string(a._data,
+                          separator=', ',
+                          prefix=' '*len(pre))
 
     return pre + body + post
