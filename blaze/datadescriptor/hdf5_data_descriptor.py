49fe28a192521cbbfad6e594fdb0c36c859beb07
Francesc Alted <francesc@continuum.io>
2014-01-22 17:45:02 +0100

    First test for HDF5 passing

--- blaze/datadescriptor/hdf5_data_descriptor.py
+++ blaze/datadescriptor/hdf5_data_descriptor.py
@@ -5,7 +5,7 @@ from dynd import nd
 import datashape
 
 from . import IDataDescriptor, Capabilities
-import tables
+import tables as tb
 from .dynd_data_descriptor import DyNDDataDescriptor
 
 
@@ -19,30 +19,45 @@ def hdf5_descriptor_iter(h5arr):
         # to a scalar, this is a way to avoid that
         el = np.array(h5arr[i], dtype=h5arr.dtype)
         yield DyNDDataDescriptor(nd.array(el))
+    h5arr._v_file.close()
 
 
 class HDF5DataDescriptor(IDataDescriptor):
     """
     A Blaze data descriptor which exposes a HDF5 dataset.
     """
-    def __init__(self, obj):
+
+    def _open(self):
+        self.f = tb.open_file(self.filename)
+        return self.f.get_node(self.f.root, self.datapath)
+
+    def _close(self):
+        self.f.close()
+
+    def __init__(self, filename, datapath):
+        self.filename = filename
+        self.datapath = datapath
+        obj = self._open()
         # We are going to support both homogeneous and heterogeneous
         # datasets, but not VL types (VLArray) for the time being.
         if not isinstance(obj, (tb.Array, tb.Table)):
             raise TypeError(('object is not a supported HDF5 dataset, '
                              'it has type %r') % type(obj))
-        self.h5arr = obj
+        self._close()
 
     @property
     def dshape(self):
         # This cannot be cached because the Array can change the dshape
-        obj = self.h5arr
-        return datashape.from_numpy(obj.shape, obj.dtype)
+        h5arr = self._open()
+        odshape = datashape.from_numpy(h5arr.shape, h5arr.dtype)
+        self._close()
+        return odshape
 
     @property
     def capabilities(self):
         """The capabilities for the HDF5 arrays."""
-        return Capabilities(
+        h5arr = self._open()
+        caps = Capabilities(
             # HDF5 arrays can be updated
             immutable = False,
             # HDF5 arrays are concrete
@@ -50,28 +65,48 @@ class HDF5DataDescriptor(IDataDescriptor):
             # HDF5 arrays are persistent
             persistent = True,
             # HDF5 arrays can be appended efficiently for EArrays and Tables
-            appendable = isinstance(self.h5arr, (tb.EArray, tb.Table),
+            appendable = isinstance(h5arr, (tb.EArray, tb.Table)),
             remote = False,
             )
+        self._close()
+        return caps
+
+    def dynd_arr(self):
+        # Positionate at the beginning of the file
+        h5arr = self._open()
+        h5arr = nd.array(h5arr[:], dtype=h5arr.dtype)
+        self._close()
+        return h5arr
 
     def __array__(self):
-        return np.array(self.h5arr)
+        h5arr = self._open()
+        h5arr = h5arr[:]
+        self._close()
+        return h5arr
 
     def __len__(self):
-        return len(self.h5arr)
+        h5arr = self._open()
+        arrlen = len(h5arr)
+        self._close()
+        return arrlen
 
     def __getitem__(self, key):
-        h5arr = self.h5arr
+        h5arr = self._open()
+        h5arr = h5arr[key]
+        self._close()
         # The returned arrays are temporary buffers,
         # so must be flagged as readonly.
-        return DyNDDataDescriptor(nd.asarray(h5arr[key], access='readonly'))
+        return DyNDDataDescriptor(nd.asarray(h5arr, access='readonly'))
 
     def __setitem__(self, key, value):
         # HDF5 arrays can be updated
-        self.h5arr[key] = value
+        h5arr = self._open()
+        h5arr[key] = value
+        self._close()
 
     def __iter__(self):
-        return hdf5_descriptor_iter(self.h5arr)
+        h5arr = self._open()
+        return hdf5_descriptor_iter(h5arr)
 
     def append(self, values):
         """Append a list of values."""
@@ -83,6 +118,7 @@ class HDF5DataDescriptor(IDataDescriptor):
         if len(shape_vals) != len(shape):
             raise ValueError("shape of values is not compatible")
         # Now, do the actual append
-        self.h5arr.append(values_arr.reshape(shape_vals))
-        self.h5arr.flush()
-
+        h5arr = self._open()
+        h5arr.append(values_arr.reshape(shape_vals))
+        h5arr.flush()
+        self._close()
