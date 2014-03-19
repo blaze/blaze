7e7a44e812224db75a48ceddf3483949bb52877c
Francesc Alted <francesc@continuum.io>
2014-03-19 14:32:48 +0100

    Added more tests for different combinations of VMs and array objects

--- blaze/compute/tests/test_elwise_eval.py
+++ blaze/compute/tests/test_elwise_eval.py
@@ -10,6 +10,7 @@ import blaze
 from common import MayBeDiskTest
 
 
+# Check for arrays that fit in the chunk size
 class evalTest(unittest.TestCase):
     vm = "numexpr"  # if numexpr not available, it will fall back to python
     N = 1000
@@ -64,28 +65,31 @@ class evalTest(unittest.TestCase):
         assert_array_equal(cr[:], nr, "eval does not work correctly")
 
 
-# Check for arrays that fit in memory, but are larger than a chunk
+# Check for arrays that are larger than a chunk
 class evalLargeTest(evalTest):
     N = 10000
 
-# Check for arrays that fit in memory, but are larger than a chunk
+# Check for arrays that fit in the chunk size
+# Using the Python VM (i.e. Blaze machinery) here
 class evalPythonTest(evalTest):
     vm = "python"
 
-# Check for arrays that fit in memory, but are larger than a chunk
+# Check for arrays that are larger than a chunk
+# Using the Python VM (i.e. Blaze machinery) here
 class evalPythonLargeTest(evalTest):
     N = 10000
     vm = "python"
 
 
-# Check for arrays that are stored on-disk
+# Check for arrays stored on-disk, but fit in a chunk
+# Check for arrays that fit in memory
 class storageTest(MayBeDiskTest):
     N = 1000
-    vm = "python"
+    vm = "numexpr"
     disk = True
 
     def test00(self):
-        """Testing elwise_eval() with only scalars and constants"""
+        """Testing elwise_eval() with only blaze arrays"""
         a, b = np.arange(self.N), np.arange(1, self.N+1)
         c = blaze.array(a, storage=self.store1)
         d = blaze.array(b, storage=self.store2)
@@ -93,8 +97,45 @@ class storageTest(MayBeDiskTest):
         nr = a * b
         assert_array_equal(cr[:], nr, "eval does not work correctly")
 
+    def test01(self):
+        """Testing elwise_eval() with blaze arrays and constants"""
+        a, b = np.arange(self.N), np.arange(1, self.N+1)
+        c = blaze.array(a, storage=self.store1)
+        d = blaze.array(b, storage=self.store2)
+        cr = blaze._elwise_eval("c * d + 1", vm=self.vm, storage=self.store3)
+        nr = a * b + 1
+        assert_array_equal(cr[:], nr, "eval does not work correctly")
 
-# Check for arrays stored on-disk but are larger than a chunk
+    def test03(self):
+        """Testing elwise_eval() with blaze and dynd arrays"""
+        a, b = np.arange(self.N), np.arange(1, self.N+1)
+        c = blaze.array(a, storage=self.store1)
+        d = nd.array(b)
+        cr = blaze._elwise_eval("c * d + 1", vm=self.vm, storage=self.store3)
+        nr = a * b + 1
+        assert_array_equal(cr[:], nr, "eval does not work correctly")
+
+    def test04(self):
+        """Testing elwise_eval() with blaze, dynd and numpy arrays"""
+        a, b = np.arange(self.N), np.arange(1, self.N+1)
+        c = blaze.array(a, storage=self.store1)
+        d = nd.array(b)
+        cr = blaze._elwise_eval("a * c + d", vm=self.vm, storage=self.store3)
+        nr = a * c + d
+        assert_array_equal(cr[:], nr, "eval does not work correctly")
+
+
+# Check for arrays stored on-disk, but are larger than a chunk
+class storageLargeTest(storageTest):
+    N = 10000
+
+# Check for arrays stored on-disk, but fit in a chunk
+# Using the Python VM (i.e. Blaze machinery) here
+class storagePythonTest(storageTest):
+    vm = "python"
+
+# Check for arrays stored on-disk, but are larger than a chunk
+# Using the Python VM (i.e. Blaze machinery) here
 class storageLargeTest(storageTest):
     N = 10000
     vm = "python"
