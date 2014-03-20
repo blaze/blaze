f8f97cfb2c643d971644f71d1c6abeb7486e0b7c
Francesc Alted <francesc@continuum.io>
2014-03-20 17:10:22 +0100

    Moved MayBeDiskTest into test_elwise_eval, as it is the only one that uses it.

--- blaze/compute/tests/test_elwise_eval.py
+++ blaze/compute/tests/test_elwise_eval.py
@@ -7,7 +7,51 @@ from numpy.testing import assert_array_equal, assert_allclose
 
 from dynd import nd, ndt
 import blaze
-from .common import MayBeDiskTest
+
+import unittest
+import tempfile
+import os, os.path
+import glob
+import shutil
+import blaze
+
+
+def remove_tree(rootdir):
+    # Remove every directory starting with rootdir
+    for dir_ in glob.glob(rootdir+'*'):
+        shutil.rmtree(dir_)
+
+# Useful superclass for disk-based tests
+class MayBeDiskTest(unittest.TestCase):
+
+    disk = False
+
+    def setUp(self):
+        if self.disk:
+            prefix = 'blaze-' + self.__class__.__name__
+            suffix = '.blz'
+            self.rootdir1 = tempfile.mkdtemp(suffix=suffix, prefix=prefix)
+            os.rmdir(self.rootdir1)
+            self.store1 = blaze.Storage(self.rootdir1)
+            self.rootdir2 = tempfile.mkdtemp(suffix=suffix, prefix=prefix)
+            os.rmdir(self.rootdir2)
+            self.store2 = blaze.Storage(self.rootdir2)
+            self.rootdir3 = tempfile.mkdtemp(suffix=suffix, prefix=prefix)
+            os.rmdir(self.rootdir3)
+            self.store3 = blaze.Storage(self.rootdir3)
+        else:
+            self.rootdir1 = None
+            self.store1 = None
+            self.rootdir2 = None
+            self.store2 = None
+            self.rootdir3 = None
+            self.store3 = None
+
+    def tearDown(self):
+        if self.disk:
+            remove_tree(self.rootdir1)
+            remove_tree(self.rootdir2)
+            remove_tree(self.rootdir3)
 
 
 # Check for arrays that fit in the chunk size
