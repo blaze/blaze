1bbf423457658897e7f8cccf527f44c2ee9ca9c1
Francesc Alted <francesc@continuum.io>
2014-03-31 15:32:55 +0200

    All tests in blaze/tests passes now

--- blaze/tests/test_get_set.py
+++ blaze/tests/test_get_set.py
@@ -5,49 +5,49 @@ import unittest
 import numpy as np
 
 import blaze
+from blaze import BLZ_DDesc
 from blaze.datadescriptor import ddesc_as_py
 
 
 class getitem(unittest.TestCase):
-    caps={'compress': False}  # the default is non-compressed arrays
+    ddesc = None
 
     def test_scalar(self):
-        a = blaze.array(np.arange(3), caps=self.caps)
+        a = blaze.array(np.arange(3), ddesc=self.ddesc)
         self.assertEqual(ddesc_as_py(a[0].ddesc), 0)
 
     def test_1d(self):
-        a = blaze.array(np.arange(3), caps=self.caps)
+        a = blaze.array(np.arange(3), ddesc=self.ddesc)
         # print("a:", a, self.caps)
         self.assertEqual(ddesc_as_py(a[0:2].ddesc), [0,1])
 
     def test_2d(self):
-        a = blaze.array(np.arange(3*3).reshape(3,3), caps=self.caps)
+        a = blaze.array(np.arange(3*3).reshape(3,3), ddesc=self.ddesc)
         self.assertEqual(ddesc_as_py(a[1].ddesc), [3,4,5])
 
 class getitem_blz(getitem):
-    caps={'compress': True}
+    ddesc = BLZ_DDesc(mode='w')
 
 class setitem(unittest.TestCase):
-    caps={'compress': False}  # the default is non-compressed arrays
+    ddesc = None
 
     def test_scalar(self):
-        a = blaze.array(np.arange(3), caps=self.caps)
+        a = blaze.array(np.arange(3), ddesc=self.ddesc)
         a[0] = 1
         self.assertEqual(ddesc_as_py(a[0].ddesc), 1)
 
     def test_1d(self):
-        a = blaze.array(np.arange(3), caps=self.caps)
+        a = blaze.array(np.arange(3), ddesc=self.ddesc)
         a[0:2] = 2
         self.assertEqual(ddesc_as_py(a[0:2].ddesc), [2,2])
 
     def test_2d(self):
-        a = blaze.array(np.arange(3*3).reshape(3,3), caps=self.caps)
+        a = blaze.array(np.arange(3*3).reshape(3,3), ddesc=self.ddesc)
         a[1] = 2
         self.assertEqual(ddesc_as_py(a[1].ddesc), [2,2,2])
 
-# BLZ is going to be read and append only for the time being
-# class setitem_blz(setitem):
-#     caps={'compress': True}
+class setitem_blz(getitem):
+    ddesc = BLZ_DDesc(mode='w')
 
 if __name__ == '__main__':
     unittest.main()
