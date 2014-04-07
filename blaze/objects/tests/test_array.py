b662234e7d3caf50b85a164f0389467672b9cec7
Matthew Rocklin <mrocklin@gmail.com>
2014-04-07 16:03:15 -0700

    more standard use of assertEqual in array tests

--- blaze/objects/tests/test_array.py
+++ blaze/objects/tests/test_array.py
@@ -8,17 +8,16 @@ class Test_1D_Array(unittest.TestCase):
         self.a = array([1, 2, 3])
 
     def test_iter_1d(self):
-        assert list(self.a) == [1, 2, 3]
+        self.assertEqual(list(self.a), [1, 2, 3])
 
 class Test_2D_Array(unittest.TestCase):
     def setUp(self):
-        self.a = array([1, 2, 3])
         self.a = array([[1, 2, 3],
                         [4, 5, 6],
                         [7, 8, 9]])
 
     def test_list_list(self):
-        assert list(list(self.a)[0]) == [1, 2, 3]
+        self.assertEqual(list(list(self.a)[0]), [1, 2, 3])
 
     def test_list_elements(self):
         assert blaze.all(list(self.a)[0] == array([1, 2, 3]))
