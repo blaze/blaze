14bfdba5cd82b8d508c66dfa6d10cabf22c9c139
Matthew Rocklin <mrocklin@gmail.com>
2014-04-07 15:57:50 -0700

    switch out function tests for unittest tests

--- blaze/objects/tests/test_array.py
+++ blaze/objects/tests/test_array.py
@@ -1,16 +1,25 @@
 from blaze import array
 import blaze
 
-def test_iter_1d():
-    a = array([1, 2, 3])
-    assert list(a) == [1, 2, 3]
+import unittest
 
+class Test_1D_Array(unittest.TestCase):
+    def setUp(self):
+        self.a = array([1, 2, 3])
 
-def test_iter_nd():
-    a = array([[1, 2, 3],
-               [4, 5, 6],
-               [7, 8, 9]])
-    assert list(list(a)[0]) == [1, 2, 3]
+    def test_iter_1d(self):
+        assert list(self.a) == [1, 2, 3]
 
-    assert blaze.all(list(a)[0] == array([1, 2, 3]))
-    assert blaze.all(list(a)[1] == array([4, 5, 6]))
+class Test_2D_Array(unittest.TestCase):
+    def setUp(self):
+        self.a = array([1, 2, 3])
+        self.a = array([[1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9]])
+
+    def test_list_list(self):
+        assert list(list(self.a)[0]) == [1, 2, 3]
+
+    def test_list_elements(self):
+        assert blaze.all(list(self.a)[0] == array([1, 2, 3]))
+        assert blaze.all(list(self.a)[1] == array([4, 5, 6]))
