80ed8effc5fd33db13c0a44bf295314e16d80210
Mark Wiebe <mwiebe@continuum.io>
2014-04-08 12:32:55 -0700

    Use the changes from PR #212 to simplify the tests

--- blaze/tests/test_datetime.py
+++ blaze/tests/test_datetime.py
@@ -15,18 +15,13 @@ class TestDate(unittest.TestCase):
         self.assertEqual(ddesc_as_py(a.ddesc), date(2000, 1, 1))
         a = blaze.array([date(1490, 3, 12), date(2020, 7, 15)])
         self.assertEqual(a.dshape, dshape('2 * date'))
-        self.assertEqual(ddesc_as_py(a.ddesc),
-                         [date(1490, 3, 12), date(2020, 7, 15)])
+        self.assertEqual(list(a), [date(1490, 3, 12), date(2020, 7, 15)])
         a = blaze.array(['1490-03-12', '2020-07-15'], dshape='date')
         self.assertEqual(a.dshape, dshape('2 * date'))
-        self.assertEqual(ddesc_as_py(a.ddesc),
-                         [date(1490, 3, 12), date(2020, 7, 15)])
+        self.assertEqual(list(a), [date(1490, 3, 12), date(2020, 7, 15)])
 
     def test_properties(self):
         a = blaze.array(['1490-03-12', '2020-07-15'], dshape='date')
-        y = a.year
-        self.assertEqual(ddesc_as_py(blaze.eval(y).ddesc), [1490, 2020])
-        m = a.month
-        self.assertEqual(ddesc_as_py(blaze.eval(m).ddesc), [3, 7])
-        d = a.day
-        self.assertEqual(ddesc_as_py(blaze.eval(d).ddesc), [12, 15])
+        self.assertEqual(list(a.year), [1490, 2020])
+        self.assertEqual(list(a.month), [3, 7])
+        self.assertEqual(list(a.day), [12, 15])
