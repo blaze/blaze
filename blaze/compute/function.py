daaccae65b49dd4a93814c7dc323f97f8631dfaa
Francesc Alted <francesc@continuum.io>
2014-04-09 13:01:16 +0200

    The convention is Deferred_DDesc, not DeferredDDesc

--- blaze/compute/function.py
+++ blaze/compute/function.py
@@ -17,7 +17,7 @@ import blaze
 import datashape
 from datashape import coretypes, dshape
 
-from ..datadescriptor import DeferredDDesc
+from ..datadescriptor import Deferred_DDesc
 from .expr import ArrayOp, ExprContext, KernelOp
 
 Overload = namedtuple('Overload', 'resolved_sig, sig, func')
@@ -168,7 +168,7 @@ class BlazeFunc(object):
 
         # Construct graph
         term = construct(self, ctx, overload, args)
-        desc = DeferredDDesc(term.dshape, (term, ctx))
+        desc = Deferred_DDesc(term.dshape, (term, ctx))
 
         return blaze.Array(desc)
 
@@ -327,6 +327,6 @@ class ReductionBlazeFunc(BlazeFunc):
 
         # Construct graph
         term = construct(self, ctx, overload, args)
-        desc = DeferredDDesc(term.dshape, (term, ctx))
+        desc = Deferred_DDesc(term.dshape, (term, ctx))
 
         return blaze.Array(desc)
--- blaze/datadescriptor/__init__.py
+++ blaze/datadescriptor/__init__.py
@@ -10,7 +10,7 @@ from .cat_data_descriptor import Cat_DDesc
 from .membuf_data_descriptor import (data_descriptor_from_ctypes,
                 data_descriptor_from_cffi)
 from .dynd_data_descriptor import DyND_DDesc
-from .deferred_data_descriptor import DeferredDDesc
+from .deferred_data_descriptor import Deferred_DDesc
 from .stream_data_descriptor import Stream_DDesc
 from .csv_data_descriptor import CSV_DDesc
 from .json_data_descriptor import JSON_DDesc
