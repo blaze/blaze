9304589410c2923134ba7cf7a1c1cae535b315f9
Matthew Rocklin <mrocklin@gmail.com>
2014-04-10 10:34:25 -0700

    pull out file creation and text writing

--- blaze/datadescriptor/util.py
+++ blaze/datadescriptor/util.py
@@ -9,13 +9,18 @@ from dynd import nd
 def filetext(text, extension=''):
     # write text to hidden file
     handle, filename = tempfile.mkstemp(extension)
-    with os.fdopen(handle, "w") as f:
-        f.write(text)
+    with openfile(extension=extension) as filename:
+        with open(filename, "w") as f:
+            f.write(text)
 
-    # Yield control to test
+        yield filename
+
+@contextmanager
+def openfile(extension=''):
+    filename = tempfile.mktemp()
+
     yield filename
 
-    # Clean up the written file
     os.remove(filename)
 
 
