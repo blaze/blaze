9c80d3cd0b67c3acd0159bcdb80d35bd0bfd4e84
Matthew Rocklin <mrocklin@gmail.com>
2014-04-10 12:44:40 -0700

    add partition_all to blaze/utils.py

---
+++ blaze/utils.py
@@ -0,0 +1,20 @@
+from itertools import islice
+
+def partition_all(n, seq):
+    """ Split sequence into subsequences of size ``n``
+
+    >>> list(partition_all(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+
+    The last element of the list may have fewer than ``n`` elements
+
+    >>> list(partition_all(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    [(1, 2, 3), (4, 5, 6), (7, 8, 9), (10,)]
+    """
+    seq = iter(seq)
+    while True:
+        result = tuple(islice(seq, 0, n))
+        if result:
+            yield result
+        else:
+            raise StopIteration()
