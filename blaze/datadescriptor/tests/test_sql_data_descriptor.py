f3f61dec45eb157d1ab6a52fc831a17c8dce1d22
Matthew Rocklin <mrocklin@gmail.com>
2014-04-11 11:41:02 -0700

    SQL tests conform unittest (maybe?)

--- blaze/datadescriptor/tests/test_sql_data_descriptor.py
+++ blaze/datadescriptor/tests/test_sql_data_descriptor.py
@@ -2,58 +2,66 @@ from sqlalchemy import create_engine
 from blaze.datadescriptor import SQL_DDesc, DyND_DDesc
 from blaze.datadescriptor.util import raises
 from dynd import nd
-
-engine = create_engine('sqlite:///:memory:', echo=True)
-
-def test_can_connect():
-    with engine.connect() as conn:
-        assert not conn.closed
-    assert conn.closed
-
-def test_table_creation():
-    dd = SQL_DDesc(engine, 'testtable',
-                           schema='{name: string, amount: int}',
-                           primary_key='name')
-    assert engine.has_table('testtable')
+import unittest
 
 
-    assert dd.table.columns.get('name').primary_key
-    assert not dd.table.columns.get('amount').primary_key
-    assert dd.dshape == 'var * {name: string, amount: int}'
+class GiantTest(unittest.TestCase):
+    def setUp(self):
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
 
-    assert raises(ValueError, lambda: SQL_DDesc(engine, 'testtable2'))
+    def tearDown(self):
+        pass
+        # How do I clean up an engine?
 
+    def test_can_connect(self):
+        with self.engine.connect() as conn:
+            assert not conn.closed
+        assert conn.closed
 
-def test_extension():
-    dd = SQL_DDesc(engine, 'testtable2',
-                           schema='{name: string, amount: int32}',
-                           primary_key='name')
-
-    data_list = [('Alice', 100), ('Bob', 50)]
-    data_dict = [{'name': name, 'amount': amount} for name, amount in data_list]
-
-    dd.extend(data_dict)
-
-    with engine.connect() as conn:
-        results = conn.execute('select * from testtable2')
-        assert list(results) == data_list
+    def test_table_creation(self):
+        dd = SQL_DDesc(self.engine, 'testtable',
+                               schema='{name: string, amount: int}',
+                               primary_key='name')
+        assert self.engine.has_table('testtable')
 
 
-    assert list(iter(dd)) == data_list or list(iter(dd)) == data_dict
+        assert dd.table.columns.get('name').primary_key
+        assert not dd.table.columns.get('amount').primary_key
+        assert dd.dshape == 'var * {name: string, amount: int}'
+
+        assert raises(ValueError, lambda: SQL_DDesc(self.engine, 'testtable2'))
 
 
-def test_chunks():
-    schema = '{name: string, amount: int32}'
-    dd = SQL_DDesc(engine, 'testtable3',
-                           schema=schema,
-                           primary_key='name')
+    def test_extension(self):
+        dd = SQL_DDesc(self.engine, 'testtable2',
+                               schema='{name: string, amount: int32}',
+                               primary_key='name')
 
-    data_list = [('Alice', 100), ('Bob', 50), ('Charlie', 200)]
-    data_dict = [{'name': name, 'amount': amount} for name, amount in data_list]
-    chunk = DyND_DDesc(nd.array(data_list, dtype=dd.dshape))
+        data_list = [('Alice', 100), ('Bob', 50)]
+        data_dict = [{'name': name, 'amount': amount} for name, amount in data_list]
 
-    dd.extend_chunks([chunk])
+        dd.extend(data_dict)
 
-    assert list(iter(dd)) == data_list or list(iter(dd)) == data_dict
+        with self.engine.connect() as conn:
+            results = conn.execute('select * from testtable2')
+            assert list(results) == data_list
 
-    assert len(list(dd.iterchunks(blen=2))) == 2
+
+        assert list(iter(dd)) == data_list or list(iter(dd)) == data_dict
+
+
+    def test_chunks(self):
+        schema = '{name: string, amount: int32}'
+        dd = SQL_DDesc(self.engine, 'testtable3',
+                                   schema=schema,
+                                   primary_key='name')
+
+        data_list = [('Alice', 100), ('Bob', 50), ('Charlie', 200)]
+        data_dict = [{'name': name, 'amount': amount} for name, amount in data_list]
+        chunk = DyND_DDesc(nd.array(data_list, dtype=dd.dshape))
+
+        dd.extend_chunks([chunk])
+
+        assert list(iter(dd)) == data_list or list(iter(dd)) == data_dict
+
+        assert len(list(dd.iterchunks(blen=2))) == 2
