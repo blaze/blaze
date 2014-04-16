055911a7ef429a8a70cc56328321ccc96a710b50
Matthew Rocklin <mrocklin@gmail.com>
2014-04-15 18:13:27 -0700

    add json data descriptors

---
+++ blaze/data/json.py
@@ -0,0 +1,147 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import json
+
+from itertools import islice
+import datashape
+from ..utils import partition_all, nth
+
+from .core import DataDescriptor
+from .. import py2help
+from dynd import nd
+from .utils import coerce
+from ..py2help import _inttypes
+
+def isdimension(ds):
+    return isinstance(ds, (datashape.Var, datashape.Fixed))
+
+
+class JSON(DataDescriptor):
+    """
+    A Blaze data descriptor to expose a JSON file.
+
+    Parameters
+    ----------
+    path : string
+        A path string for the JSON file.
+    schema : string or datashape
+        A datashape (or its string representation) of the schema
+        in the JSON file.
+    """
+    def __init__(self, path, mode='r', schema=None, dshape=None):
+        if 'w' not in mode and not os.path.isfile(path):
+            raise ValueError('JSON file "%s" does not exist' % path)
+        self.path = path
+        self.mode = mode
+        if dshape:
+            dshape = datashape.dshape(dshape)
+        if dshape and not schema and isdimension(dshape[0]):
+            schema = dshape.subarray(1)
+
+        if isinstance(schema, py2help._strtypes):
+            schema = datashape.dshape(schema)
+        if not schema:
+            # TODO: schema detection from file
+            raise ValueError('No schema found')
+        # TODO: should store DataShape object
+        self.schema = schema
+        # Initially the array is not loaded (is this necessary?)
+        self._cache_arr = None
+        self._dshape = dshape
+
+    @property
+    def dshape(self):
+        return self._dshape or datashape.dshape('var * ' + str(self.schema))
+
+    @property
+    def capabilities(self):
+        """The capabilities for the json data descriptor."""
+        return {'immutable': False,
+                'deferred': False,
+                'persistent': True,
+                'appendable': True,
+                'remote': False}
+
+    @property
+    def _arr_cache(self):
+        if self._cache_arr is not None:
+            return self._cache_arr
+        with open(self.path, mode=self.mode) as jsonfile:
+            # This will read everything in-memory (but a memmap approach
+            # is in the works)
+            self._cache_arr = nd.parse_json(str(self.dshape), jsonfile.read())
+        return self._cache_arr
+
+    def __iter__(self):
+        for line in self._arr_cache:
+            yield nd.as_py(line)
+
+    def _chunks(self, blen=100):
+        with open(self.path) as f:
+            for chunk in partition_all(blen, f):
+                text = '[' + ',\r\n'.join(chunk) + ']'
+                dshape = str(len(chunk) * self.schema)
+                yield nd.parse_json(dshape, text)
+
+    def dynd_arr(self):
+        return self._arr_cache
+
+    def remove(self):
+        """Remove the persistent storage."""
+        os.unlink(self.path)
+
+
+class JSON_Streaming(JSON):
+    """
+    A Blaze data descriptor to expose a Streaming JSON file.
+
+    Parameters
+    ----------
+    path : string
+        A path string for the JSON file.
+    schema : string or datashape
+        A datashape (or its string representation) of the schema
+        in the JSON file.
+    """
+    @property
+    def _arr_cache(self):
+        if self._cache_arr is not None:
+            return self._cache_arr
+        with open(self.path, mode=self.mode) as jsonfile:
+            # This will read everything in-memory (but a memmap approach
+            # is in the works)
+            text = '[' + ', '.join(jsonfile) + ']'
+        self._cache_arr = nd.parse_json(str(self.dshape), text)
+        return self._cache_arr
+
+    def __getitem__(self, key):
+        with open(self.path) as f:
+            if isinstance(key, _inttypes):
+                result = json.loads(nth(key, f))
+            elif isinstance(key, slice):
+                result = list(map(json.loads,
+                                    islice(f, key.start, key.stop, key.step)))
+            else:
+                raise NotImplementedError('Fancy indexing not supported\n'
+                        'Create DyND array and use fancy indexing from there')
+        return coerce(self.schema, result)
+
+    def _iter(self):
+        with open(self.path) as f:
+            for line in f:
+                yield json.loads(line)
+
+    def _iterchunks(self, blen=100):
+        with open(self.path) as f:
+            for chunk in partition_all(blen, f):
+                text = '[' + ',\r\n'.join(chunk) + ']'
+                dshape = str(len(chunk)) + ' * ' + self.schema
+                yield nd.parse_json(dshape, text)
+
+    def _extend(self, rows):
+        with open(self.path, self.mode) as f:
+            f.seek(0, os.SEEK_END)  # go to the end of the file
+            for row in rows:
+                json.dump(row, f)
+                f.write('\n')
---
+++ blaze/data/tests/test_json.py
@@ -0,0 +1,141 @@
+from __future__ import absolute_import, division, print_function
+
+import unittest
+import os
+import tempfile
+import json
+from dynd import nd
+import datashape
+from blaze.datadescriptor.as_py import ddesc_as_py
+
+from blaze.data import JSON, JSON_Streaming
+from blaze.utils import filetext, raises
+
+# TODO: This isn't actually being used!
+json_buf = u"""{
+    "type": "ImageCollection",
+    "images": [
+        "Image": {
+            "Width":  800,
+            "Height": 600,
+            "Title":  "View from 15th Floor",
+            "Thumbnail": {
+                "Url":    "http://www.example.com/image/481989943",
+                "Height": 125,
+                "Width":  "100"
+            },
+            "IDs": [116, 943, 234, 38793]
+        }
+    ]
+}
+"""
+
+# TODO: This isn't actually being used!
+json_schema = """{
+  type: string,
+  images: var * {
+        Width: int16,
+        Height: int16,
+        Title: string,
+        Thumbnail: {
+            Url: string,
+            Height: int16,
+            Width: int16,
+        },
+        IDs: var * int32,
+    };
+}
+"""
+
+
+json_buf = u"[1, 2, 3, 4, 5]"
+json_dshape = "var * int8"
+
+
+class TestJSON(unittest.TestCase):
+
+    def setUp(self):
+        handle, self.json_file = tempfile.mkstemp(".json")
+        with os.fdopen(handle, "w") as f:
+            f.write(json_buf)
+
+    def tearDown(self):
+        os.remove(self.json_file)
+
+    def test_raise_error_on_non_existent_file(self):
+        self.assertRaises(ValueError,
+                    lambda: JSON('does-not-exist23424.josn', 'r'))
+
+    def test_basic_object_type(self):
+        dd = JSON(self.json_file, dshape=json_dshape)
+        self.assertEqual(list(dd), [1, 2, 3, 4, 5])
+
+    def test_iter(self):
+        dd = JSON(self.json_file, dshape=json_dshape)
+        # This equality does not work yet
+        # self.assertEqual(dd.dshape, datashape.dshape(
+        #     'Var, %s' % json_schema))
+        print(list(dd))
+        self.assertEqual(list(dd), [1, 2, 3, 4, 5])
+
+class Test_StreamingTransfer(unittest.TestCase):
+
+    data = [{'name': 'Alice', 'amount': 100},
+            {'name': 'Alice', 'amount': 50},
+            {'name': 'Bob', 'amount': 10},
+            {'name': 'Charlie', 'amount': 200},
+            {'name': 'Bob', 'amount': 100}]
+
+    text = '\n'.join(map(json.dumps, data))
+
+    schema = '{name: string, amount: int32}'
+
+    def test_init(self):
+        with filetext(self.text) as fn:
+            dd = JSON_Streaming(fn, schema=self.schema)
+            self.assertEquals(list(dd), self.data)
+            assert dd.dshape in set((
+                datashape.dshape('var * {name: string, amount: int32}'),
+                datashape.dshape('5 * {name: string, amount: int32}')))
+
+
+    def test_chunks(self):
+        with filetext(self.text) as fn:
+            dd = JSON_Streaming(fn, schema=self.schema)
+            chunks = list(dd.chunks(blen=2))
+            assert isinstance(chunks[0], nd.array)
+            self.assertEquals(len(chunks), 3)
+            self.assertEquals(nd.as_py(chunks[0]), self.data[:2])
+
+
+    def test_append(self):
+        with filetext('') as fn:
+            dd = JSON_Streaming(fn, mode='w', schema=self.schema)
+            dd.append(self.data[0])
+            with open(fn) as f:
+                self.assertEquals(json.loads(f.read().strip()), self.data[0])
+
+            self.assertRaises(ValueError, lambda : dd.append(5.5))
+            self.assertRaises(ValueError,
+                              lambda : dd.append({'name': 5, 'amount': 1.3}))
+
+    def test_extend(self):
+        with filetext('') as fn:
+            dd = JSON_Streaming(fn, mode='r+', schema=self.schema)
+            dd.extend(self.data)
+
+            self.assertEquals(list(dd), self.data)
+
+    def test_getitem(self):
+        with filetext(self.text) as fn:
+            dd = JSON_Streaming(fn, mode='r', schema=self.schema)
+            assert dd[0] == self.data[0]
+            assert dd[2:4] == self.data[2:4]
+
+    def test_dynd_arr(self):
+        with filetext(self.text) as fn:
+            dd = JSON_Streaming(fn, mode='r', schema=self.schema)
+            assert nd.as_py(dd.dynd_arr()) == self.data
+
+if __name__ == '__main__':
+    unittest.main()
