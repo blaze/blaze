2a05a47090604efa25a0b01854281a1fca7d02f5
Matthew Rocklin <mrocklin@gmail.com>
2014-04-15 17:44:43 -0700

    add csv data descriptor

---
+++ blaze/data/core.py
@@ -0,0 +1,100 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = ['DataDescriptor', 'copy']
+
+import abc
+
+from blaze.error import StreamingDimensionError
+from blaze.compute.strategy import CKERNEL
+from .utils import validate
+
+from itertools import chain
+from dynd import nd
+
+
+class DataDescriptor(object):
+    """
+    The Blaze data descriptor is an interface which exposes
+    data to Blaze. The data descriptor doesn't implement math
+    or any other kind of functions, its sole purpose is providing
+    single and multi-dimensional data to Blaze via a data shape,
+    and the indexing/iteration interfaces.
+
+    Indexing and python iteration must always return data descriptors,
+    this is the python interface to the data. A summary of the
+    data access patterns for a data descriptor dd, in the
+    0.3 version of blaze are:
+
+     - descriptor integer indexing
+            child_dd = dd[i, j, k]
+     - slice indexing
+            child_dd = dd[i:j]
+     - descriptor outer/leading dimension iteration
+            for child_dd in dd: do_something(child_dd)
+     - memory access via dynd array (either using dynd library
+       to process, or directly depending on the ABI of the dynd
+       array object, which will be stabilized prior to dynd 1.0)
+
+    The descriptor-based indexing methods operate only through the
+    Python interface, JIT-compiled access should be done through
+    processing the dynd type and corresponding array metadata.
+    """
+    def append(self, value):
+        """
+        This allows appending values in the data descriptor.
+        """
+        self.extend([value])
+
+    def extend(self, rows):
+        """ Extend data with many rows
+
+        See Also:
+            append
+        """
+        rows = iter(rows)
+        row = next(rows)
+        if not validate(self.schema, row):
+            raise ValueError('Invalid data:\n\t %s \nfor dshape \n\t%s' %
+                    (str(row), self.schema))
+        self._extend(chain([row], rows))
+
+
+    def extend_chunks(self, chunks):
+        self._extend_chunks((nd.array(chunk) for chunk in chunks))
+
+    def _extend_chunks(self, chunks):
+        self.extend((row for chunk in chunks for row in nd.as_py(chunk)))
+
+    def chunks(self, **kwargs):
+        def dshape(chunk):
+            return str(len(chunk) * self.dshape.subarray(1))
+
+        chunks = self._chunks(**kwargs)
+        return (nd.array(chunk, dtype=dshape(chunk)) for chunk in chunks)
+
+    def _chunks(self, blen=100):
+        raise NotImplementedError()
+
+    def getattr(self, name):
+        raise NotImplementedError('this data descriptor does not support attribute access')
+
+    def dynd_arr(self):
+        """Concrete data descriptors must provide their array data
+           as a dynd array, accessible via this method.
+        """
+        if not self.deferred:
+            raise NotImplementedError((
+                'Data descriptor of type %s claims '
+                'claims to not being deferred, but did not '
+                'override dynd_arr()') % type(self))
+        else:
+            raise TypeError((
+                'Data descriptor of type %s is deferred') % type(self))
+
+    def __array__(self):
+        return nd.as_numpy(self.dynd_arr())
+
+
+def copy(src, dest, **kwargs):
+    """ Copy content from one data descriptor to another """
+    dest.extend_chunks(src.iterchunks(**kwargs))
---
+++ blaze/data/csv.py
@@ -0,0 +1,173 @@
+from __future__ import absolute_import, division, print_function
+
+import csv
+import itertools as it
+import os
+
+import datashape
+from dynd import nd
+
+from .core import DataDescriptor
+from .utils import coerce, coerce_record_to_row
+from ..utils import partition_all, nth
+from .. import py2help
+
+__all__ = ['CSV']
+
+
+def open_file(path, mode, has_header):
+    """Return a file handler positionated at the first valid line."""
+    csvfile = open(path, mode=mode)
+    if has_header:
+        csvfile.readline()
+    return csvfile
+
+
+class CSV(DataDescriptor):
+    """
+    A Blaze data descriptor which exposes a CSV file.
+
+    Parameters
+    ----------
+    path : string
+        A path string for the CSV file.
+    schema : string or datashape
+        A datashape (or its string representation) of the schema
+        in the CSV file.
+    dialect : string or csv.Dialect instance
+        The dialect as understood by the `csv` module in Python standard
+        library.  If not specified, a value is guessed.
+    has_header : boolean
+        Whether the CSV file has a header or not.  If not specified a value
+        is guessed.
+    """
+    immutable = False
+    deferred = False
+    persistent = True
+    appendable = True
+    remote = False
+
+    def __init__(self, path, mode='r', schema=None, dialect=None,
+            has_header=None, **kwargs):
+        if 'r' in mode and os.path.isfile(path) is not True:
+            raise ValueError('CSV file "%s" does not exist' % path)
+        self.path = path
+        self.mode = mode
+        csvfile = open(path, mode=self.mode)
+
+        # Handle Schema
+        if isinstance(schema, py2help._strtypes):
+            schema = datashape.dshape(schema)
+        if not schema:
+            # TODO: schema detection from first row
+            raise ValueError('Need schema')
+        self.schema = str(schema)
+
+        # Guess Dialect if not an input
+        if dialect is None and 'r' in mode:
+            sniffer = csv.Sniffer()
+            try:
+                dialect = sniffer.sniff(csvfile.read(1024))
+            except:
+                pass
+        if dialect is None:
+            dialect = csv.get_dialect('excel')
+        elif isinstance(dialect, py2help.basestring):
+            dialect = csv.get_dialect(dialect)
+        # Convert dialect to dictionary
+        self.dialect = dict((key, getattr(dialect, key))
+                            for key in dir(dialect) if not key.startswith('_'))
+
+        # Update dialect with any keyword arguments passed in
+        # E.g. allow user to override with delimiter=','
+        for k, v in kwargs.items():
+            if k in self.dialect:
+                self.dialect[k] = v
+
+        # Handle Header
+        if has_header is None and mode != 'w':
+            # Guess whether the file has a header or not
+            sniffer = csv.Sniffer()
+            csvfile.seek(0)
+            sample = csvfile.read(1024)
+            try:
+                self.has_header = sniffer.has_header(sample)
+            except:
+                self.has_header = has_header
+
+        else:
+            self.has_header = has_header
+
+        csvfile.close()
+
+    @property
+    def dshape(self):
+        return datashape.Var() * self.schema
+
+    def dynd_arr(self):
+        with open_file(self.path, self.mode, self.has_header) as csvfile:
+            seq = csv.reader(csvfile, **self.dialect)
+            return nd.array(seq, dtype=self.schema)
+
+    def __getitem__(self, key):
+        with open_file(self.path, self.mode, self.has_header) as csvfile:
+            if isinstance(key, py2help._inttypes):
+                line = nth(key, csvfile)
+                result = next(csv.reader([line], **self.dialect))
+            elif isinstance(key, slice):
+                start, stop, step = key.start, key.stop, key.step
+                result = list(csv.reader(it.islice(csvfile, start, stop, step),
+                                         **self.dialect))
+            else:
+                raise IndexError("key '%r' is not valid" % key)
+        return coerce(self.schema, result)
+
+    def __iter__(self):
+        with open(self.path, self.mode) as f:
+            if self.has_header:
+                next(f)  # burn header
+            for row in csv.reader(f, **self.dialect):
+                yield coerce(self.schema, row)
+
+    def _extend(self, rows):
+        rows = iter(rows)
+        with open_file(self.path, self.mode, self.has_header) as f:
+            row = next(rows)
+            if isinstance(row, dict):
+                schema = datashape.dshape(self.schema)
+                row = coerce_record_to_row(schema, row)
+                rows = (coerce_record_to_row(schema, row) for row in rows)
+
+            # Write all rows to file
+            f.seek(0, os.SEEK_END)  # go to the end of the file
+            writer = csv.writer(f, **self.dialect)
+            writer.writerow(row)
+            writer.writerows(rows)
+
+    def _chunks(self, blen=100, start=None, stop=None):
+        """Return chunks of size `blen` (in leading dimension).
+
+        Parameters
+        ----------
+        blen : int
+            The length, in rows, of the buffers that are returned.
+        start : int
+            Where the iterator starts.  The default is to start at the
+            beginning.
+        stop : int
+            Where the iterator stops. The default is to stop at the end.
+
+        Returns
+        -------
+        out : iterable
+            This iterable returns buffers as DyND arrays,
+
+        """
+        with open_file(self.path, self.mode, self.has_header) as f:
+            f = it.islice(csv.reader(f, **self.dialect), start, stop)
+            for rows in partition_all(blen, f):
+                yield rows
+
+    def remove(self):
+        """Remove the persistent storage."""
+        os.unlink(self.path)
