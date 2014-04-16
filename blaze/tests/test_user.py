701519871a6cd94a0cfe2f1d2af7fbc4480545d0
Matthew Rocklin <mrocklin@gmail.com>
2014-04-16 15:10:21 -0700

    add into operation

--- blaze/tests/test_user.py
+++ blaze/tests/test_user.py
@@ -2,7 +2,7 @@ import unittest
 
 from dynd import nd
 
-from blaze.user import validate
+from blaze.user import validate, into
 from blaze import array
 
 
@@ -14,3 +14,34 @@ class Test_Validate(unittest.TestCase):
     def test_dynd(self):
         assert validate('2 * int', nd.array([1, 2], dtype='2 * int32'))
         assert not validate('3 * int', nd.array([1, 2], dtype='2 * int32'))
+
+class Test_Into(unittest.TestCase):
+    def test_containers(self):
+        self.assertEqual(into([], (1, 2, 3)),
+                                  [1, 2, 3])
+        self.assertEqual(into((), (1, 2, 3)),
+                                  (1, 2, 3))
+        self.assertEqual(into({}, [(1, 2), (3, 4)]),
+                                  {1: 2, 3: 4})
+        self.assertEqual(into((), {1: 2, 3: 4}),
+                                  ((1, 2), (3, 4)))
+
+    def test_dynd(self):
+        self.assertEqual(nd.as_py(into(nd.array(), (1, 2, 3))),
+                         nd.as_py(nd.array([1, 2, 3])))
+        self.assertEqual(into([], nd.array([1, 2])),
+                                  [1, 2])
+        self.assertEqual(into([], nd.array([[1, 2], [3, 4]])),
+                                  [[1, 2], [3, 4]])
+
+    def test_blaze(self):
+        self.assertEqual(into([], array([1, 2])),
+                                  [1, 2])
+        self.assertEqual(into((), array([1, 2])),
+                                  (1, 2))
+        self.assertEqual(into([], array([[1, 2], [3, 4]])),
+                                  [[1, 2], [3, 4]])
+        self.assertEqual(into((), array([[1, 2], [3, 4]])),
+                                  ((1, 2), (3, 4)))
+        self.assertEqual(nd.as_py(into(nd.array(), array([[1, 2], [3, 4]]))),
+                         nd.as_py(nd.array([[1, 2], [3, 4]])))
--- blaze/user.py
+++ blaze/user.py
@@ -14,3 +14,34 @@ def validate(ds, arr):
 @dispatch(DataShape, nd.array)
 def validate(ds, arr):
     return issubschema(nd.dshape_of(arr), ds)
+
+@dispatch((list, tuple, set), (list, tuple, set))
+def into(a, b):
+    return type(a)(b)
+
+@dispatch(dict, (list, tuple, set))
+def into(a, b):
+    return dict(b)
+
+@dispatch((list, tuple, set), dict)
+def into(a, b):
+    return type(a)(map(type(a), b.items()))
+
+@dispatch(nd.array, object)
+def into(a, b):
+    return nd.array(b)
+
+@dispatch(list, nd.array)
+def into(a, b):
+    return nd.as_py(b)
+
+@dispatch((list, tuple, set), Array)
+def into(a, b):
+    if len(b.dshape.shape) == 1:
+        return type(a)(b)
+    else:
+        return type(a)(into(a, item) for item in b)
+
+@dispatch(nd.array, Array)
+def into(a, b):
+    return b.ddesc.dynd_arr()
