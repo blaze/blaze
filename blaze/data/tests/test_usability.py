117f4d3366ff50979c6b9f55b8a547706d1f150e
Matthew Rocklin <mrocklin@gmail.com>
2014-04-19 11:57:09 -0700

    skip resource hdf5 test
    This test runs fine but segfaults when run with the whole test suite

--- blaze/data/tests/test_usability.py
+++ blaze/data/tests/test_usability.py
@@ -5,6 +5,7 @@ import gzip
 
 from blaze.utils import filetext, filetexts, tmpfile
 from blaze.data import *
+from blaze.py2help import skip
 
 class TestResource(TestCase):
     def setUp(self):
@@ -44,6 +45,7 @@ class TestResource(TestCase):
                                    schema='{x: int, y: int}'),
                           SQL)
 
+    @skip("This runs fine in isolation, segfaults in full test")
     def test_hdf5(self):
         with tmpfile('.hdf5') as filename:
             assert isinstance(resource(filename + '::/path/to/data/',
