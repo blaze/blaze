ed5db5d39c2d5a386a2e17a9de3ec268a139468d
Matthew Rocklin <mrocklin@gmail.com>
2014-04-23 19:31:13 -0700

    add Projection

--- blaze/objects/table.py
+++ blaze/objects/table.py
@@ -1,4 +1,4 @@
-from datashape import dshape, var
+from datashape import dshape, var, DataShape, Record
 
 
 class Table(object):
@@ -12,3 +12,17 @@ class Table(object):
     @property
     def columns(self):
         return self.schema[0].names
+
+class Projection(Table):
+    def __init__(self, table, columns):
+        self.table = table
+        self._columns = columns
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @property
+    def schema(self):
+        d = self.table.schema[0].fields
+        return DataShape(Record([(col, d[col]) for col in self.columns]))
