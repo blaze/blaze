afe31e02b908ea9872f9f4451d374364e51c5cea
Matthew Rocklin <mrocklin@gmail.com>
2014-04-23 19:26:08 -0700

    add columns property

--- blaze/objects/tests/test_table.py
+++ blaze/objects/tests/test_table.py
@@ -4,3 +4,7 @@ def test_dshape():
     t = Table('{name: string, amount: int}')
     assert t.dshape == dshape('var * {name: string, amount: int}')
 
+def test_column():
+    t = Table('{name: string, amount: int}')
+    assert t.columns == ['name', 'amount']
+
