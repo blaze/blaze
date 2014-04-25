fb03b6c74170c50594655dee8204de6bd7e2e93d
Matthew Rocklin <mrocklin@gmail.com>
2014-04-25 08:09:20 -0700

    into -> like in datadescriptor

--- blaze/datadescriptor/tests/test_user.py
+++ blaze/datadescriptor/tests/test_user.py
@@ -2,16 +2,17 @@ import unittest
 
 from dynd import nd
 
-from blaze.datadescriptor.user import into
+from blaze.datadescriptor.user import like
+from blaze.usability import like
 from blaze import array
 
-class Test_Into(unittest.TestCase):
+class Test_like(unittest.TestCase):
     def test_DDesc(self):
         from blaze.datadescriptor import DyND_DDesc
         dd = DyND_DDesc(nd.array([1, 2]))
-        self.assertEqual(into([], dd),
+        self.assertEqual(like([], dd),
                          [1, 2])
-        self.assertEqual(nd.as_py(into(nd.array(), dd)),
+        self.assertEqual(nd.as_py(like(nd.array(), dd)),
                          nd.as_py(nd.array([1, 2])))
-        b = into(array(0), dd)
-        self.assertEqual(into([], b), into([], dd))
+        b = like(array(0), dd)
+        self.assertEqual(like([], b), like([], dd))
--- blaze/datadescriptor/user.py
+++ blaze/datadescriptor/user.py
@@ -8,13 +8,13 @@ from .as_py import ddesc_as_py
 
 
 @dispatch(list, DDesc)
-def into(a, b):
+def like(a, b):
     return ddesc_as_py(b)
 
 @dispatch(nd.array, DDesc)
-def into(a, b):
+def like(a, b):
     return b.dynd_arr()
 
 @dispatch(Array, DDesc)
-def into(a, b):
+def like(a, b):
     return array(b)
