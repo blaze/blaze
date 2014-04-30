313b424fee9022d3d90e3cc4077d171e0f478251
Matthew Rocklin <mrocklin@gmail.com>
2014-04-30 10:14:46 -0500

    sql compute wraps select statement
    Also test arithmetic

--- blaze/compute/sql.py
+++ blaze/compute/sql.py
@@ -25,32 +25,39 @@ import sqlalchemy
 base = (int, float, str, bool)
 
 @dispatch(Projection, sqlalchemy.Table)
-def compute(t, s):
-    s = compute(t.table, s)
+def _compute(t, s):
+    s = _compute(t.table, s)
     return sa.select([s.c.get(col) for col in t.columns])
 
 
 @dispatch(Column, sqlalchemy.Table)
-def compute(t, s):
-    s = compute(t.table, s)
+def _compute(t, s):
+    s = _compute(t.table, s)
     return s.c.get(t.columns[0])
 
 
 @dispatch(base, object)
-def compute(a, b):
+def _compute(a, b):
     return a
 
 
-@dispatch(Relational, sqlalchemy.Table)
-def compute(t, s):
-    return t.op(compute(t.lhs, s), compute(t.rhs, s))
+@dispatch(ColumnWise, sqlalchemy.Table)
+def _compute(t, s):
+    return t.op(_compute(t.lhs, s), _compute(t.rhs, s))
 
 
 @dispatch(Selection, sqlalchemy.Table)
-def compute(t, s):
-    return sa.select([compute(t.table, s)]).where(compute(t.predicate, s))
+def _compute(t, s):
+    return sa.select([_compute(t.table, s)]).where(_compute(t.predicate, s))
 
 
 @dispatch(Table, sqlalchemy.Table)
-def compute(t, s):
+def _compute(t, s):
     return s
+
+@dispatch(Table, sqlalchemy.Table)
+def compute(t, s):
+    result = _compute(t, s)
+    if not isinstance(result, sqlalchemy.sql.selectable.Select):
+        result = sa.select([result])
+    return result
