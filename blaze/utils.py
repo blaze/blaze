57a5251ba94ec058225dcf578d8ef06109c2feea
Matthew Rocklin <mrocklin@gmail.com>
2014-05-07 14:15:02 -0700

    get util, nth_list is lazy

--- blaze/utils.py
+++ blaze/utils.py
@@ -2,6 +2,7 @@ from itertools import islice
 from contextlib import contextmanager
 import tempfile
 import os
+from collections import Iterator
 
 
 def partition_all(n, seq):
@@ -43,11 +44,11 @@ def nth(n, seq):
 def nth_list(n, seq):
     """
 
-    >>> nth_list([0, 1, 4], 'Hello')
+    >>> tuple(nth_list([0, 1, 4], 'Hello'))
     ('H', 'e', 'o')
-    >>> nth_list([4, 1, 0], 'Hello')
+    >>> tuple(nth_list([4, 1, 0], 'Hello'))
     ('o', 'e', 'H')
-    >>> nth_list([0, 0, 0], 'Hello')
+    >>> tuple(nth_list([0, 0, 0], 'Hello'))
     ('H', 'H', 'H')
     """
     seq = iter(seq)
@@ -63,7 +64,33 @@ def nth_list(n, seq):
         old = index
 
     order = [x[1] for x in sorted(zip(n, range(len(n))))]
-    return tuple(result[i] for i in order)
+    return (result[i] for i in order)
+
+
+def get(ind, coll, lazy=False):
+    """
+
+    >>> get(0, 'Hello')
+    'H'
+
+    >>> get([1, 0], 'Hello')
+    ('e', 'H')
+
+    >>> get(slice(1, 4), 'Hello')
+    ('e', 'l', 'l')
+
+    >>> get(slice(1, 4), 'Hello', lazy=True)  # doctest: +SKIP
+    <itertools.islice object at 0x25ac470>
+    """
+    if isinstance(ind, list):
+        result = nth_list(ind, coll)
+    elif isinstance(ind, slice):
+        result = islice(coll, ind.start, ind.stop, ind.step)
+    else:
+        result = coll[ind]
+    if lazy==False and isinstance(result, Iterator):
+        result = tuple(result)
+    return result
 
 
 @contextmanager
