93e6859508fe73c11623a3395bb7e138fdbe9d74
Matthew Rocklin <mrocklin@gmail.com>
2014-05-07 15:02:21 -0700

    add coerce_to_ordered
    This should be replaced once DyND nd.as_py supports ordered export

--- blaze/data/utils.py
+++ blaze/data/utils.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 from dynd import nd
 from collections import Iterator
+from datashape import dshape, Record
+from datashape.predicates import isunit, isdimension
 
 
 def validate(schema, item):
@@ -16,6 +18,30 @@ def coerce(dshape, item):
     if isinstance(item, Iterator):
         item = list(item)
     return nd.as_py(nd.array(item, dtype=str(dshape)))
+
+
+def coerce_to_ordered(ds, data):
+    """ Coerce data with dicts into an ordered ND collection
+
+    >>> from datashape import dshape
+
+    >>> coerce_to_ordered('{x: int, y: int}', {'x': 1, 'y': 2})
+    [1, 2]
+
+    >>> coerce_to_ordered('var * {x: int, y: int}',
+    ...                  [{'x': 1, 'y': 2}, {'x': 10, 'y': 20}])
+    [[1, 2], [10, 20]]
+    """
+    if isinstance(ds, str):
+        ds = dshape(ds)
+    if isinstance(ds[0], Record):
+        rec = ds[0]
+        return [coerce_to_ordered(rec[name], data[name])
+                for name in rec.names]
+    if isdimension(ds[0]):
+        return [coerce_to_ordered(ds.subarray(1), row)
+                for row in data]
+    return data
 
 
 def coerce_record_to_row(schema, rec):
