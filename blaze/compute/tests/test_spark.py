601ab429bf99e52c78859f394abb3a7ca8a831b2
talumbau <tj.alumbaugh@continuum.io>
2014-05-15 10:41:19 -0500

    join operation on pyspark rdds

--- blaze/compute/tests/test_spark.py
+++ blaze/compute/tests/test_spark.py
@@ -3,10 +3,7 @@ from __future__ import absolute_import, division, print_function
 from blaze.compute.spark import *
 from blaze.expr.table import *
 
-import os
-import platform
-import pyspark
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkContext
 
 t = TableSymbol('{name: string, amount: int, id: int}')
 
@@ -14,16 +11,42 @@ data = [['Alice', 100, 1],
         ['Bob', 200, 2],
         ['Alice', 50, 3]]
 
+t2 = TableSymbol('{name: string, city: string}')
+
+data2 = [['Alice', 'Austin'],
+         ['Bob', 'Boston']]
+
 sc = SparkContext("local", "Simple App")
 
 rdd = sc.parallelize(data)
+rdd2 = sc.parallelize(data2)
+
 
 def test_table():
     assert compute(t, rdd) == rdd
 
 
 def test_projection():
-    assert compute(t['name'], rdd).collect() == rdd.map(lambda x: x[0]).collect()
+    assert compute(t['name'], rdd).collect() == rdd.map(lambda x:
+                                                        x[0]).collect()
+
 
 def test_multicols_projection():
-    assert compute(t[['amount', 'name']], rdd).collect() == [[100, 'Alice'], [200, 'Bob'], [50, 'Alice']]
+    assert compute(t[['amount', 'name']], rdd).collect() == [[100, 'Alice'],
+                                                             [200, 'Bob'],
+                                                             [50, 'Alice']]
+
+
+def test_selection():
+    assert compute(t[t['name'] == 'Alice'], rdd).collect() ==\
+        rdd.filter(lambda x: x[0] == 'Alice').collect()
+
+
+def test_join():
+
+    joined = Join(t, t2, 'name')
+    expected = [['Alice', 100, 1, 'Austin'],
+                ['Bob', 200, 2, 'Boston'],
+                ['Alice', 50, 3, 'Austin']]
+    result = compute(joined, rdd, rdd2)
+    assert all([i in expected for i in result.collect()])
