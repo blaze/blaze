c00c38a3ee341ae55e4bb82fb9955e49c095555e
Matthew Rocklin <mrocklin@gmail.com>
2014-06-16 14:39:24 -0700

    serve: add simple datasets view

--- blaze/serve/server.py
+++ blaze/serve/server.py
@@ -8,3 +8,7 @@ datasets = dict()
 def hello():
     return 'Welcome to Blaze Server'
 
+@app.route('/datasets.json')
+def dataset():
+    return '\n'.join(': '.join((name, str(dset.dshape))) for name, dset in
+                                datasets.items())
