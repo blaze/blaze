06304486de36e7f0e6e99bf299e0011e441f7816
Matthew Rocklin <mrocklin@gmail.com>
2014-06-16 15:09:57 -0700

    serve: basic remote data indexing

--- blaze/serve/server.py
+++ blaze/serve/server.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from __future__ import absolute_import, division, print_function
+
+from flask import Flask, request
+import json
+
+from .index import parse_index
 
 app = Flask('Blaze-Server')
 
@@ -12,3 +17,38 @@ def hello():
 def dataset():
     return '\n'.join(': '.join((name, str(dset.dshape))) for name, dset in
                                 datasets.items())
+
+
+@app.route('/data/<name>.json', methods=['POST'])
+def data(name):
+    if request.headers['content-type'] != 'application/json':
+        return "Expected JSON"
+    try:
+        data = json.loads(request.data)
+    except ValueError:
+        return "Bad JSON.  Got %s " % request.data
+
+    try:
+        dset = datasets[name]
+    except KeyError:
+        return "Dataset not found"
+
+    try:
+        index = parse_index(data['index'])
+    except ValueError:
+        return "Bad index"
+
+    return json.dumps(dset.py.__getitem__(index))
+
+if __name__ == '__main__':
+    from blaze.data.python import Python
+
+    accounts = Python([['Alice', 100], ['Bob', 200]],
+                      schema='{name: string, amount: int32}')
+
+    cities = Python([['Alice', 'NYC'], ['Bob', 'LA'], ['Charlie', 'Beijing']],
+                      schema='{name: string, city: string}')
+
+    datasets['accounts'] = accounts
+    datasets['cities'] = cities
+    app.run(debug=True)
