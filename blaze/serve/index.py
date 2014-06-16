c8de620eaace148517963e9bac6aae1c2e0ae815
Matthew Rocklin <mrocklin@gmail.com>
2014-06-16 14:19:07 -0700

    serve: add emit_index

--- blaze/serve/index.py
+++ blaze/serve/index.py
@@ -8,3 +8,15 @@ def parse_index(ind):
     if isinstance(ind, dict):
         return slice(ind.get('start'), ind.get('stop'), ind.get('step'))
     raise ValueError('Do not know how to parse %s into an index' % str(ind))
+
+
+def emit_index(ind):
+    if isinstance(ind, (int, _strtypes)):
+        return ind
+    if isinstance(ind, tuple):
+        return list(map(emit_index, ind))
+    if isinstance(ind, slice):
+        result = {'start': ind.start, 'stop': ind.stop, 'step': ind.step}
+        if result['step'] is None:
+            del result['step']
+        return result
