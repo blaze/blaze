0b7b9ac5f53a690c30a5bc72324cabf5cb5d518c
Matthew Rocklin <mrocklin@gmail.com>
2014-06-16 14:39:24 -0700

    serve: add simple datasets view

--- blaze/serve/tests/test_server.py
+++ blaze/serve/tests/test_server.py
@@ -1,11 +1,25 @@
 from blaze.serve.server import app, datasets
 from blaze.data.python import Python
 
-dd = Python([['Alice', 100], ['Bob', 200]],
-            schema='{name: string, amount: int32}')
+accounts = Python([['Alice', 100], ['Bob', 200]],
+                  schema='{name: string, amount: int32}')
 
-datasets['accounts'] = dd
+cities = Python([['Alice', 'NYC'], ['Bob', 'LA'], ['Charlie', 'Beijing']],
+                  schema='{name: string, city: string}')
+
+datasets['accounts'] = accounts
+datasets['cities'] = cities
 
 def test_basic():
     test = app.test_client()
-    assert test.get('/')
+    assert 'OK' in test.get('/').status
+
+
+def test_datasets():
+    test = app.test_client()
+    response = test.get('/datasets.json')
+    assert 'accounts' in response.data
+    assert 'cities' in response.data
+
+    assert str(accounts.dshape) in response.data
+    assert str(cities.dshape) in response.data
