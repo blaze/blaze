0fde25e680b2d1c060b2e76b40b1297ae06937c0
Matthew Rocklin <mrocklin@gmail.com>
2014-06-18 12:00:20 -0700

    table: basic compute on exprs + Table

--- blaze/api/table.py
+++ blaze/api/table.py
@@ -1,8 +1,8 @@
-from ..expr.table import TableSymbol
+from ..expr.table import TableSymbol, TableExpr
 from ..data.core import DataDescriptor
 from ..data.python import Python
 from ..dispatch import dispatch
-from datashape import discover
+from datashape import discover, Tuple, Record, dshape
 import itertools
 
 names = ('_%d' % i for i in itertools.count(1))
@@ -18,8 +18,15 @@ class Table(TableSymbol):
         self.name = name or next(names)
 
     @dispatch((list, tuple))
-    def __init__(self, seq, name=None):
-        self.schema = discover(seq[:10]).subshape[0]
+    def __init__(self, seq, name=None, columns=None):
+        schema = discover(seq[:10]).subshape[0]
+        if isinstance(schema[0], Tuple):
+            columns = columns or list(range(len(schema[0].dshapes)))
+            types = schema[0].dshapes
+        if isinstance(schema[0], Record):
+            columns = columns or schema[0].names
+            types = schema[0].types
+        self.schema = dshape(Record(list(zip(columns, types))))
         self.data = tuple(seq)
         self.name = name or next(names)
 
@@ -30,3 +37,14 @@ class Table(TableSymbol):
 @dispatch(Table)
 def compute(t):
     return t.data
+
+
+@dispatch(TableExpr)
+def compute(expr):
+    resources = expr.resources()
+    if not resources:
+        raise ValueError("No data resources found")
+
+    else:
+        print(resources)
+        return compute(expr, resources)
--- blaze/api/tests/test_table.py
+++ blaze/api/tests/test_table.py
@@ -1,10 +1,12 @@
 from blaze.api.table import Table, compute
+from blaze.compute.core import compute
+from blaze.compute.python import compute
 
 
 data = (('Alice', 100),
         ('Bob', 200))
 
-t = Table(data)
+t = Table(data, columns=['name', 'amount'])
 
 def test_resources():
     assert t.resources() == {t: t.data}
@@ -12,3 +14,6 @@ def test_resources():
 
 def test_compute():
     assert compute(t) == data
+
+def test_compute():
+    assert list(compute(t['amount'] + 1)) == [101, 201]
