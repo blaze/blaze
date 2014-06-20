8431458f7f18ec0dde86d46ec18dbdb61412f8ef
Matthew Rocklin <mrocklin@gmail.com>
2014-06-20 07:25:38 -0700

    bump version

--- blaze/__init__.py
+++ blaze/__init__.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.WARNING)
 inf = float('inf')
 nan = float('nan')
 
-__version__ = '0.4.2-dev'
+__version__ = '0.6.0-dev'
 
 # If IPython is already loaded, register the Blaze catalog magic
 # from . import catalog
