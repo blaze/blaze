691d48cab5183eacfe2424bcbde41ff9348fcf55
Matthew Rocklin <mrocklin@gmail.com>
2014-06-25 17:30:19 -0700

    server: Selection API

--- blaze/serve/server.py
+++ blaze/serve/server.py
@@ -99,3 +99,80 @@ def data(datasets, name):
                     'index': data['index'],
                     'datashape': str(dset.dshape.subshape[index]),
                     'data': rv})
+
+from ..expr.table import *
+from ..expr.scalar.interface import exprify
+from ..api.into import into
+from ..compute.core import compute
+
+
+@route('/select/<name>.json', methods=['POST', 'PUT', 'GET'])
+def select(datasets, name):
+    """ Basic Selection API
+
+    Allows remote querying of datasets.  Takes query data as JSON
+
+    Takes requests like
+
+    Example
+    -------
+
+    For the following array:
+
+    [['Alice', 100],
+     ['Bob', 200],
+     ['Charlie', 300]]
+
+    schema = '{name: string, amount: int32}'
+
+    And the following
+
+    url: /data/table-name.json
+    POST-data: {'selection': 'amount >= 200',
+                'columns': 'name'}
+
+    and returns responses like
+
+    {"name": "table-name",
+     ...
+     "datashape": "2 * string",
+     "data": ["Bob", "Charlie"]}
+     """
+    if request.headers['content-type'] != 'application/json':
+        return ("Expected JSON data", 404)
+    try:
+        data = json.loads(request.data)
+    except ValueError:
+        return ("Bad JSON.  Got %s " % request.data, 404)
+
+    try:
+        dset = datasets[name]
+    except KeyError:
+        return ("Dataset %s not found" % name, 404)
+    t = TableSymbol('t', dset.schema)
+    dtypes = {c: t[c].dtype for c in t.columns}
+
+    columns = data.get('columns', None)
+    if columns:
+        try:
+            columns = data['columns']
+        except ValueError:
+            return ("Bad columns", 404)
+    try:
+        select = exprify(data['selection'], dtypes)
+    except (ValueError, KeyError):
+        return ("Bad selection", 404)
+
+    expr = Selection(t, ColumnWise(t, select))
+    if columns:
+        expr = expr[columns]
+    try:
+        rv = into([], compute(expr, dset))
+    except RuntimeError:
+        return ("Bad selection", 404)
+
+    return jsonify({'name': name,
+                    'columns': expr.columns,
+                    'selection': str(select),
+                    'datashape': str(expr.dshape),
+                    'data': rv})
--- blaze/serve/tests/test_server.py
+++ blaze/serve/tests/test_server.py
@@ -5,6 +5,8 @@ from flask import json
 from blaze.serve.server import Server
 from blaze.data.python import Python
 from blaze.serve.index import parse_index, emit_index
+from blaze.compute.python import compute
+
 
 accounts = Python([['Alice', 100], ['Bob', 200]],
                   schema='{name: string, amount: int32}')
@@ -12,7 +14,12 @@ accounts = Python([['Alice', 100], ['Bob', 200]],
 cities = Python([['Alice', 'NYC'], ['Bob', 'LA'], ['Charlie', 'Beijing']],
                   schema='{name: string, city: string}')
 
-server = Server({'accounts': accounts, 'cities': cities})
+pairs = Python([(1, 2), (2, 1), (3, 4), (4, 3)],
+               schema='{x: int, y: int}')
+
+server = Server(datasets={'accounts': accounts,
+                          'cities': cities,
+                          'pairs': pairs})
 
 test = server.app.test_client()
 
@@ -35,7 +42,8 @@ def test_full_response():
 def test_datasets():
     response = test.get('/datasets.json')
     assert json.loads(response.data) == {'accounts': str(accounts.dshape),
-                                         'cities': str(cities.dshape)}
+                                         'cities': str(cities.dshape),
+                                         'pairs': str(pairs.dshape)}
 
 
 def test_data():
@@ -68,3 +76,26 @@ def test_bad_responses():
                                  data = json.dumps(0),
                                  content_type='application/json').status
     assert 'OK' not in test.post('/data/accounts.json').status
+
+
+def test_selection():
+    query = {'selection': 'x > y'}
+    expected = [[2, 1], [4, 3]]
+
+    response = test.post('/select/pairs.json',
+                         data = json.dumps(query),
+                         content_type='application/json')
+    assert 'OK' in response.status
+    assert json.loads(response.data)['data'] == expected
+
+
+def test_selection_on_columns():
+    query = {'selection': 'city == "LA"',
+             'columns': 'name'}
+    expected = ['Bob']
+
+    response = test.post('/select/cities.json',
+                         data = json.dumps(query),
+                         content_type='application/json')
+    assert 'OK' in response.status
+    assert json.loads(response.data)['data'] == expected
