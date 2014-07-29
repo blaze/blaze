6a3d3f16da4105e9bb062dcdda47eb443617ce60
Matthew Rocklin <mrocklin@gmail.com>
2014-07-29 10:31:03 -0700

    spark: into

--- blaze/tests/test_spark.py
+++ blaze/tests/test_spark.py
@@ -9,3 +9,8 @@ def test_spark_coerce():
     rdd = sc.parallelize([('1', 'hello'), ('2', 'world')])
     assert (coerce('{x: int, y: string}', rdd).collect() ==
             [(1, 'hello'), (2, 'world')])
+
+
+def test_into():
+    rdd = sc.parallelize([('1', 'hello'), ('2', 'world')])
+    assert into([], rdd) == into([], rdd.collect())
