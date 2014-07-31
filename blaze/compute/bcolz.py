e9d2a0f3e1b78fe3b22b13ee9fd98ebc5b117710
Matthew Rocklin <mrocklin@gmail.com>
2014-07-30 19:53:18 -0700

    compute: add bcolz

---
+++ blaze/compute/bcolz.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import, division, print_function
+
+from blaze.expr.table import *
+from ..dispatch import dispatch
+import bcolz
+from toolz import map
+
+
+@dispatch(bcolz.ctable)
+def discover(t):
+    return datashape.from_numpy(t.shape, t.dtype)
+
+
+@dispatch(Selection, bcolz.ctable)
+def compute_one(sel, t, **kwargs):
+    s = eval_str(sel.predicate.expr)
+    return map(tuple, t.where(s))
+
+
+@dispatch(Head, bcolz.ctable)
+def compute_one(h, t, **kwargs):
+    return t[:h.n]
+
+
+@dispatch(Column, bcolz.ctable)
+def compute_one(c, t, **kwargs):
+    return t[c.column]
+
+
+@dispatch(Projection, bcolz.ctable)
+def compute_one(p, t, **kwargs):
+    return t[p.columns]
+
+
+@dispatch(count, bcolz.ctable)
+def compute_one(c, t):
+    return len(t)
+
+
+@dispatch(Expr, bcolz.ctable)
+def compute_one(e, t, **kwargs):
+    return compute_one(e, iter(t), **kwargs)
