3f95d82d51c1f747aaaef847c09dd5e3be403812
Matthew Rocklin <mrocklin@gmail.com>
2014-08-05 09:52:37 -0700

    into: blz <- ndarray, DataFrame, list, Iterator

--- blaze/blz.py
+++ blaze/blz.py
@@ -3,9 +3,14 @@ from __future__ import absolute_import, division, print_function
 import blz
 import numpy as np
 from pandas import DataFrame
+from collections import Iterator
+from toolz import partition_all
 
 from .dispatch import dispatch
 from .compute.blz import *
+
+
+__all__ = ['into']
 
 
 @dispatch((tuple, set, list, type, object), (blz.btable, blz.barray))
@@ -16,6 +21,41 @@ def into(o, b):
 @dispatch(np.ndarray, (blz.btable, blz.barray))
 def into(a, b):
     return b[:]
+
+
+@dispatch((blz.barray, blz.btable), np.ndarray)
+def into(a, b, **kwargs):
+    return blz.btable(b, **kwargs)
+
+
+
+def fix_len_string_filter(ser):
+    """ Convert object strings to fixed length, pass through others """
+    if ser.dtype == np.dtype('O'):
+        return np.asarray(list(ser))
+    else:
+        return np.asarray(ser)
+
+
+@dispatch(blz.btable, DataFrame)
+def into(a, df, **kwargs):
+    return blz.btable([fix_len_string_filter(df[c]) for c in df.columns],
+                      names=list(df.columns), **kwargs)
+
+
+@dispatch((blz.barray, blz.btable), (tuple, list))
+def into(a, b, **kwargs):
+    return blz.btable(list(zip(*b)), **kwargs)
+
+
+@dispatch((blz.barray, blz.btable), Iterator)
+def into(a, b, **kwargs):
+    chunks = partition_all(1024, b)
+    chunk = next(chunks)
+    a = blz.btable(list(zip(*chunk)), **kwargs)
+    for chunk in chunks:
+        a.append(list(zip(*chunk)))
+    return a
 
 
 @dispatch(DataFrame, blz.btable)
--- blaze/tests/test_blz.py
+++ blaze/tests/test_blz.py
@@ -17,6 +17,41 @@ def test_into_ndarray_btable():
                            dtype=[('a', int), ('b', float)]))
 
 
+def test_into_btable_numpy():
+    assert str(into(blz.btable, np.array([(1, 1.), (2, 2.), (3, 3.)],
+                            dtype=[('a', np.int32), ('b', np.float32)]))) == \
+            str(blz.btable([np.array([1, 2, 3], dtype=np.int32),
+                        np.array([1., 2., 3.], dtype=np.float32)],
+                       names=['a', 'b']))
+
+
+def test_into_btable_DataFrame():
+    df = DataFrame([[1, 'Alice'],
+                    [2, 'Bob'],
+                    [3, 'Charlie']], columns=['id', 'name'])
+
+    b = into(blz.btable, df)
+
+    assert list(b.names) == list(df.columns)
+    assert list(b['id']) == [1, 2, 3]
+    print(b['name'])
+    print(b['name'][0])
+    print(type(b['name'][0]))
+    assert list(b['name']) == ['Alice', 'Bob', 'Charlie']
+
+
+def test_into_btable_list():
+    b = into(blz.btable, [(1, 1.), (2, 2.), (3, 3.)], names=['a', 'b'])
+    assert list(b['a']) == [1, 2, 3]
+    assert b.names == ['a', 'b']
+
+
+def test_into_btable_iterator():
+    b = into(blz.btable, iter([(1, 1.), (2, 2.), (3, 3.)]), names=['a', 'b'])
+    assert list(b['a']) == [1, 2, 3]
+    assert b.names == ['a', 'b']
+
+
 def test_into_ndarray_barray():
     assert str(into(np.ndarray, b['a'])) == \
             str(np.array([1, 2, 3]))
