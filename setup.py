7135d33f62d65e6c84dc2133e2155e63c91e0e1f
Mark Wiebe <mwiebe@continuum.io>
2014-08-07 18:01:45 -0700

    Updates for version 0.6

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ longdesc = open('README.md').read()
 
 setup(
     name='blaze',
-    version='0.5',
+    version='0.6.0',
     author='Continuum Analytics',
     author_email='blaze-dev@continuum.io',
     description='Blaze',
