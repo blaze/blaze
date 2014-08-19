2654812e66ad7388a4dd7f1e53dbdf6511f74908
Phillip Cloud <cpcloud@gmail.com>
2014-08-19 17:48:22 -0400

    Implement projection and ColumnWise for PyTables

--- blaze/compute/pytables.py
+++ blaze/compute/pytables.py
@@ -1,12 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
+import numpy as np
 import tables as tb
-from blaze.expr import *
+from blaze.expr import Selection, Head, Column, ColumnWise, Projection
+from blaze.expr import eval_str
 from datashape import Record
 from ..dispatch import dispatch
-
-
-__all__ = ['tb']
 
 
 @dispatch(tb.Table)
@@ -20,6 +19,27 @@ def compute_one(sel, t, **kwargs):
     return t.read_where(s)
 
 
+@dispatch(Projection, tb.Table)
+def compute_one(proj, t, **kwargs):
+    # only options here are
+    # read the whole thing in and then select
+    # or
+    # create an output array that is at most the size of the on disk table and
+    # fill it will the columns iteratively
+    # both of these options aren't ideal but pytables has no way to select
+    # multiple column subsets so pick the one where we can optimize for the best
+    # case rather than prematurely pessimizing
+    #
+    # TODO: benchmark on big tables because i'm not sure exactly what the
+    # implications here are for memory usage
+    columns = list(proj.columns)
+    dtype = np.dtype([(col, t.dtype[col]) for col in columns])
+    out = np.empty(t.shape, dtype=dtype)
+    for c in columns:
+        out[c] = t.col(c)
+    return out
+
+
 @dispatch(Head, tb.Table)
 def compute_one(h, t, **kwargs):
     return t[:h.n]
@@ -28,3 +48,11 @@ def compute_one(h, t, **kwargs):
 @dispatch(Column, tb.Table)
 def compute_one(c, t, **kwargs):
     return t.col(c.column)
+
+
+@dispatch(ColumnWise, tb.Table)
+def compute_one(c, t, **kwargs):
+    columns = c.active_columns()
+    uservars = dict((col, getattr(t.cols, col)) for col in columns)
+    expr = tb.Expr(str(c.expr), uservars=uservars)
+    return expr.eval()
