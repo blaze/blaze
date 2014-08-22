d20134c49c6e5a711275705eca256df2212ccf30
Phillip Cloud <cpcloud@gmail.com>
2014-08-22 11:16:19 -0400

    Remove generic create_index from pytables

--- blaze/compute/pytables.py
+++ blaze/compute/pytables.py
@@ -20,32 +20,6 @@ def drop(t):
     t.remove()
 
 
-@dispatch(object)
-def create_index(o):
-    """Create an index on a column.
-
-    Parameters
-    ----------
-    o : indexable object
-
-    Examples
-    --------
-    >>> import tables as tb
-    >>> import numpy as np
-    >>> data = [(1, 2.0, 'a'), (2, 3.0, 'b'), (3, 4.0, 'c')]
-    >>> arr = np.array(data, dtype=[('id', 'i8'), ('value', 'f8'),
-    ...                             ('key', '|S1')])
-    >>> f = tb.open_file(tempfile.mkstemp(), mode='w')
-    >>> t = f.create_table('/', 'table', arr)
-    >>> create_index(t, 'id')
-    >>> 'id' in t.colsindexed
-    >>> t.close()
-    >>> f.close()
-    """
-    raise NotImplementedError("create_index not implemented for type %r" %
-                              type(o).__name__)
-
-
 @dispatch(tb.Table, basestring)
 def create_index(t, column_name, **kwargs):
     create_index(getattr(t.cols, column_name), **kwargs)
