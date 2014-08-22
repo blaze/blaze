894ac119b697c4c70b9c7bb9b6513715d58c8b9e
Matthew Rocklin <mrocklin@gmail.com>
2014-08-22 08:53:36 -0700

    into exhaustive: remove csv from base targets

--- blaze/api/tests/test_into_exhaustive.py
+++ blaze/api/tests/test_into_exhaustive.py
@@ -89,7 +89,7 @@ def normalize(a):
 def test_base():
     """ Test all pairs of base in-memory data structures """
     sources = [v for k, v in data.items() if k not in [list]]
-    targets = [v for k, v in data.items() if k not in [Table, Collection]]
+    targets = [v for k, v in data.items() if k not in [Table, Collection, CSV]]
     for a in sources:
         for b in targets:
             assert normalize(into(type(b), a)) == normalize(b)
