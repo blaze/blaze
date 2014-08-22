bb083b193f66d625d63fe7ba30d949342e224a07
Phillip Cloud <cpcloud@gmail.com>
2014-08-22 11:02:12 -0400

    Tests for create_index sql

--- blaze/tests/test_sql.py
+++ blaze/tests/test_sql.py
@@ -1,6 +1,6 @@
 import pytest
-from sqlalchemy.exc import OperationalError
-from blaze.sql import drop
+from sqlalchemy.exc import OperationalError, ArgumentError
+from blaze.sql import drop, create_index
 from blaze import compute, Table, SQL
 
 
@@ -26,3 +26,41 @@ def test_drop(sql):
     drop(sql)
     with pytest.raises(OperationalError):
         drop(sql)
+
+
+class TestCreateIndex(object):
+
+    def test_create_index(self, sql):
+        create_index(sql, 'idx', 'x')
+        with pytest.raises(OperationalError):
+            create_index(sql, 'idx', 'x')
+
+    def test_create_index_fails(self, sql):
+        with pytest.raises(AttributeError):
+            create_index(sql, 'zidx', 'z')
+
+    def test_composite_index(self, sql):
+        create_index(sql, 'idx_xy', ['x', 'y'])
+        with pytest.raises(OperationalError):
+            create_index(sql, 'idx_xy', ['x', 'y'])
+
+    def test_composite_index_fails(self, sql):
+        with pytest.raises(AttributeError):
+            create_index(sql, 'idx_name', ['z', 'bizz'])
+
+    def test_composite_index_fails_with_existing_columns(self, sql):
+        with pytest.raises(AttributeError):
+            create_index(sql, 'idx_name', ['x', 'z', 'bizz'])
+
+    def test_multiple_indexes(self, sql):
+        create_index(sql, {'idx_x': 'x', 'idx_y': 'y'})
+        with pytest.raises(OperationalError):
+            create_index(sql, {'idx_x': 'x', 'idx_y': 'y'})
+
+    def test_multiple_indexes_fails(self, sql):
+        with pytest.raises(AssertionError):
+            create_index(sql, {'idx_z': 'z', 'idx_h': 'h'})
+
+    def test_multiple_indexes_fails_with_existing_columns(self, sql):
+        with pytest.raises(AssertionError):
+            create_index(sql, {'idx_z': 'z', 'idx_y': 'y'})
