e54c744fbf2d33741b8202f66c852836a5b8a902
Dav Clark <davclark@berkeley.edu>
2014-08-22 18:50:37 -0700

    @mrocklin added a test

--- blaze/tests/test_rpy2.py
+++ blaze/tests/test_rpy2.py
@@ -13,3 +13,11 @@ def test_rpy2_into():
     pdf2 = into(pd.DataFrame, rdf)
     # This could still be better prolly -DJC
     assert (pdf2 == pdf).all().all()
+
+def test_into_list():
+    pdf = pd.DataFrame({'a': [1,2,3], 'b': [4,5,5.5]})
+    rdf = into(Sexp, pdf)
+    L = into(list, rdf)
+
+    assert isinstance(L, list)
+    assert L == [(1, 4), (2, 5), (3, 5.5)]
