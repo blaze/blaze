c2b241241ca714caa91785459c9d9fd98172b012
Phillip Cloud <cpcloud@gmail.com>
2014-08-23 00:45:40 -0400

    Test multiple column sort

--- blaze/compute/tests/test_rethink.py
+++ blaze/compute/tests/test_rethink.py
@@ -60,3 +60,14 @@ def test_selection(ts, tb):
     q = ts[(ts.name == 'Alice') & (ts.amount < 200)]
     result = compute_one(q, tb)
     assert result == [{'name': 'Alice', 'amount': 100, 'id': 3}]
+
+
+def test_multiple_column_sort(ts, tb):
+    expr = ts.sort(['name', 'id'], ascending=False).head(3)[['name', 'id']]
+    result = compute_one(expr, tb, conn)
+    bank = [{'name': 'Alice', 'id': 3},
+            {'name': 'Alice', 'id': 4},
+            {'name': 'Bob', 'id': 7},
+            {'name': 'Bob', 'id': 10},
+            {'name': 'Bob', 'id': 42}]
+    assert result == bank[:-4:-1]
