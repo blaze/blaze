dc29704a6ac0a28a39bd7e9de80cd692a55f2ad5
Matthew Rocklin <mrocklin@gmail.com>
2014-08-26 13:17:18 -0700

    resource: list of files using glob

--- blaze/api/resource.py
+++ blaze/api/resource.py
@@ -2,13 +2,29 @@ from __future__ import absolute_import, division, print_function
 from dynd import nd
 import pandas as pd
 import os
+from glob import glob
 from ..dispatch import dispatch
 from ..data import DataDescriptor, CSV
+from blaze.compute.chunks import ChunkIndexable
+
+class ChunkList(ChunkIndexable):
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        return iter(self.data)
 
 
-def resource(a, **kwargs):
-    if os.path.isfile(a) and a.endswith("csv"):
-        return CSV(a, **kwargs)
+def resource(uri, **kwargs):
+    if '*' in uri:
+        uris = sorted(glob(uri))
+        return ChunkList(list(map(resource, uris)))
+
+    if os.path.isfile(uri) and uri.endswith("csv"):
+        return CSV(uri, **kwargs)
     else:
         raise NotImplementedError(
-            "Blaze can't read '%s' yet!" % (a))
+            "Blaze can't read '%s' yet!" % uri)
--- blaze/api/tests/test_resource.py
+++ blaze/api/tests/test_resource.py
@@ -3,7 +3,15 @@ from blaze.data import *
 from blaze.api.into import into
 
 def test_resource_csv():
-    assert isinstance(resource('blaze/api/tests/a1.csv'), CSV)
+    assert isinstance(resource('blaze/api/tests/accounts_1.csv'), CSV)
 
 def test_into_resource():
-    assert into(list, 'blaze/api/tests/a1.csv')
+    assert into(list, 'blaze/api/tests/accounts_1.csv') == [(1, 'Alice', 100),
+                                                            (2, 'Bob', 200)]
+
+def test_into_directory_of_csv_files():
+    assert into(list, 'blaze/api/tests/accounts_*.csv') == [(1, 'Alice', 100),
+                                                            (2, 'Bob', 200),
+                                                            (3, 'Charlie', 300),
+                                                            (4, 'Dan', 400),
+                                                            (5, 'Edith', 500)]
