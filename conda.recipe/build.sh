96cb0a73846049658418023c1ee327a74de460b6
Benjamin Zaitlen <quasiben@gmail.com>
2014-09-02 14:45:48 -0500

    adding override of build number with git describe

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+BLD_DIR=`pwd`
+
 # Recipe and source are stored together
 SRC_DIR=$RECIPE_DIR/..
 pushd $SRC_DIR
 
+# X.X.X.dev builds
+version=`git describe --tags`
+
+echo $version.dev> __conda_version__.txt
+cp __conda_version__.txt $BLD_DIR
+
 $PYTHON setup.py install
 popd
+
