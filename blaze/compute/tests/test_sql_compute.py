d36b247bf075bd725f353d6740e4eb17a9e26539
Phillip Cloud <cpcloud@gmail.com>
2014-09-09 16:09:03 -0400

    Fix typo

--- blaze/compute/tests/test_sql_compute.py
+++ blaze/compute/tests/test_sql_compute.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import pytest
 from blaze.compute.sql import compute, computefull, select
 from blaze import SQL
 from blaze.expr import *
