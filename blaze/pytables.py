df5ce33ce6133b0453e3512ebbfceca5e1706d52
Matthew Rocklin <mrocklin@gmail.com>
2014-09-10 10:28:53 -0700

    pytables: define chunks and get_chunk on pytables

--- blaze/pytables.py
+++ blaze/pytables.py
@@ -55,3 +55,19 @@ def PyTables(path, datapath, dshape=None):
             possibly_create_table(filename, dtype)
             shutil.copyfile(filename, path)
     return tb.open_file(path, mode='a').get_node(datapath)
+
+
+@dispatch(tb.Table)
+def chunks(b, chunksize=2**15):
+    start = 0
+    n = len(b)
+    while start < n:
+        yield b[start:start + chunksize]
+        start += chunksize
+
+
+@dispatch(tb.Table, int)
+def get_chunk(b, i, chunksize=2**15):
+    start = chunksize * i
+    stop = chunksize * (i + 1)
+    return b[start:stop]
