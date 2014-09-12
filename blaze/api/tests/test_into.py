413ecb18bc19d6bf36332bfd3dedf0c599ca0f10
Phillip Cloud <cpcloud@gmail.com>
2014-09-12 14:59:11 -0400

    Cleanup test_into to use pytest effectively

--- blaze/api/tests/test_into.py
+++ blaze/api/tests/test_into.py
@@ -6,27 +6,20 @@ from datashape import dshape
 from datetime import datetime
 import os
 
+import pandas as pd
+from pandas import DataFrame
+from blaze.data.python import Python
+from blaze.data import CSV
+
 from blaze.api.into import into, discover
 from blaze import Table
-from blaze.utils import tmpfile
+from blaze.utils import tmpfile, filetext
 import pytest
 
-def skip(test_foo):
-    return
 
-def skip_if_not(x):
-    def maybe_a_test_function(test_foo):
-        if not x:
-            return
-        else:
-            return test_foo
-    return maybe_a_test_function
-
-
-class Test_into(unittest.TestCase):
+class TestInto(unittest.TestCase):
     def test_containers(self):
-        self.assertEqual(into([], (1, 2, 3)),
-                                  [1, 2, 3])
+        self.assertEqual(into([], (1, 2, 3)), [1, 2, 3])
         self.assertEqual(into([], iter((1, 2, 3))),
                                   [1, 2, 3])
         self.assertEqual(into((), (1, 2, 3)),
@@ -63,129 +56,105 @@ class Test_into(unittest.TestCase):
                          str(into(np.ndarray(()), (1, 2, 3))))
 
 
-try:
-    from pandas import DataFrame
-except ImportError:
-    DataFrame = None
-
-try:
-    from blaze.data import Python, CSV
-except ImportError:
-    Python = None
-    CSV = None
-
-try:
-    from bokeh.objects import ColumnDataSource
-except ImportError:
-    ColumnDataSource = None
-
-try:
-    from tables import (Table as PyTables, open_file, UInt8Col, StringCol,
-        IsDescription)
-except ImportError:
-    PyTables = None
-
 @pytest.yield_fixture
 def h5():
+    pytest.importorskip('tables')
+    from tables import IsDescription, UInt8Col, StringCol, open_file
+
     class Test(IsDescription):
-        posted_dow = UInt8Col()
-        jobtype  = UInt8Col()
-        location  = UInt8Col()
-        date  = StringCol(20)
-        country  = StringCol(2)
+        posted_dow = UInt8Col(pos=0)
+        jobtype = UInt8Col(pos=1)
+        location = UInt8Col(pos=2)
+        date = StringCol(20, pos=3)
+        country = StringCol(2, pos=4)
 
     with tmpfile('.h5') as filename:
-        h5file = open_file(filename, mode = "w", title = "Test file")
+        h5file = open_file(filename, mode="w", title="Test file")
         group = h5file.create_group("/", 'test', 'Info')
         tab = h5file.create_table(group, 'sample', Test, "Example")
-        arow = tab.row
-        arow['country'] = 'ab'
-        arow['date'] = '20121105'
-        arow['location'] = 0
-        arow['jobtype']  = 1
-        arow['posted_dow']  = 3
+
         # Insert a new record
-        arow.append()
+        tab.append([(3, 1, 0, '20121105', 'ab')])
         tab.flush()
+
         yield h5file
-        # Close (and flush) the file
         h5file.close()
+
+
+@pytest.fixture
+def data():
+    return [('Alice', 100), ('Bob', 200)]
+
+
+@pytest.fixture
+def schema():
+    return '{name: string, amount: int}'
+
+
+@pytest.fixture
+def data_table(data, schema):
+    return Table(data, schema=schema)
 
 
 @pytest.yield_fixture
 def good_csv():
     with tmpfile(".csv") as filename:
-        badfile = open(filename, mode="w")
-        # Insert a new record
-        badfile.write("userid,text,country\n")
-        badfile.write("1,Alice,az\n")
-        badfile.write("2,Bob,bl\n")
-        badfile.write("3,Charlie,cz\n")
-        badfile.flush()
-        yield badfile
-        # Close (and flush) the file
-        badfile.close()
+        with open(filename, mode='w') as f:
+            # Insert a new record
+            f.write("userid,text,country\n")
+            f.write("1,Alice,az\n")
+            f.write("2,Bob,bl\n")
+            f.write("3,Charlie,cz\n")
+            f.flush()
+            yield f
 
 
 @pytest.yield_fixture
 def bad_csv_df():
     with tmpfile(".csv") as filename:
-        badfile = open(filename, mode="w")
-        # Insert a new record
-        badfile.write("userid,text,country\n")
-        badfile.write("1,Alice,az\n")
-        badfile.write("2,Bob,bl\n")
-        for i in range(0,100):
-            badfile.write(str(i) + ",badguy,zz\n")
-        badfile.write("4,Dan,gb,extra,extra\n")
-        badfile.flush()
-        yield badfile
-        # Close (and flush) the file
-        badfile.close()
+        with open(filename, mode='w') as badfile:
+            # Insert a new record
+            badfile.write("userid,text,country\n")
+            badfile.write("1,Alice,az\n")
+            badfile.write("2,Bob,bl\n")
+            for i in range(100):
+                badfile.write("%d,badguy,zz\n" % i)
+            badfile.write("4,Dan,gb,extra,extra\n")
+            badfile.flush()
+            yield badfile
+
 
 @pytest.yield_fixture
 def out_hdf5():
+    pytest.importorskip('tables')
     with tmpfile(".h5") as filename:
         yield filename
 
-@skip_if_not(PyTables and DataFrame)
+
 def test_into_pytables_dataframe(h5):
     samp = h5.root.test.sample
-    final = into(DataFrame, samp)
+    final = into(pd.DataFrame, samp)
     assert len(final) == 1
 
-@skip_if_not(DataFrame and Python)
-def test_pandas_data_descriptor():
-    data = [['Alice', 100], ['Bob', 200]]
-    schema='{name: string, amount: int}'
+
+def test_pandas_data_descriptor(data, schema):
     dd = Python(data, schema=schema)
     result = into(DataFrame, dd)
     expected = DataFrame(data, columns=['name', 'amount'])
-    print(result)
-    print(expected)
-
     assert str(result) == str(expected)
 
 
-@skip_if_not(DataFrame and nd.array)
-def test_pandas_dynd():
-    data = [['Alice', 100], ['Bob', 200]]
-    schema='{name: string, amount: int}'
-
+def test_pandas_dynd(data, schema):
     arr = nd.array(data, dtype=schema)
 
     result = into(DataFrame, arr)
     expected = DataFrame(data, columns=['name', 'amount'])
-    print(result)
-    print(expected)
 
     assert str(result) == str(expected)
 
 
-@skip_if_not(DataFrame)
-def test_pandas_numpy():
-    data = [('Alice', 100), ('Bob', 200)]
-    dtype=[('name', 'O'), ('amount', int)]
+def test_pandas_numpy(data):
+    dtype = [('name', 'O'), ('amount', int)]
 
     x = np.array(data, dtype=dtype)
 
@@ -195,12 +164,9 @@ def test_pandas_numpy():
 
     result = into(DataFrame(columns=['name', 'amount']), x)
     expected = DataFrame(data, columns=['name', 'amount'])
-    print(result)
-    print(expected)
     assert str(result) == str(expected)
 
 
-@skip_if_not(DataFrame)
 def test_pandas_seq():
     assert str(into(DataFrame, [1, 2])) == \
             str(DataFrame([1, 2]))
@@ -210,9 +176,7 @@ def test_pandas_seq():
             str(DataFrame([[1, 2], [3, 4]], columns=['a', 'b']))
 
 
-@skip_if_not(DataFrame)
-def test_pandas_pandas():
-    data = [('Alice', 100), ('Bob', 200)]
+def test_pandas_pandas(data):
     df = DataFrame(data, columns=['name', 'balance'])
     new_df = into(DataFrame, df)
     # Data must be the same
@@ -221,9 +185,7 @@ def test_pandas_pandas():
     assert id(new_df) != id(df)
 
 
-@skip_if_not(DataFrame)
-def test_DataFrame_Series():
-    data = [('Alice', 100), ('Bob', 200)]
+def test_DataFrame_Series(data):
     df = DataFrame(data, columns=['name', 'balance'])
 
     new_df = into(DataFrame, df['name'])
@@ -236,53 +198,41 @@ def test_DataFrame_Series():
     assert isinstance(new_df, DataFrame)
 
 
-def test_discover_ndarray():
-    data = [['Alice', 100], ['Bob', 200]]
-    schema='{name: string, balance: int32}'
+def test_discover_ndarray(data, schema):
     arr = nd.array(data, dtype=schema)
     assert discover(arr) == 2 * dshape(schema)
 
-@skip_if_not(DataFrame)
-def test_discover_pandas():
-    data = [['Alice', 100], ['Bob', 200]]
+
+def test_discover_pandas(data, schema):
     df = DataFrame(data, columns=['name', 'balance'])
-
-    print(discover(df))
-    assert discover(df).subshape[0] == dshape('{name: string, balance: int64}')
+    assert discover(df).subshape[0] == dshape(schema)
 
 
-@skip_if_not(DataFrame and nd.array)
-def test_discover_pandas():
-    data = [('Alice', 100), ('Bob', 200)]
+def test_discover_pandas(data):
     df = DataFrame(data, columns=['name', 'balance'])
 
     result = into(nd.array, df)
 
     assert nd.as_py(result, tuple=True) == data
 
-@skip_if_not(Table and DataFrame)
-def test_into_table_dataframe():
-    data = [['Alice', 100], ['Bob', 200]]
-    t = Table(data, columns=['name', 'amount'])
 
+def test_into_table_dataframe(data_table, data):
+    t = data_table
     assert list(into(DataFrame(), t).columns) == list(t.columns)
     assert into([], into(DataFrame(), t)) == list(map(tuple, data))
 
 
-@skip_if_not(Table and ColumnDataSource)
-def test_Column_data_source():
-    data = [('Alice', 100), ('Bob', 200)]
-    t = Table(data, columns=['name', 'balance'])
+def test_Column_data_source(data_table):
+    pytest.importorskip('bokeh')
+    from bokeh.objects import ColumnDataSource
 
-    cds = into(ColumnDataSource(), t)
+    cds = into(ColumnDataSource(), data_table)
 
     assert isinstance(cds, ColumnDataSource)
-    assert set(cds.column_names) == set(t.columns)
+    assert set(cds.column_names) == set(data_table.columns)
 
 
-def test_numpy_list():
-    data = [('Alice', 100), ('Bob', 200)]
-
+def test_numpy_list(data):
     dtype = into(np.ndarray, data).dtype
     assert np.issubdtype(dtype[0], str)
     assert np.issubdtype(dtype[1], int)
@@ -290,16 +240,12 @@ def test_numpy_list():
     assert into([], into(np.ndarray, data)) == data
 
 
-@skip_if_not(Table)
-def test_numpy_tableExpr():
-    data = [('Alice', 100), ('Bob', 200)]
-    t = Table(data, '{name: string, amount: int64}')
-
-    assert into(np.ndarray, t).dtype == \
-            np.dtype([('name', 'O'), ('amount', 'i8')])
+def test_numpy_table_expr(data):
+    t = Table(data, schema='{name: string, amount: int64}')
+    assert (into(np.ndarray, t).dtype ==
+            np.dtype([('name', 'O'), ('amount', 'i8')]))
 
 
-@skip_if_not(DataFrame and CSV)
 def test_DataFrame_CSV():
     with filetext('1,2\n3,4\n') as fn:
         csv = CSV(fn, schema='{a: int64, b: float64}')
@@ -312,31 +258,29 @@ def test_DataFrame_CSV():
         assert list(df.dtypes) == [np.int64, np.float64]
 
 
-@skip_if_not(PyTables and DataFrame)
 def test_into_tables_path(good_csv, out_hdf5):
-    tble = into(PyTables, good_csv.name, filename=out_hdf5, datapath='foo')
+    import tables as tb
+    tble = into(tb.Table, good_csv.name, filename=out_hdf5, datapath='foo')
     assert len(tble) == 3
 
     tble.close()
 
 
-@skip_if_not(CSV and DataFrame)
-def test_into_tables_path(good_csv):
+@pytest.mark.xfail(raises=TypeError)
+def test_into_csv_blaze_table(good_csv):
     csv = CSV(good_csv.name)
     t = Table(csv)
-    df = into(DataFrame, t[['userid', 'text']])
+    df = into(pd.DataFrame, t[['userid', 'text']])
     assert list(df.columns) == ['userid', 'text']
 
 
-
-@skip_if_not(PyTables and DataFrame)
 def test_into_tables_path_bad_csv(bad_csv_df, out_hdf5):
-    tble = into(PyTables, bad_csv_df.name,
-                          filename=out_hdf5,
-                          datapath='foo',
-                          error_bad_lines=False)
+    import tables as tb
+    tble = into(tb.Table, bad_csv_df.name, filename=out_hdf5, datapath='foo',
+                error_bad_lines=False)
     df_from_tbl = into(DataFrame, tble)
-    #Check that it's the same as straight from the CSV
+
+    # Check that it's the same as straight from the CSV
     df_from_csv = into(DataFrame, bad_csv_df.name, error_bad_lines=False)
     assert len(df_from_csv) == len(df_from_tbl)
     assert list(df_from_csv.columns) == list(df_from_tbl.columns)
@@ -352,7 +296,7 @@ def test_numpy_datetimes():
 
 
 def test_numpy_python3_bytes_to_string_conversion():
-    x = np.array(['a','b'], dtype='S1')
+    x = np.array(['a', 'b'], dtype='S1')
     assert all(isinstance(s, str) for s in into(list, x))
     x = np.array([(1, 'a'), (2, 'b')], dtype=[('id', 'i4'), ('letter', 'S1')])
     assert isinstance(into(list, x)[0][1], str)
@@ -365,7 +309,7 @@ def test_into_DataFrame_Excel_xls_format():
     exp = DataFrame([[100, 1, "Alice", "2000-12-25T00:00:01"],
                     [200, 2, "Bob", "2001-12-25T00:00:01"],
                     [300, 3, "Charlie", "2002-12-25T00:00:01"]],
-                    columns = ["amount", "id", "name", "timestamp"])
+                    columns=["amount", "id", "name", "timestamp"])
     df = into(DataFrame, fn)
     assert (df == exp).all().all()
 
@@ -376,7 +320,7 @@ def test_into_DataFrame_Excel_xlsx_format():
     fn = os.path.join(dirname, 'accounts_1.xlsx')
     exp = DataFrame([[1, "Alice", 100],
                      [2, "Bob", 200]],
-                    columns = ["id", "name", "amount"])
+                    columns=["id", "name", "amount"])
     df = into(DataFrame, fn)
     assert (df == exp).all().all()
 
