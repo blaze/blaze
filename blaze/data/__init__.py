60280847899125bda41ff5f738555836c6977e59
Phillip Cloud <cpcloud@gmail.com>
2014-09-23 21:06:14 -0400

    Remove python lib imports from blaze

--- blaze/data/__init__.py
+++ blaze/data/__init__.py
@@ -10,4 +10,4 @@ from .meta import *
 
 __all__ = ['CSV', 'Concat', 'DataDescriptor', 'HDF5', 'JSON', 'JSON_Streaming',
            'SQL', 'Stack', 'coerce', 'coerce_row_to_dict', 'coerce_to_ordered',
-           'date', 'datetime', 'discover', 'dshape', 'nd', 'time', 'Excel']
+           'discover', 'dshape', 'nd', 'Excel']
