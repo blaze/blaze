68e48f00747da4c58db71e4d25370db86de9d95d
Phillip Cloud <cpcloud@gmail.com>
2014-09-25 15:55:28 -0400

    Cleanup

--- blaze/api/resource.py
+++ blaze/api/resource.py
@@ -5,6 +5,7 @@ from ..regex import RegexDispatcher
 
 resource = RegexDispatcher('resource')
 
+
 @resource.register('.*', priority=1)
 def resource_all(uri, *args, **kwargs):
     raise NotImplementedError("Unable to parse uri to data resource: " + uri)
