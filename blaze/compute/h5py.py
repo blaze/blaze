f48cff86818144686c22d699cad2a07dbcf867e2
Matthew Rocklin <mrocklin@gmail.com>
2014-10-13 08:01:28 -0700

    h5py: first try at chunked compute_down

--- blaze/compute/h5py.py
+++ blaze/compute/h5py.py
@@ -2,15 +2,16 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import h5py
+from multipledispatch import MDNotImplementedError
+from operator import mul
 
-from ..expr import Reduction, Field, Projection, Broadcast, Selection
-from ..expr import Distinct, Sort, Head, Label, ReLabel, Union, Expr, Slice
-from ..expr import std, var, count, nunique
-from ..expr import BinOp, UnaryOp, USub, Not
+from ..expr import *
+from ..expr.split import *
 
 from .core import base, compute
 from ..dispatch import dispatch
 from ..api.into import into
+from ..partition import partitions, partition_get, partition_set
 
 __all__ = []
 
@@ -18,3 +19,50 @@ __all__ = []
 @dispatch(Slice, h5py.Dataset)
 def compute_up(expr, data, **kwargs):
     return data[expr.index]
+
+
+def listshape(L):
+    """
+
+    >>> listshape([1, 2, 3])
+    (3,)
+    >>> listshape([[1, 2], [3, 4], [5, 6]])
+    (3, 2)
+    """
+    result = ()
+    while isinstance(L, list):
+        result = result + (len(L),)
+        L = L[0]
+    return result
+
+
+@dispatch(Expr, h5py.Dataset)
+def compute_down(expr, data, **kwargs):
+    chunkshape = data.chunks
+    assert blockshape
+
+    leaf = expr._leaves()[0]
+    chunk = Symbol('chunk', DataShape(*(chunkshape + (leaf.dshape.measure,))))
+    try:
+        (chunk, chunk_expr), (agg, agg_expr) = split(leaf, expr, chunk=chunk)
+    except:
+        raise MDNotImplementedError()
+
+
+    leaf_partitions = partitions(data, blockshape=blockshape)
+
+    chunkshape, dtype = datashape.to_numpy(chunk_expr.dshape)
+
+    intermediate = np.empty(shape=tuple(map(mul, shape, listshape(leaf_partitions)),
+                            dtype=dtype))
+
+    intermediate_partitions = partitions(intermediate, blockshape=shape)
+
+    for source, target in zip(flatten(leaf_partitions),
+                              flatten(intermediate_partitions)):
+        inp = partition_get(data, source, blockshape=blockshape)
+        out = compute(chunk_expr, {chunk: inp})
+        partition_set(intermediate, target, out, blockshape=shape)
+
+    result = compute(agg_expr, {agg: intermediate})
+    return result
--- blaze/compute/tests/test_h5py.py
+++ blaze/compute/tests/test_h5py.py
@@ -10,6 +10,8 @@ from blaze.expr import Symbol
 from datashape import discover
 from blaze.utils import tmpfile
 
+from blaze.compute.h5py import *
+
 
 def eq(a, b):
     return (a == b).all()
@@ -32,3 +34,29 @@ s = Symbol('s', discover(x))
 def test_slicing(data):
     for idx in [0, 1, (0, 1), slice(1, 3), (0, slice(1, 5, 2))]:
         assert eq(compute(s[idx], data), x[idx])
+
+
+@pytest.yield_fixture
+def points():
+    with tmpfile('.h5') as filename:
+        f = h5py.File(filename)
+        d = f.create_dataset('/x', shape=(10, 5),
+                                   dtype=[('x', 'f4'), ('y', 'f4')],
+                                   chunks=(2, 2))
+        d[:] = x
+        yield d
+        f.close()
+
+p = np.empty((10, 5), dtype=[('x', 'f4'), ('y', 'f4')])
+p['x'] = x
+p['y'] = x
+
+ps = Symbol('ps', discover(p))
+
+def test_compute_down_on_everything(points):
+    expr = (ps.x + 1).sum(axis=0)
+    result = compute(expr, points)
+    expected = compute(expr, p)
+
+    assert eq(result, expected)
+
