adeeafffbe51794b4b88f3176fa58fcd3ed556ea
Matthew Rocklin <mrocklin@gmail.com>
2014-10-17 10:57:34 -0700

    expr: broadcast test tabular case

--- blaze/expr/tests/test_broadcast.py
+++ blaze/expr/tests/test_broadcast.py
@@ -40,3 +40,15 @@ def test_broadcast_function():
     b = broadcast(expr, [x, Mult(2, y)])
     xx, yy = b._scalars
     assert b._scalar_expr.isidentical((xx + yy) ** 2)
+
+
+t = Symbol('t', 'var * {x: int, y: int, z: int}')
+
+
+def test_tabular_case():
+    expr = Add(x, Mult(y, 2))
+
+    b = broadcast(expr, [t])
+    tt, = b._scalars
+
+    assert b._scalar_expr.isidentical(tt.x + tt.y * 2)
