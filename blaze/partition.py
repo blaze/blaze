ed51e8258412ef1764f390d18e96f855f99d0814
Matthew Rocklin <mrocklin@gmail.com>
2014-10-20 17:14:42 -0500

    docstring for partitions

--- blaze/partition.py
+++ blaze/partition.py
@@ -1,3 +1,36 @@
+""" Abstraction around partitionable data
+
+Some data can be partitioned into disjoint pieces.
+Examples include lists, tuples, and arrays of various flavors.
+
+This module provides functions to abstract away the handling of such
+partitions.  It proposes three operations, each of these operations deals with
+data (like a numpy array) and a *partition-key* which is some token to define a
+part of the data.  In the case of numpy (and other array-like data) a natural
+partition-key might be a tuple of slices, e.g.
+
+    (slice(0, 10), slice(0, 10)) :: partition-key
+
+Our three operations:
+
+
+    partitions :: data -> [partition-keys]
+        Get list or list-of-lists of partition-keys
+
+    partition_get :: data, partition-key -> partition
+        Get a particular partition from a dataset
+
+    partition_set :: data, partition-key, value -> void
+        Set the value of a particular partition in a dataset
+
+Using these three operations we should be able to write down very simple and
+abstract algorithms like "copy"
+
+    def copy(in_data, out_data):
+        for in_key, out_key in zip(partitions(in_data), partitions(out_data)):
+            data = partition_get(in_data, in_key)
+            partition_set(out_data, out_key, data)
+"""
 from .dispatch import dispatch
 
 import numpy as np
