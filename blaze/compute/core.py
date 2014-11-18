8114a1b5079ca8f8b10693b29cce5c95afdf4057
Matthew Rocklin <mrocklin@gmail.com>
2014-11-18 13:15:59 -0800

    compute: pre_compute is given top expression, not leaf

--- blaze/compute/core.py
+++ blaze/compute/core.py
@@ -428,7 +428,7 @@ def compute(expr, d, **kwargs):
 
     expr2, d2 = swap_resources_into_scope(expr, d)
     if pre_compute_:
-        d3 = dict((e, pre_compute_(e, dat)) for e, dat in d2.items())
+        d3 = dict((e, pre_compute_(expr2, dat)) for e, dat in d2.items())
     else:
         d3 = d2
 
