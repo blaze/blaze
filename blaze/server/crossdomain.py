ed4d205151d506262f931913d27e2166d70f57a1
hugo <hugo.r.shi@gmail.com>
2014-11-18 00:08:46 -0500

    docstring

--- blaze/server/crossdomain.py
+++ blaze/server/crossdomain.py
@@ -7,6 +7,32 @@ from six import string_types
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
                 automatic_options=True):
+    """from http://flask.pocoo.org/snippets/56/
+    Cross-site HTTP requests are HTTP requests for resources
+    from a different domain than the domain of the resource
+    making the request. For instance, a resource loaded from
+    Domain A makes a request for a resource on Domain B. The
+    way this is implemented in modern browsers is by using
+    HTTP Access Control headers:
+    Documentation on developer.mozilla.org.
+    methods: Optionally a list of methods that are allowed for
+      this view. If not provided it will allow
+      all methods that are implemented.
+    headers: Optionally a list of headers
+      that are allowed for this request.
+    origin: '*' to allow all origins, otherwise a
+      string with a URL or a list of URLs that
+      might access the resource.
+    max_age: The number of seconds as integer or timedelta
+      object for which the preflighted request is valid.
+    attach_to_all: True if the decorator should add the access
+      control headers to all HTTP methods or False
+      if it should only add them to OPTIONS responses.
+    automatic_options: If enabled the decorator will use the
+      default Flask OPTIONS response and attach the headers there,
+      otherwise the view function will be called to generate
+      an appropriate response.
+    """
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
     if headers is not None and not isinstance(headers, string_types):
