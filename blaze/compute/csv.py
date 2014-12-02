5d6ae0f3270b34272cc110336b2863897cfba8eb
Matthew Rocklin <mrocklin@gmail.com>
2014-12-02 10:20:34 -0800

    csv: nicer pre_compute on Head and Projection

--- blaze/compute/csv.py
+++ blaze/compute/csv.py
@@ -5,10 +5,12 @@ import os
 
 from ..dispatch import dispatch
 from ..data.csv import CSV
-from ..expr import Expr
+from ..expr import Expr, Head, ElemWise, Distinct, Symbol, Projection, Field
+from ..expr.core import path
 from ..utils import available_memory
 from ..expr.split import split
-from toolz import curry, concat
+from ..api.into import into
+from toolz import curry, concat, map
 import pandas as pd
 import numpy as np
 from collections import Iterator, Iterable
@@ -17,10 +19,31 @@ from .core import compute
 @dispatch(Expr, CSV)
 def pre_compute(expr, data, comfortable_memory=None, chunksize=2**18, **kwargs):
     comfortable_memory = comfortable_memory or min(1e9, available_memory() / 4)
-    if os.path.getsize(data.path) < comfortable_memory:
-        return data.pandas_read_csv()
+
+    kwargs = dict()
+
+    # Chunk if the file is large
+    if os.path.getsize(data.path) > comfortable_memory:
+        kwargs['chunksize'] = chunksize
+
+    # Insert projection into read_csv
+    leaf = expr._leaves()[0]
+    pth = list(path(expr, leaf))
+    if len(pth) >= 2 and isinstance(pth[-2], (Projection, Field)):
+        kwargs['usecols'] = pth[-2].fields
+
+    return data.pandas_read_csv(**kwargs)
+
+
+Cheap = (Head, ElemWise, Distinct, Symbol)
+
+@dispatch(Head, CSV)
+def pre_compute(expr, data, **kwargs):
+    leaf = expr._leaves()[0]
+    if all(isinstance(e, Cheap) for e in path(expr, leaf)):
+        return into(Iterator, data)
     else:
-        return data.pandas_read_csv(chunksize=chunksize)
+        raise MDNotImplementedError()
 
 
 def compute_chunk(chunk, chunk_expr, part):
@@ -43,3 +66,13 @@ def compute_down(expr, data, map=map, **kwargs):
         intermediate = concat(parts)
 
     return compute(agg_expr, {agg: intermediate})
+
+
+@dispatch(Iterator, pandas.io.parsers.TextFileReader)
+def into(a, b, **kwargs):
+    return concat(map(into(a), b))
+
+
+@dispatch((list, tuple, set), pandas.io.parsers.TextFileReader)
+def into(a, b, **kwargs):
+    return into(a, into(Iterator, b))
--- blaze/compute/tests/test_csv_compute.py
+++ blaze/compute/tests/test_csv_compute.py
@@ -4,6 +4,7 @@ from blaze.utils import example
 from blaze.expr import Expr, symbol
 from pandas import DataFrame
 import pandas
+from collections import Iterator
 
 def test_pre_compute_on_small_csv_gives_dataframe():
     csv = CSV(example('iris.csv'))
@@ -18,8 +19,24 @@ def test_pre_compute_on_large_csv_gives_chunked_reader():
                       pandas.io.parsers.TextFileReader)
 
 
+def test_pre_compute_with_head_on_large_csv_yields_iterator():
+    csv = CSV(example('iris.csv'))
+    s = symbol('s', csv.dshape)
+    assert isinstance(pre_compute(s.species.head(), csv, comfortable_memory=10),
+                      Iterator)
+
+
 def test_compute_chunks_on_single_csv():
     csv = CSV(example('iris.csv'))
     s = symbol('s', csv.dshape)
     expr = s.sepal_length.max()
     assert compute(expr, {s: csv}, comfortable_memory=10, chunksize=50) == 7.9
+
+
+def test_pre_compute_with_projection_projects_on_data_frames():
+    csv = CSV(example('iris.csv'))
+    s = symbol('s', csv.dshape)
+    result = pre_compute(s[['sepal_length', 'sepal_width']].distinct(),
+                         csv, comfortable_memory=10)
+    assert set(result.get_chunk().columns) == \
+            set(['sepal_length', 'sepal_width'])
