884e7a99aca57f25be5f570e8fab3e97a2c562d3
Matthew Rocklin <mrocklin@gmail.com>
2014-12-03 19:06:57 -0800

    remove .sql import

--- blaze/data/__init__.py
+++ blaze/data/__init__.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, print_function
 from .core import *
 from .csv import *
 from .excel import *
-from .sql import *
 from .json import *
 from .hdf5 import *
 from .meta import *
