4f87e7391257f131487b6e77afc6485fa7905aa3
Matthew Rocklin <mrocklin@gmail.com>
2014-12-15 17:55:41 -0800

    bump version to 0.6.8

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ with open('README.md') as f:
 
 setup(
     name='blaze',
-    version='0.6.7',
+    version='0.6.8',
     author='Continuum Analytics',
     author_email='blaze-dev@continuum.io',
     description='Blaze',
