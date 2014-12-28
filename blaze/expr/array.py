802e53b3a9a113f8ac31a159e104c69d69d77e52
Matthew Rocklin <mrocklin@gmail.com>
2014-12-28 10:12:45 -0800

    expr: tensordot

--- blaze/expr/array.py
+++ blaze/expr/array.py
@@ -1,5 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
 from .expressions import Expr, ndim
 from datashape import DataShape
+from collections import Iterable
 
 class Transpose(Expr):
     __slots__ = '_hash', '_child', 'axes'
@@ -22,11 +25,51 @@ def T(expr):
     return transpose(expr)
 
 
+class TensorDot(Expr):
+    __slots__ = '_hash', 'lhs', 'rhs', '_left_axes', '_right_axes'
+    __inputs__ = 'lhs', 'rhs'
+
+    @property
+    def dshape(self):
+        shape = tuple([d for i, d in enumerate(self.lhs.shape)
+                         if i not in self._left_axes] +
+                      [d for i, d in enumerate(self.rhs.shape)
+                         if i not in self._right_axes])
+        # TODO: handle type promotion
+        return DataShape(*(shape + (self.lhs.dshape.measure,)))
+
+def tensordot(lhs, rhs, axes=None):
+    if axes is None:
+        if ndim(lhs) == 2:
+            left = ndim(lhs) - 1
+        right = 0
+    elif isinstance(axes, Iterable):
+        left, right = axes
+    else:
+        left, right = axes, axes
+
+    if isinstance(left, int):
+        left = (left,)
+    if isinstance(right, int):
+        right = (right,)
+    if isinstance(left, list):
+        left = tuple(left)
+    if isinstance(right, list):
+        right = tuple(right)
+
+    return TensorDot(lhs, rhs, left, right)
+
+def dot(lhs, rhs):
+    return tensordot(lhs, rhs)
+
+
+from datashape.predicates import isnumeric, isboolean
 from .expressions import dshape_method_list, method_properties
 
 dshape_method_list.extend([
     (lambda ds: ndim(ds) > 1, set([transpose])),
     (lambda ds: ndim(ds) == 2, set([T])),
+    (lambda ds: ndim(ds) >= 1 and (isnumeric(ds) or isboolean(ds)), set([dot]))
     ])
 
 method_properties.add(T)
--- blaze/expr/tests/test_array.py
+++ blaze/expr/tests/test_array.py
@@ -2,6 +2,7 @@ from blaze.expr import *
 from blaze.expr.array import *
 
 
+w = symbol('x', '5 * 6 * int32')
 x = symbol('x', '4 * 5 * int32')
 y = symbol('y', '5 * int32')
 z = symbol('z', '4 * int32')
@@ -14,3 +15,16 @@ def test_transpose():
     assert ndim(x.dshape) == 2
 
     assert x.T.shape == (5, 4)
+
+
+def test_dot():
+    assert x.dot(y).shape == (4,)
+    assert x.dot(w).shape == (4, 6)
+
+
+def test_tensordot():
+    expr = tensordot(x, z, axes=[0, 0])
+    assert expr.shape == (5,)
+
+    expr = tensordot(x, z, axes=[[0], [0]])
+    assert expr.shape == (5,)
