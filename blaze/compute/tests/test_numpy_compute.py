6472958bc54b695cc1680cd86b420a8d5dcc3abb
Phillip Cloud <cpcloud@gmail.com>
2014-12-29 15:41:28 -0500

    Add test for dtypes

--- blaze/compute/tests/test_numpy_compute.py
+++ blaze/compute/tests/test_numpy_compute.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
+import numbers
 import pytest
 from datetime import datetime, date
 
@@ -67,6 +68,8 @@ def test_invert_not():
 def test_Reductions():
     assert compute(t['amount'].mean(), x) == x['amount'].mean()
     assert compute(t['amount'].count(), x) == len(x['amount'])
+    assert isinstance(compute(t['amount'].count(), x), (numbers.Integral,
+                                                        np.integer))
     assert compute(t['amount'].sum(), x) == x['amount'].sum()
     assert compute(t['amount'].min(), x) == x['amount'].min()
     assert compute(t['amount'].max(), x) == x['amount'].max()
