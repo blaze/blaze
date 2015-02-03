302abdeae9b95e600edd1aa6044fb7d931d597eb
Matthew Rocklin <mrocklin@gmail.com>
2015-02-03 07:32:26 -0800

    test that blaze.abs behaves well

--- blaze/expr/tests/test_functions.py
+++ blaze/expr/tests/test_functions.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import, division, print_function
 
 from blaze.expr.functions import *
-from blaze.expr import TableSymbol, Expr, Symbol
+from blaze.expr import Expr, symbol
+from blaze.expr import math
 import numpy as np
 
 
 def test_reductions():
     assert max([1, 2, 3]) == 3
     assert max(np.array([1, 2, 3])) == 3
-    assert isinstance(max(TableSymbol('t', '{x: int, y: int}').x), Expr)
+    assert isinstance(max(symbol('t', 'var * {x: int, y: int}').x), Expr)
     assert all([True, True, True]) is True
     assert all([True, True, False]) is False
 
@@ -17,4 +18,11 @@ def test_math():
     assert sin(0) == 0
     assert isinstance(sin(0), float)
     assert isinstance(sin(np.int32(0)), np.float)
-    assert isinstance(sin(Symbol('x', 'real')), Expr)
+    assert isinstance(sin(symbol('x', 'real')), Expr)
+
+
+def test_abs():
+    assert abs(-1) == 1
+    assert (abs(np.array([-1, 2, 3])) == np.array([1, 2, 3])).all()
+    x = symbol('x', 'int')
+    assert isinstance(abs(x), math.abs)
