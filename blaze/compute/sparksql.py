b60a62636d3beff15df88130f6a3b319de96f285
Phillip Cloud <cpcloud@gmail.com>
2015-02-07 20:50:21 -0500

    Gut sparksql

--- blaze/compute/sparksql.py
+++ blaze/compute/sparksql.py
@@ -1,135 +1,136 @@
 from __future__ import absolute_import, division, print_function
 
-import toolz
-from toolz import pipe
-import itertools
-from datashape import discover, Unit, Tuple, Record, iscollection, isscalar
+import datashape
+from datashape import discover
+from datashape.predicates import isrecord
 import sqlalchemy as sa
+from toolz import pipe, curry
+from toolz.curried import filter
+from into import convert
 from into.backends.sql import dshape_to_alchemy
 
 from ..dispatch import dispatch
-from ..expr import *
+from ..expr import Expr, Field, symbol
+from .core import compute
 from .utils import literalquery
 
-__all__ = []
-
-try:
-    import pyspark
-    from pyspark.sql import SchemaRDD
-except ImportError:
-    SchemaRDD = type(None)
-
-names = ('_table_%d' % i for i in itertools.count(1))
 
 __all__ = []
 
-class SparkSQLQuery(object):
-    """ Pair of PySpark SQLContext and SQLAlchemy Table
-
-    Python's SparkSQL interface only accepts strings.  We use SQLAlchemy to
-    generate these strings.  To do this we'll have to pass around pairs of
-    (SQLContext, sqlalchemy.Selectable).  Additionally we track a mapping of
-    {schemardd: sqlalchemy.Table}
-
-    Parameters
-    ----------
-
-    context: pyspark.sql.SQLContext
-
-    query: sqlalchemy.Selectable
-
-    mapping: dict :: {pyspark.sql.SchemaRDD: sqlalchemy.Table}
-    """
-    __slots__ = 'context', 'query', 'mapping'
-
-    def __init__(self, context, query, mapping):
-        self.context = context
-        self.query = query
-        self.mapping = mapping
+from pyspark.sql import SQLContext
+import pyspark.sql as sql
 
 
-def make_query(rdd, primary_key='', name=None):
-    # SparkSQL
-    name = name or next(names)
-    context = rdd.sql_ctx
-    context.registerRDDAsTable(rdd, name)
-
-    # SQLAlchemy
-    schema = discover(rdd).subshape[0]
-    columns = dshape_to_alchemy(schema)
-    for column in columns:
-        if column.name == primary_key:
-            column.primary_key = True
-
-    metadata = sa.MetaData()  # TODO: sync this between many tables
-
-    query = sa.Table(name, metadata, *columns)
-
-    mapping = {rdd: query}
-
-    return SparkSQLQuery(context, query, mapping)
+@convert.register(list, sql.DataFrame)
+def sparksql_dataframe_to_list(df, **kwargs):
+    return list(map(tuple, df.collect()))
 
 
-@dispatch(Symbol, SchemaRDD)
-def compute_up(ts, rdd, **kwargs):
-    return make_query(rdd)
+@dispatch(Field, SQLContext)
+def compute_up(expr, data, **kwargs):
+    name = expr._name
+    columns = dshape_to_alchemy(expr.dshape)
+    return sa.Table(name, sa.MetaData(), *columns)
 
 
-@dispatch((var, Label, std, Sort, count, nunique, Selection, mean,
-           Head, ReLabel, Distinct, ElemWise, By, any, all, sum, max,
-           min, Reduction, Projection, Field), SchemaRDD)
-def compute_up(e, rdd, **kwargs):
-    return compute_up(e, make_query(rdd), **kwargs)
+@curry
+def istable(db, t):
+    return (isinstance(t, Field) and isrecord(t.dshape.measure) and
+            t._child.isidentical(db))
 
 
-@dispatch((BinOp, Join),
-          (SparkSQLQuery, SchemaRDD),
-          (SparkSQLQuery, SchemaRDD))
-def compute_up(e, a, b, **kwargs):
-    if not isinstance(a, SparkSQLQuery):
-        a = make_query(a)
-    if not isinstance(b, SparkSQLQuery):
-        b = make_query(b)
-    return compute_up(e, a, b, **kwargs)
+@dispatch(Expr, SQLContext)
+def compute_down(expr, data):
+    """ Compile a blaze expression to a sparksql expression"""
+    leaves = expr._leaves()
+
+    # make sure we only have a single leaf node
+    if len(leaves) != 1:
+        raise ValueError('Must compile from exactly one root database')
+
+    leaf, = leaves
+
+    # field expressions on the database are Field instances with a record
+    # measure whose immediate child is the database leaf
+    tables = pipe(expr._subterms(), filter(istable(leaf)), list)
+
+    # raise if we don't have tables in our database
+    if not tables:
+        raise ValueError('Expressions not referencing a table cannot be '
+                         'compiled')
+
+    # make new symbols for each table
+    new_leaves = [symbol(t._name, t.dshape) for t in tables]
+
+    # sub them in the expression
+    expr = expr._subs(dict(zip(tables, new_leaves)))
+
+    # get sqlalchemy tables, we can't go through compute_down here as that will
+    # recurse back into this function
+    sa_tables = [compute_up(t, data) for t in tables]
+
+    # compute using sqlalchemy
+    scope = dict(zip(new_leaves, sa_tables))
+    query = compute(expr, scope)
+
+    # interpolate params
+    qs = str(literalquery(query))
+    return data.sql(qs)
 
 
-@dispatch((UnaryOp, Expr), SparkSQLQuery)
-def compute_up(expr, q, **kwargs):
-    scope = kwargs.pop('scope', dict())
-    scope = dict((t, q.mapping.get(data, data)) for t, data in scope.items())
-
-    q2 = compute_up(expr, q.query, scope=scope, **kwargs)
-    return SparkSQLQuery(q.context, q2, q.mapping)
-
-
-@dispatch((BinOp, Join, Expr), SparkSQLQuery, SparkSQLQuery)
-def compute_up(expr, a, b, **kwargs):
-    assert a.context == b.context
-
-    mapping = toolz.merge(a.mapping, b.mapping)
-
-    scope = kwargs.pop('scope', dict())
-    scope = dict((t, mapping.get(data, data)) for t, data in scope.items())
-
-    c = compute_up(expr, a.query, b.query, scope=scope, **kwargs)
-    return SparkSQLQuery(a.context, c, mapping)
+# see http://spark.apache.org/docs/latest/sql-programming-guide.html#spark-sql-datatype-reference
+sparksql_to_dshape = {
+    sql.ByteType: datashape.int8,
+    sql.ShortType: datashape.int16,
+    sql.IntegerType: datashape.int32,
+    sql.LongType: datashape.int64,
+    sql.FloatType: datashape.float32,
+    sql.DoubleType: datashape.float64,
+    sql.StringType: datashape.string,
+    sql.BinaryType: datashape.bytes_,
+    sql.BooleanType: datashape.bool_,
+    sql.TimestampType: datashape.datetime_,
+    sql.DateType: datashape.date_,
+    # sql.ArrayType: ?,
+    # sql.MapTYpe: ?,
+    # sql.StructType: ?
+}
 
 
-from .sql import select
-def sql_string(query):
-    return pipe(query, select, literalquery, str)
+def schema_to_dshape(schema):
+    dshape = []
+    for field in schema.fields:
+        name = field.name
+        value = sparksql_to_dshape.get(type(field.dataType))
+        if value is None:
+            raise ValueError('No known mapping for SparkSQL type %r to '
+                             'datashape type' % str(field.dataType))
+        dshape.append((name, value))
+    return datashape.Record(dshape)
 
-@dispatch(Expr, SparkSQLQuery)
-def post_compute(expr, query, scope=None):
-    result = query.context.sql(sql_string(query.query))
-    if iscollection(expr.dshape) and isscalar(expr.dshape.measure):
-        result = result.map(lambda x: x[0])
-    return result
+
+def scala_set_to_set(ctx, x):
+    from py4j.java_gateway import java_import
+
+    # import scala
+    java_import(ctx._jvm, 'scala')
+
+    # grab Scala's set converter and convert to a Python set
+    return set(ctx._jvm.scala.collection.JavaConversions.setAsJavaSet(x))
 
 
-@dispatch(Head, SparkSQLQuery)
-def post_compute(expr, query, scope=None):
-    result = query.context.sql(sql_string(query.query))
-    if iscollection(expr.dshape) and isscalar(expr.dshape.measure):
-        result = result.map(lambda x: x[0])
-    return result.collect()
+def get_catalog(ctx):
+    # the .catalog() method yields a SimpleCatalog instance. This class is
+    # hidden from the Python side, but is present in the public Scala API
+    java_names = ctx._ssql_ctx.catalog().tables().keySet()
+    table_names = scala_set_to_set(ctx, java_names)
+    tables = map(ctx.table, table_names)
+    return dict(zip(table_names, tables))
+
+
+@dispatch(SQLContext)
+def discover(ctx):
+    catalog = get_catalog(ctx)
+    tableshapes = [(name, datashape.var * schema_to_dshape(t.schema()))
+                   for name, t in catalog.items()]
+    return datashape.DataShape(datashape.Record(tableshapes))
