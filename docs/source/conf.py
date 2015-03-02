6fb0d8830b4e734321fdae636a8bbece473f60a5
Phillip Cloud <cpcloud@gmail.com>
2015-03-02 14:45:32 -0500

    Guard against not having RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,9 +142,14 @@ exclude_patterns = []
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    try:
+        import sphinx_rtd_theme
+    except ImportError:
+        html_theme = 'default'
+        html_theme_path = []
+    else:
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of the Pygments (syntax highlighting) style to use.
 highlight_language = 'python'
