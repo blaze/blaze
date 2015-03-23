c3a88aba05b383265e762c886eb154b3818223e1
Phillip Cloud <cpcloud@gmail.com>
2015-03-23 11:58:18 -0400

    Test multicolumn sort in sparksql

--- blaze/compute/tests/test_sparksql.py
+++ blaze/compute/tests/test_sparksql.py
@@ -223,7 +223,8 @@ def test_math(ctx, db, func):
 
 
 @pytest.mark.parametrize(['field', 'ascending'],
-                         itertools.product(['name', 'id'], [True, False]))
+                         itertools.product(['name', ['name', 'amount']],
+                                           [True, False]))
 def test_sort(ctx, db, field, ascending):
     expr = db.t.sort(field, ascending=ascending)
     result = compute(expr, ctx)
