4ee3e1f787607f20a7b84d80364633f1c9063c67
Blake Griffith <blake.a.griffith@gmail.com>
2015-04-08 11:29:36 -0500

    pandas compute_up for isin expression

--- blaze/compute/pandas.py
+++ blaze/compute/pandas.py
@@ -33,7 +33,7 @@ from ..dispatch import dispatch
 from ..expr import (Projection, Field, Sort, Head, Broadcast, Selection,
                     Reduction, Distinct, Join, By, Summary, Label, ReLabel,
                     Map, Apply, Merge, std, var, Like, Slice, summary,
-                    ElemWise, DateTime, Millisecond, Expr, Symbol,
+                    ElemWise, DateTime, Millisecond, Expr, Symbol, IsIn,
                     UTCFromTimestamp, nelements, DateTimeTruncate, count,
                     UnaryStringFunction)
 from ..expr import UnaryOp, BinOp
@@ -546,3 +546,12 @@ units_map = {
 @dispatch(DateTimeTruncate, Series)
 def compute_up(expr, data, **kwargs):
     return Series(compute_up(expr, into(np.ndarray, data), **kwargs))
+
+@dispatch(IsIn, DataFrame)
+def compute_up(expr, df, **kwargs):
+    return df.isin(expr)
+
+@dispatch(IsIn, Series)
+def compute_up(expr, df, **kwargs):
+    k = list(expr._key)
+    return df.isin(k)
