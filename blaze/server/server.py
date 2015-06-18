1068c03861cc97baa1f12253d15e64ec88779862
Phillip Cloud <cpcloud@gmail.com>
2015-06-18 14:41:57 -0400

    Remove _main from server and stick it spider.py

--- blaze/server/server.py
+++ blaze/server/server.py
@@ -357,37 +357,3 @@ def compserver(serial_format):
         'data': result,
         'names': expr.fields
     })
-
-
-def _main():
-    import argparse
-    import os
-
-    from blaze import spider
-
-    p = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('path', type=str,
-                   help=('A directory to recurse into or a YAML file '
-                         'specifying the resources to conjure up'))
-    p.add_argument('-p', '--port', type=int, default=DEFAULT_PORT,
-                   help='Port number')
-    p.add_argument('-l', '--follow-links', action='store_true',
-                   help='Follow links when listing files')
-    p.add_argument('-e', '--ignored-exception', nargs='*',
-                   default=['Exception'],
-                   help='Exceptions to ignore when calling resource on a file')
-    p.add_argument('-H', '--hidden', action='store_true',
-                   help='Call resource on hidden files')
-    args = p.parse_args()
-    resources = spider(os.path.expanduser(args.directory),
-                       followlinks=args.follow_links,
-                       ignore=tuple(getattr(__builtins__, e)
-                                    for e in args.ignored_exceptions),
-                       hidden=args.hidden,
-                       globs=args.globs)
-    Server(resources).run(port=args.port)
-
-
-if __name__ == '__main__':
-    _main()
