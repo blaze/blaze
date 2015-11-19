7627fcb359fad7f85928215e8f6988d6de91c5f8
Phillip Cloud <cpcloud@gmail.com>
2015-11-19 16:22:52 -0600

    Add test case

--- blaze/compute/tests/test_sql_compute.py
+++ blaze/compute/tests/test_sql_compute.py
@@ -1862,3 +1862,14 @@ def test_shift_on_column(n):
     FROM accounts
     """
     assert normalize(str(result)) == normalize(expected)
+
+
+def test_empty_string_comparison():
+    expr = t.amount[t.name == '']
+    result = compute(expr, s)
+    expected = """
+    SELECT accounts.amount
+    FROM accounts
+    WHERE accounts.name = :name_1
+    """
+    assert normalize(str(result)) == normalize(expected)
