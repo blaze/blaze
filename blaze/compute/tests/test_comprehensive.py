5f51ff7798ca5f1262df9fed53700f6c405f2102
Phillip Cloud <cpcloud@gmail.com>
2015-11-27 14:52:14 -0500

    Fixup test comprehensive to use fixtures

--- blaze/compute/tests/test_comprehensive.py
+++ blaze/compute/tests/test_comprehensive.py
@@ -1,45 +1,48 @@
 from __future__ import absolute_import, division, print_function
 
+import pytest
 import numpy as np
 from pandas import DataFrame
-import numpy as np
-from odo import resource, into
+from odo import resource, odo
 from datashape.predicates import isscalar, iscollection, isrecord
 from blaze.expr import symbol, by
 from blaze.interactive import Data
 from blaze.compute import compute
 from blaze.expr.functions import sin, exp
+import pandas as pd
+import pandas.util.testing as tm
 
-
-sources = []
 
 t = symbol('t', 'var * {amount: int64, id: int64, name: string}')
 
-L = [[ 100, 1, 'Alice'],
-     [ 200, 2, 'Bob'],
-     [ 300, 3, 'Charlie'],
+L = [[100, 1, 'Alice'],
+     [200, 2, 'Bob'],
+     [300, 3, 'Charlie'],
      [-400, 4, 'Dan'],
-     [ 500, 5, 'Edith']]
+     [500, 5, 'Edith']]
 
 df = DataFrame(L, columns=['amount', 'id', 'name'])
 
-x = into(np.ndarray, df)
+x = odo(df, np.ndarray)
 
-sources = [df, x]
+sources = {
+    type(df): df,
+    type(x): x
+}
 
 try:
-    import sqlalchemy
+    __import__('sqlalchemy')
     sql = resource('sqlite:///:memory:::accounts', dshape=t.dshape)
-    into(sql, L)
-    sources.append(sql)
-except:
+    odo(L, sql)
+    sources.update({type(sql): sql})
+except ImportError:
     sql = None
 
 
 try:
     import bcolz
-    bc = into(bcolz.ctable, df)
-    sources.append(bc)
+    bc = odo(df, bcolz.ctable)
+    sources.update({type(bc): bc})
 except ImportError:
     bc = None
 
@@ -47,8 +50,9 @@ try:
     import pymongo
 except ImportError:
     pymongo = mongo = None
-if pymongo:
 
+
+if pymongo is not None:
     try:
         db = pymongo.MongoClient().db
 
@@ -58,94 +62,91 @@ if pymongo:
             coll = db['_test_comprehensive']
 
         coll.drop()
-        mongo = into(coll, df)
-        sources.append(mongo)
+        mongo = odo(df, coll)
+        sources.update({type(mongo): mongo})
     except pymongo.errors.ConnectionFailure:
         mongo = None
 
-# {expr: [list-of-exclusions]}
-expressions = {
-        t: [],
-        t['id']: [],
-        abs(t['amount']): [],
-        t.id.max(): [],
-        t.amount.sum(): [],
-        t.amount.sum(keepdims=True): [],
-        t.amount.count(keepdims=True): [],
-        t.amount.nunique(keepdims=True): [mongo],
-        t.amount.nunique(): [],
-        t.amount.head(): [],
-        t.amount + 1: [mongo],
-        sin(t.amount): [sql, mongo], # sqlite doesn't support trig
-        exp(t.amount): [sql, mongo],
-        t.amount > 50: [mongo],
-        t[t.amount > 50]: [],
-        t.like(name='Alic*'): [],
-        t.sort('name'): [bc],
-        t.sort('name', ascending=False): [bc],
-        t.head(3): [],
-        t.name.distinct(): [],
-        t[t.amount > 50]['name']: [], # odd ordering issue
-        t.id.map(lambda x: x + 1, schema='int64', name='id'): [sql, mongo],
-        t[t.amount > 50]['name']: [],
-        by(t.name, total=t.amount.sum()): [],
-        by(t.id, count=t.id.count()): [],
-        by(t[['id', 'amount']], count=t.id.count()): [],
-        by(t[['id', 'amount']], total=(t.amount + 1).sum()): [mongo],
-        by(t[['id', 'amount']], n=t.name.nunique()): [mongo, bc],
-        by(t.id, count=t.amount.count()): [],
-        by(t.id, n=t.id.nunique()): [mongo, bc],
-        # by(t, count=t.count()): [],
-        # by(t.id, count=t.count()): [],
-        t[['amount', 'id']]: [x], # https://github.com/numpy/numpy/issues/3256
-        t[['id', 'amount']]: [x, bc], # bcolz sorting
-        t[0]: [sql, mongo, bc],
-        t[::2]: [sql, mongo, bc],
-        t.id.utcfromtimestamp: [sql],
-        t.distinct().nrows: [],
-        t.nelements(axis=0): [],
-        t.nelements(axis=None): [],
-        t.amount.truncate(200): [sql]
-        }
 
-base = df
+@pytest.mark.parametrize(
+    ['func', 'string', 'typ'],
+    [
+        (func, str(func(t)), k)
+        for func, disabled in [
+            (lambda t: t, []),
+            (lambda t: t.id, []),
+            (lambda t: abs(t['amount']), []),
+            (lambda t: t.id.max(), []),
+            (lambda t: t.amount.sum(), []),
+            (lambda t: t.amount.sum(keepdims=True), []),
+            (lambda t: t.amount.count(keepdims=True), []),
+            (lambda t: t.amount.nunique(keepdims=True), [type(mongo)]),
+            (lambda t: t.amount.nunique(keepdims=True), []),
+            (lambda t: t.amount.head(), [type(mongo)]),
+            (lambda t: t.amount + 1, []),
+            (lambda t: sin(t.amount), [type(sql), type(mongo)]),
+            (lambda t: exp(t.amount), [type(sql), type(mongo)]),
+            (lambda t: t.amount > 50, [type(mongo)]),
+            (lambda t: t[t.amount > 50], []),
+            (lambda t: t.like(name='Alic*'), []),
+            (lambda t: t.sort('name'), [type(bc)]),
+            (lambda t: t.sort('name', ascending=False), [type(bc)]),
+            (lambda t: t.head(3), []),
+            (lambda t: t.name.distinct(), []),
+            (lambda t: t[t.amount > 50]['name'], []),
+            (
+                lambda t: t.id.map(lambda x: x + 1, schema='int64', name='id'),
+                [type(sql), type(mongo)]
+            ),
+            (lambda t: by(t.name, total=t.amount.sum()), []),
+            (lambda t: by(t.id, count=t.id.count()), []),
+            (lambda t: by(t[['id', 'amount']], count=t.id.count()), []),
+            (
+                lambda t: by(t[['id', 'amount']], total=(t.amount + 1).sum()),
+                [type(mongo)]
+            ),
+            (
+                lambda t: by(t[['id', 'amount']], n=t.name.nunique()),
+                [type(mongo), type(bc)]
+            ),
+            (lambda t: by(t.id, count=t.amount.count()), []),
+            (lambda t: by(t.id, n=t.id.nunique()), [type(mongo), type(bc)]),
+            # (lambda t: by(t, count=t.count()), []),
+            # (lambda t: by(t.id, count=t.count()), []),
 
-
-def df_eq(a, b):
-    return (list(a.columns) == list(b.columns)
-            # and list(a.dtypes) == list(b.dtypes)
-            and into(set, into(list, a)) == into(set, into(list, b)))
-
-
-def typename(obj):
-    return type(obj).__name__
-
-
-def test_base():
-    for expr, exclusions in expressions.items():
-        if iscollection(expr.dshape):
-            model = into(DataFrame, into(np.ndarray, expr._subs({t: Data(base, t.dshape)})))
+            # https://github.com/numpy/numpy/issues/3256
+            (lambda t: t[['amount', 'id']], [type(x)]),
+            (lambda t: t[['id', 'amount']], [type(x), type(bc)]),
+            (lambda t: t[0], list(map(type, [sql, mongo, bc]))),
+            (lambda t: t[::2], list(map(type, [sql, mongo, bc]))),
+            (lambda t: t.id.utcfromtimestamp, [type(sql)]),
+            (lambda t: t.distinct().nrows, []),
+            (lambda t: t.nelements(axis=0), []),
+            (lambda t: t.nelements(axis=None), []),
+            (lambda t: t.amount.truncate(200), [type(sql)])
+        ]
+        for k in sorted(set(sources) - set(disabled), key=str)
+    ]
+)
+def test_new_base(func, string, typ):
+    expr = func(t)
+    model = compute(expr._subs({t: Data(df, t.dshape)}))
+    source = sources[typ]
+    T = Data(source)
+    new_expr = expr._subs({t: T})
+    if iscollection(expr.dshape):
+        if isscalar(expr.dshape.measure):
+            result = odo(new_expr, pd.Series)
+            assert odo(result, set) == odo(model, set)
         else:
-            model = compute(expr._subs({t: Data(base, t.dshape)}))
-        print('\nexpr: %s\n' % expr)
-        for source in sources:
-            if id(source) in map(id, exclusions):
-                continue
-            print('%s <- %s' % (typename(model), typename(source)))
-            T = Data(source)
-            if iscollection(expr.dshape):
-                result = into(type(model), expr._subs({t: T}))
-                if isscalar(expr.dshape.measure):
-                    assert set(into(list, result)) == set(into(list, model))
-                else:
-                    assert df_eq(result, model)
-            elif isrecord(expr.dshape):
-                result = compute(expr._subs({t: T}))
-                assert into(tuple, result) == into(tuple, model)
-            else:
-                result = compute(expr._subs({t: T}))
-                try:
-                    result = result.scalar()
-                except AttributeError:
-                    pass
-                assert result == model
+            result = odo(new_expr, pd.DataFrame)
+            tm.assert_frame_equal(result.sort(), model.sort())
+    elif isrecord(expr.dshape):
+        assert odo(compute(new_expr), tuple) == odo(model, tuple)
+    else:
+        result = compute(new_expr)
+        try:
+            result = result.scalar()
+        except AttributeError:
+            pass
+        assert result == model
