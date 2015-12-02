7848338fd8c1a73c8371617fc4b72a139380cc50
Phillip Cloud <cpcloud@gmail.com>
2015-12-02 18:03:54 -0500

    Test for new like expression

--- blaze/expr/tests/test_strings.py
+++ blaze/expr/tests/test_strings.py
@@ -1,12 +1,24 @@
 import datashape
-from blaze.expr import TableSymbol, like, Like
+import pytest
+from datashape import dshape
+
+from blaze import symbol
 
 
-def test_like():
-    t = TableSymbol('t', '{name: string, amount: int, city: string}')
-
-    expr = like(t, name='Alice*')
-
-    assert eval(str(expr)).isidentical(expr)
-    assert expr.schema == t.schema
-    assert expr.dshape[0] == datashape.var
+@pytest.mark.parametrize(
+    'ds',
+    [
+        'var * {name: string}',
+        'var * {name: ?string}',
+        'var * string',
+        'var * ?string',
+        'string',
+    ]
+)
+def test_like(ds):
+    t = symbol('t', ds)
+    expr = getattr(t, 'name', t).like('Alice*')
+    assert expr.pattern == 'Alice*'
+    assert expr.schema.measure == dshape(
+        '%sbool' % ('?' if '?' in ds else '')
+    ).measure
