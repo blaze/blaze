7cdfeac610634291265108ec0b407a1a22af3179
Jasmine Sandhu <jsandhu@continuum.io>
2016-04-11 10:38:32 -0700

    BUG: Add missing import for warnings

--- blaze/expr/strings.py
+++ blaze/expr/strings.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
+import warnings
+
 import datashape
 from datashape import String, DataShape, Option, bool_
 from odo.utils import copydoc
