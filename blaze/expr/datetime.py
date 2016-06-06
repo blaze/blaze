cf8565926fe213cb0ef2fc15740ff82e621859c1
Stefan Seefeld <stefan@seefeld.name>
2016-06-06 13:23:43 -0400

    Add datetime functions to 'dt' namespace.

--- blaze/expr/datetime.py
+++ blaze/expr/datetime.py
@@ -7,9 +7,12 @@ from datashape import dshape, isdatelike, isnumeric
 
 
 __all__ = ['DateTime', 'Date', 'date', 'Year', 'year', 'Month', 'month', 'Day',
-           'day', 'Hour', 'hour', 'Minute', 'minute', 'Second', 'second',
-           'Millisecond', 'millisecond', 'Microsecond', 'microsecond', 'Date',
-           'date', 'Time', 'time', 'UTCFromTimestamp', 'DateTimeTruncate']
+           'day', 'days', 'Hour', 'hour', 'Minute', 'minute', 'Second', 'second',
+           'Millisecond', 'millisecond', 'Microsecond', 'microsecond', 'nanosecond',
+           'Date',
+           'date', 'Time', 'time', 'week', 'nanoseconds', 'seconds', 'total_seconds',
+           'UTCFromTimestamp', 'DateTimeTruncate',
+           'Ceil', 'Floor', 'Round', 'strftime']
 
 
 class DateTime(ElemWise):
@@ -119,6 +122,27 @@ def utcfromtimestamp(expr):
     return UTCFromTimestamp(expr)
 
 
+class nanosecond(DateTime): _dtype = datashape.int64
+class week(DateTime): _dtype = datashape.int64
+class weekday(DateTime): _dtype = datashape.int64
+class weekday_name(DateTime): _dtype = datashape.string
+class daysinmonth(DateTime): _dtype = datashape.int64
+class weekofyear(DateTime): _dtype = datashape.int64
+class dayofyear(DateTime): _dtype = datashape.int64
+class dayofweek(DateTime): _dtype = datashape.int64
+class quarter(DateTime): _dtype = datashape.int64
+class is_month_start(DateTime): _dtype = datashape.bool_
+class is_month_end(DateTime): _dtype = datashape.bool_
+class is_quarter_start(DateTime): _dtype = datashape.bool_
+class is_quarter_end(DateTime): _dtype = datashape.bool_
+class is_year_start(DateTime): _dtype = datashape.bool_
+class is_year_end(DateTime): _dtype = datashape.bool_
+class days_in_month(DateTime): _dtype = datashape.int64
+
+class strftime(ElemWise):
+    _arguments = '_child', 'format'
+    schema = datashape.string
+
 units = (
     'year',
     'month',
@@ -222,10 +246,149 @@ def truncate(expr, *args, **kwargs):
     return DateTimeTruncate(expr, measure, normalize_time_unit(unit))
 
 
+class UnaryDateTimeFunction(ElemWise):
+
+    """DateTime function that only takes a single argument."""
+    _arguments = '_child'
+
+
+class Round(ElemWise):
+    _arguments = '_child', 'freq'
+
+    @property
+    def schema(self):
+        return self._child.schema
+
+
+class Floor(ElemWise):
+    _arguments = '_child', 'freq'
+
+    @property
+    def schema(self):
+        return self._child.schema
+
+
+class Ceil(ElemWise):
+    _arguments = '_child', 'freq'
+
+    @property
+    def schema(self):
+        return self._child.schema
+
+
+class dt_ns(object):
+
+    def __init__(self, field):
+        self.field = field
+
+    def year(self):
+        return year(self.field)
+    def month(self):
+        return month(self.field)
+    def day(self):
+        return day(self.field)
+    def hour(self):
+        return hour(self.field)
+    def minute(self):
+        return minute(self.field)
+    def date(self):
+        return date(self.field)
+    def time(self):
+        return time(self.field)
+    def second(self):
+        return second(self.field)
+    def millisecond(self):
+        return millisecond(self.field)
+    def microsecond(self):
+        return microsecond(self.field)
+    def nanosecond(self):
+        return nanosecond(self.field)
+    def weekday(self):
+        return weekday(self.field)
+    def weekday_name(self):
+        return weekday_name(self.field)
+    def daysinmonth(self):
+        return daysinmonth(self.field)
+    def weekofyear(self):
+        return weekofyear(self.field)
+    def dayofyear(self):
+        return dayofyear(self.field)
+    def dayofweek(self):
+        return dayofweek(self.field)
+    def quarter(self):
+        return quarter(self.field)
+    def is_month_start(self):
+        return is_month_start(self.field)
+    def is_month_end(self):
+        return is_month_end(self.field)
+    def is_quarter_start(self):
+        return is_quarter_start(self.field)
+    def is_quarter_end(self):
+        return is_quarter_end(self.field)
+    def is_year_start(self):
+        return is_year_start(self.field)
+    def is_year_end(self):
+        return is_year_end(self.field)
+    def days_in_month(self):
+        return days_in_month(self.field)
+    def strftime(self, format):
+        return strftime(self.field, format)
+    def truncate(self, *args, **kwargs):
+        return truncate(self.field, *args, **kwargs)
+    def round(self, freq):
+        return Round(self.field, freq)
+    def floor(self, freq):
+        return Floor(self.field, freq)
+    def ceil(self, freq):
+        return Ceil(self.field, freq)
+    def week(self):
+        return week(self.field)
+
+class dt(object):
+
+    __name__ = 'dt'
+
+    def __get__(self, obj, type=None):
+        return dt_ns(obj) if obj is not None else self
+
+
+class days(DateTime): _dtype = datashape.int64
+class nanoseconds(DateTime): _dtype = datashape.int64
+class seconds(DateTime): _dtype = datashape.int64
+class total_seconds(DateTime): _dtype = datashape.int64
+
+
+class td_ns(object):
+
+    def __init__(self, field):
+        self.field = field
+
+    def days(self): return days(self.field)
+    def nanoseconds(self): return nanoseconds(self.field)
+    def seconds(self): return seconds(self.field)
+    def total_seconds(self): return total_seconds(self.field)
+
+
+class td(object):
+
+    # pandas uses the same 'dt' name for
+    # DateTimeProperties and TimedeltaProperties.
+    __name__ = 'dt'
+
+    def __get__(self, obj, type=None):
+        return td_ns(obj) if obj is not None else self
+
+
+def isdeltalike(ds):
+    from datashape.coretypes import timedelta_
+    return ds == timedelta_
+
 schema_method_list.extend([
     (isdatelike, set([year, month, day, hour, minute, date, time, second,
-                      millisecond, microsecond, truncate])),
-    (isnumeric, set([utcfromtimestamp]))
+                      millisecond, microsecond, truncate,
+                      dt()])),
+    (isnumeric, set([utcfromtimestamp])),
+    (isdeltalike, set([td()]))
 ])
 
 method_properties |= set([year, month, day, hour, minute, second, millisecond,
