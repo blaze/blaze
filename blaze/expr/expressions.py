020399326e90b7f1e5cf91ab7718964b38bd8df5
Kurt Smith <kwmsmith@gmail.com>
2016-06-08 18:40:24 -0500

    Testing...

--- blaze/expr/expressions.py
+++ blaze/expr/expressions.py
@@ -304,7 +304,7 @@ class Symbol(Expr):
 
 @copydoc(Symbol)
 def symbol(name, dshape, token=None):
-    return Symbol(name, datashape.dshape(dshape), token or 0)
+    return Symbol(name, dshape, token or 0)
 
 
 @dispatch(Symbol, Mapping)
