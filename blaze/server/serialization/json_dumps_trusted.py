c58fac8648135388f476f021389e9754da9dc900
Scott Sanderson <ssanderson@quantopian.com>
2016-08-17 16:09:21 -0400

    BUG: Unicode errors for py2 client and py3 server.

--- blaze/server/serialization/json_dumps_trusted.py
+++ blaze/server/serialization/json_dumps_trusted.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 from collections import Callable
 
 from blaze.dispatch import dispatch
+from blaze.compatibility import u8
 from functools import partial
 
 from .json_dumps import json_dumps
@@ -17,8 +18,8 @@ def json_dumps_trusted(f):
     # let the server serialize any callable - this is only used for testing
     # at present - do the error handling when json comes from client so in
     # object_hook, catch anything that is not pandas_numpy
-    fcn = ".".join([f.__module__, f.__name__])
-    return {'__!callable': fcn}
+    fcn = u8(".".join([f.__module__, f.__name__]))
+    return {u'__!callable': fcn}
 
 
 for tps, func in json_dumps.funcs.items():
