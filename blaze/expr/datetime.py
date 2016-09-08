7cbce24c794128a8b1d144818ad59a2cb2395b5b
Joe Jevnik <joe@quantopian.com>
2016-09-08 13:02:51 -0400

    ENH: fix dayof(week|year) for sql and update dtypes for datetime exprs

--- blaze/expr/datetime.py
+++ blaze/expr/datetime.py
@@ -131,14 +131,14 @@ def utcfromtimestamp(expr):
 
 
 class nanosecond(DateTime): _dtype = datashape.int64
-class week(DateTime): _dtype = datashape.int64
-class weekday(DateTime): _dtype = datashape.int64
+class week(DateTime): _dtype = datashape.int16
 class weekday_name(DateTime): _dtype = datashape.string
-class daysinmonth(DateTime): _dtype = datashape.int64
-class weekofyear(DateTime): _dtype = datashape.int64
-class dayofyear(DateTime): _dtype = datashape.int64
-class dayofweek(DateTime): _dtype = datashape.int64
-class quarter(DateTime): _dtype = datashape.int64
+class daysinmonth(DateTime): _dtype = datashape.int16
+class weekofyear(DateTime): _dtype = datashape.int16
+class dayofyear(DateTime): _dtype = datashape.int16
+class dayofweek(DateTime): _dtype = datashape.int16
+weekday = dayofweek  # alias
+class quarter(DateTime): _dtype = datashape.int16
 class is_month_start(DateTime): _dtype = datashape.bool_
 class is_month_end(DateTime): _dtype = datashape.bool_
 class is_quarter_start(DateTime): _dtype = datashape.bool_
@@ -312,7 +312,9 @@ class dt_ns(object):
     def nanosecond(self):
         return nanosecond(self.field)
     def weekday(self):
-        return weekday(self.field)
+        """Alias for dayofweek
+        """
+        return dayofweek(self.field)
     def weekday_name(self):
         return weekday_name(self.field)
     def daysinmonth(self):
