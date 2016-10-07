e2262c3ab928af6329461c27593972218212509b
Kurt Smith <kwmsmith@gmail.com>
2016-10-07 14:55:12 -0500

    Add regression test for issue #1530.

--- blaze/expr/tests/test_expr.py
+++ blaze/expr/tests/test_expr.py
@@ -95,6 +95,9 @@ def test_label():
     assert label(e, 'foo')._name == 'foo'
     assert label(e, 'e').isidentical(e)
     assert label(e, 'foo').label('bar')._name == 'bar'
+    e = symbol('t', 'var * {name: string}')
+    r = e.name.label('a').label('b')
+    assert r._label == r._name
 
 
 def test_fields_with_spaces():
